1f1880cf508be18da62e5d849ac3e49dc753ed91
diego <diegorodriguezmancini@gmail.com>
2019-12-05 17:21:55 -0300

    initdb

--- apf/core/management/helpers.py
+++ apf/core/management/helpers.py
@@ -52,6 +52,7 @@ def initdb():
         db_credentials = "sqlite:///:memory:"
     engine = create_engine(db_credentials)
     Base.metadata.create_all(engine)
+    Session.configure(bind=engine)
 
 
 def new_step():
