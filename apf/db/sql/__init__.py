5471c7c6445ca4ad2f31dbf4bb352a0cd94401ac
Diego Rodriguez <diegorodriguezmancini@gmail.com>
2019-12-10 11:34:45 -0300

    get or create filter

--- apf/db/sql/__init__.py
+++ apf/db/sql/__init__.py
@@ -5,13 +5,14 @@ from sqlalchemy.orm import sessionmaker
 Base = declarative_base()
 Session = sessionmaker()
 
-
-def get_or_create(session, model, **kwargs):
-    instance = session.query(model).filter_by(**kwargs).first()
+# TODO return only fields in filter_by + kwargs
+def get_or_create(session, model, filter_by=None, **kwargs):
+    instance = session.query(model).filter_by(**filter_by).first()
     if instance:
         return instance, False
     else:
-        instance = model(**kwargs)
+        filter_by.update(kwargs)
+        instance = model(**filter_by)
         session.add(instance)
         return instance, True
 
@@ -25,9 +26,22 @@ def get_session(db_config):
     Session.configure(bind=engine)
     return Session()
 
+
 def add_to_database(session, objects):
     if isinstance(objects, list):
         session.add_all(objects)
     else:
         session.add(objects)
     session.commit()
+
+
+def bulk_insert(objects, model, session, batch_size=1000):
+    batch = []
+    insert_stmt = model.__table__.insert()
+    for obj in objects:
+        if len(batch) > batch_size:
+            session.execute(insert_stmt, batch)
+            batch.clear()
+        batch.append(obj)
+    if batch:
+        session.execute(insert_stmt, batch)
