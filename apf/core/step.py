18ad58e3509cb1841e60e92b9e8ec4de58caf08b
Camilo Valenzuela <cacke91@gmail.com>
2019-12-16 12:28:51 -0300

    Added disable autocommit

--- apf/core/step.py
+++ apf/core/step.py
@@ -26,20 +26,21 @@ class GenericStep():
         self.config = config
         self.consumer = GenericConsumer() if consumer is None else consumer
         self.metrics = None
+        self.disable_autocommit = self.config.get("DISABLE_AUTOCOMMIT", False)
 
         if "ES_CONFIG" in config:
             logging.getLogger("elasticsearch").setLevel(logging.WARNING)
             self.logger.info("Creating ES Metrics sender")
             self.metrics = Elasticsearch(**config["ES_CONFIG"])
 
-    def send_metrics(self,doc_type="metric",**metrics):
+    def send_metrics(self,**metrics):
         if self.metrics:
             date = datetime.datetime.utcnow().strftime("%Y%m%d")
             index_prefix = self.config["ES_CONFIG"].get("INDEX_PREFIX", "pipeline")
             self.index = f"{index_prefix}-{self.__class__.__name__.lower()}-{date}"
             metrics["@timestamp"] = datetime.datetime.utcnow()
             metrics["source"] = self.__class__.__name__
-            self.metrics.index(index = self.index,doc_type = doc_type,body=metrics)
+            self.metrics.index(index = self.index,body=metrics)
 
     @abstractmethod
     def execute(self, message):
@@ -59,5 +60,6 @@ class GenericStep():
         for self.message in self.consumer.consume():
             t0 = time.time()
             self.execute(self.message)
-            self.consumer.commit()
+            if self.disable_autocommit:
+                self.consumer.commit()
             self.send_metrics(execution_time = time.time()-t0)
