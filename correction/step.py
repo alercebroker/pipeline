017484a546b3abb3ff25d209973da0a4a900898b
Ubuntu <ubuntu@ip-172-31-90-247.ec2.internal>
2019-12-20 15:29:03 +0000

    upload s3

--- correction/step.py
+++ correction/step.py
@@ -7,8 +7,10 @@ from apf.db.sql import get_session, get_or_create, bulk_insert
 from apf.producers import KafkaProducer
 from apf.consumers import KafkaConsumer
 import datetime
+import math
 import time
 import json
+import io
 from .s3 import get_object_url, upload_file
 np.seterr(divide='ignore')
 
@@ -41,8 +43,12 @@ class Correction(GenericStep):
         }
         self.producer.produce(write)
 
-        if type(self.consumer) is KafkaConsumer:   
-            upload_file(self.consumer.message.value(), "ztf-storage", message["candidate"]["candid"])
+        if type(self.consumer) is KafkaConsumer:
+            f = io.BytesIO(self.consumer.message.value())
+            year, month, day = self.jd_to_date(message["candidate"]["jd"])
+            date = "{}{}{}".format(year, month, int(day))
+            url = upload_file(f,date, message["candidate"]["candid"])
+            self.logger.info(url)
 
 
 
@@ -193,3 +199,39 @@ class Correction(GenericStep):
         for d in ret["non_detections"]:
             del d['_sa_instance_state']
         return ret
+
+    def jd_to_date(self,jd):
+        
+        jd = jd + 0.5
+        
+        F, I = math.modf(jd)
+        I = int(I)
+        
+        A = math.trunc((I - 1867216.25)/36524.25)
+        
+        if I > 2299160:
+            B = I + 1 + A - math.trunc(A / 4.)
+        else:
+            B = I
+            
+        C = B + 1524
+        
+        D = math.trunc((C - 122.1) / 365.25)
+        
+        E = math.trunc(365.25 * D)
+        
+        G = math.trunc((C - E) / 30.6001)
+        
+        day = C - E + F - math.trunc(30.6001 * G)
+        
+        if G < 13.5:
+            month = G - 1
+        else:
+            month = G - 13
+            
+        if month > 2.5:
+            year = D - 4716
+        else:
+            year = D - 4715
+            
+        return year, month, day
