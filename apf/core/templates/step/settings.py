fd36fac35798d5642b06645f69b0d5ef6299c940
Camilo Valenzuela <cacke91@gmail.com>
2019-12-30 14:21:58 -0300

    Added comments to template settings

--- apf/core/templates/step/settings.py
+++ apf/core/templates/step/settings.py
@@ -14,11 +14,18 @@
 ## Other parameters that can be passed are defined here
 # https://elasticsearch-py.readthedocs.io/en/master/api.html#elasticsearch.Elasticsearch
 
-
+## Consumer configuration
+### Each consumer has different parameters and can be found in the documentation-
 CONSUMER_CONFIG = {}
+
+## Database configuration
+### Depending on the database backend the parameters can change
 DB_CONFIG = {}
+
+## Step Configuration
 STEP_CONFIG = {
     "DB_CONFIG": DB_CONFIG,
-    # "COMMIT": False,           #Disables commit, useful to debug KafkaConsumer
     # "ES_CONFIG": ES_CONFIG,    #Enables metrics for step
+    # "N_PROCESS": 4,            # Number of process for multiprocess script
+    # "COMMIT": False,           #Disables commit, useful to debug KafkaConsumer
 }
