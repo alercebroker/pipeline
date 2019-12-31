6ea1cbe986ba4c84eba2a2faf7d753646d19445f
Diego Rodriguez <diegorodriguezmancini@gmail.com>
2019-12-31 15:58:38 -0300

    step

---
+++ s3_step/step.py
@@ -0,0 +1,53 @@
+from apf.core.step import GenericStep
+import logging
+import io
+from .s3 import upload_file
+import math
+
+class S3Step(GenericStep):
+    """S3Step Description
+
+    Parameters
+    ----------
+    consumer : GenericConsumer
+        Description of parameter `consumer`.
+    **step_args : type
+        Other args passed to step (DB connections, API requests, etc.)
+
+    """
+
+    def __init__(self, consumer=None, config=None, level=logging.INFO, **step_args):
+        super().__init__(consumer, config=config, level=level)
+
+    def execute(self, message):
+        self.logger.info(message["objectId"])
+        f = io.BytesIO(self.consumer.message.value())
+        year, month, day = self.jd_to_date(message["candidate"]["jd"])
+        date = "{}{}{}".format(year, month, int(day))
+        upload_file(
+            f, date, message["candidate"]["candid"], self.config["STORAGE"]["NAME"])
+
+
+    def jd_to_date(self, jd):
+        jd = jd + 0.5
+        F, I = math.modf(jd)
+        I = int(I)
+        A = math.trunc((I - 1867216.25)/36524.25)
+        if I > 2299160:
+            B = I + 1 + A - math.trunc(A / 4.)
+        else:
+            B = I
+        C = B + 1524
+        D = math.trunc((C - 122.1) / 365.25)
+        E = math.trunc(365.25 * D)
+        G = math.trunc((C - E) / 30.6001)
+        day = C - E + F - math.trunc(30.6001 * G)
+        if G < 13.5:
+            month = G - 1
+        else:
+            month = G - 13
+        if month > 2.5:
+            year = D - 4716
+        else:
+            year = D - 4715
+        return year, month, day
---
+++ settings.py
@@ -0,0 +1,43 @@
+##################################################
+#       s3_step   Settings File
+##################################################
+
+# Set the global logging level to debug
+# LOGGING_DEBUG = True
+
+# Elasticsearch Metrics Consfiguration
+# ES_CONFIG = {
+#     "INDEX_PREFIX": "",
+#     # Used to generate index index_prefix+class_name+date
+#     # Other parameters
+# }
+# Other parameters that can be passed are defined here
+# https://elasticsearch-py.readthedocs.io/en/master/api.html#elasticsearch.Elasticsearch
+
+# Consumer configuration
+# Each consumer has different parameters and can be found in the documentation-
+CONSUMER_CONFIG = {
+     # "DIRECTORY_PATH": "../avro_files/ztf_public_20190302"
+
+     "TOPICS": ["ztf_20191218_programid1_aux"],
+     "PARAMS": {
+         "bootstrap.servers": "kafka1.alerce.online:9092",
+         "group.id": "test"
+     }
+}
+
+# Database configuration
+# Depending on the database backend the parameters can change
+DB_CONFIG = {}
+
+# Step Configuration
+STEP_CONFIG = {
+    "DB_CONFIG": DB_CONFIG,
+    # "ES_CONFIG": ES_CONFIG,    #Enables metrics for step
+    # "N_PROCESS": 4,            # Number of process for multiprocess script
+    "COMMIT": False,  # Disables commit, useful to debug KafkaConsumer
+    "STORAGE": {
+        "NAME": "alerce-challenges"
+    }
+}
+
