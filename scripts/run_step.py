230e54084ce03ba41968e3f6890f1f960fa6a1a7
diego <diego@pop-os.localdomain>
2020-04-01 14:08:15 -0300

    docker compose

--- scripts/run_step.py
+++ scripts/run_step.py
@@ -3,15 +3,6 @@ import sys
 
 import logging
 
-level = logging.INFO
-if 'LOGGING_DEBUG' in locals():
-    if LOGGING_DEBUG:
-        level=logging.DEBUG
-
-logging.basicConfig(level=level,
-                    format='%(asctime)s %(levelname)s %(name)s.%(funcName)s: %(message)s',
-                    datefmt='%Y-%m-%d %H:%M:%S',)
-
 SCRIPT_PATH = os.path.dirname(os.path.abspath(__file__))
 PACKAGE_PATH = os.path.abspath(os.path.join(SCRIPT_PATH,".."))
 
@@ -21,6 +12,14 @@ from settings import CONSUMER_CONFIG, STEP_CONFIG
 from correction import Correction
 from apf.consumers import AVROFileConsumer
 
+level = logging.INFO
+if 'LOGGING_DEBUG' in locals():
+    if LOGGING_DEBUG:
+        level=logging.DEBUG
+
+logging.basicConfig(level=level,
+                    format='%(asctime)s %(levelname)s %(name)s.%(funcName)s: %(message)s',
+                    datefmt='%Y-%m-%d %H:%M:%S',)
 consumer = AVROFileConsumer(config=CONSUMER_CONFIG)
 
 step = Correction(consumer,config=STEP_CONFIG,level=level)
--- settings.py
+++ settings.py
@@ -1,78 +1,83 @@
 ##################################################
 #       correction   Settings File
 ##################################################
-
-CONSUMER_CONFIG = {
-    "DIRECTORY_PATH": "../avro_files/ztf_public_20190302"
-    # "TOPICS": ["mag_test"],
-    # "PARAMS": {
-    #     "bootstrap.servers": "kafka1.alerce.online:9092",
-    #     "group.id": "test"
-    # }
-}
-
+import os
+LOGGING_DEBUG = True
 DB_CONFIG = {
     "PSQL": {
-        "HOST": "localhost",
-        "USER": "postgres",
-        "PASSWORD": "docker",
-        "PORT": 5432,
-        "DB_NAME": "test"
+        "HOST": os.environ["DB_HOST"],
+        "USER": os.environ["DB_USER"],
+        "PASSWORD": os.environ["DB_PASSWORD"],
+        "PORT": int(os.environ["DB_PORT"]),
+        "DB_NAME": os.environ["DB_NAME"]
     }
 }
 
-ES_CONFIG = {"INDEX_PREFIX":"ztf_pipeline"}
+CONSUMER_CONFIG = {
+    "DIRECTORY_PATH": "/test_pipeline/test_data/tests/ztf_sample/"
+}
+
+
+PRODUCER_CONFIG = {
+    "TOPIC": os.environ["PRODUCER_TOPIC"],
+    "PARAMS": {
+        'bootstrap.servers': os.environ["PRODUCER_SERVER"],
+    },
+    "SCHEMA": {
+        'doc': 'Lightcurve',
+        'name': 'lightcurve',
+        'type': 'record',
+        'fields': [
+            {'name': 'oid', 'type': 'string'},
+            {'name': 'detections', 'type': {
+                'type': 'array',
+                'items': {
+                    'type': 'record',
+                    'name': 'detection',
+                    'fields': [
+                        {'name': 'candid', 'type': 'string'},
+                        {'name': 'mjd', 'type': 'float'},
+                        {'name': 'fid', 'type': 'int'},
+                        {'name': 'magpsf_corr', 'type': [
+                            'float', 'null'], 'default':None},
+                        {'name': 'magap_corr', 'type': [
+                            'float', 'null'], 'default':None},
+                        {'name': 'sigmapsf_corr', 'type': [
+                            'float', 'null'], 'default':None},
+                        {'name': 'sigmagap_corr', 'type': [
+                            'float', 'null'], 'default':None},
+                        {'name': 'ra', 'type': 'float'},
+                        {'name': 'dec', 'type': 'float'},
+                        {'name': 'rb', 'type': [
+                            'float', 'null'], 'default':None},
+                        {'name': 'oid', 'type': 'string'},
+                        {'name': 'alert',
+                         'type': {
+                             'type': 'map',
+                             'values': ['int', 'float', 'string', 'null']
+                         }}
+                    ]
+                }
+            }},
+            {'name': 'non_detections', 'type': {
+                'type': 'array',
+                'items': {
+                    'type': 'map',
+                    'values': ['float', 'int', 'string', 'null']
+                }
+            }}
+        ],
+    }
+}
+ES_CONFIG = {
+    "INDEX_PREFIX": os.environ["ES_PREFIX"],
+    "host": os.environ["ES_NETWORK_HOST"],
+    "port": os.environ["ES_NETWORK_PORT"]
+}
 
 STEP_CONFIG = {
     "DB_CONFIG": DB_CONFIG,
     # "ES_CONFIG": ES_CONFIG,
-    "PRODUCER_CONFIG": {
-        "TOPIC": "s3_test",
-        "PARAMS": {
-            'bootstrap.servers': '127.0.0.1:9092',
-        },
-        "SCHEMA": {
-            'doc': 'Lightcurve',
-            'name': 'lightcurve',
-            'type': 'record',
-            'fields': [
-                {'name': 'oid', 'type': 'string'},
-                {'name': 'detections', 'type': {
-                    'type': 'array',
-                    'items': {
-                        'type': 'record',
-                        'name': 'detection',
-                        'fields': [
-                            {'name': 'candid', 'type': 'string'},
-                            {'name': 'mjd', 'type': 'float'},
-                            {'name': 'fid', 'type': 'int'},
-                            {'name': 'magpsf_corr', 'type': [
-                                'float', 'null'], 'default':None},
-                            {'name': 'magap_corr', 'type': [
-                                'float', 'null'], 'default':None},
-                            {'name': 'sigmapsf_corr', 'type': [
-                                'float', 'null'], 'default':None},
-                            {'name': 'sigmagap_corr', 'type': [
-                                'float', 'null'], 'default':None},
-                            {'name': 'ra', 'type': 'float'},
-                            {'name': 'dec', 'type': 'float'},
-                            {'name': 'rb', 'type': [
-                                'float', 'null'], 'default':None},
-                            {'name': 'oid', 'type': 'string'},
-                            {'name': 'alert',
-                             'type': {
-                                 'type': 'map',
-                                 'values': ['int', 'float', 'string', 'null']
-                             }
-                             }
-                        ]
-                    }
-                }},
-                {'name': 'non_detections', 'type': {
-                    'type': 'array',
-                    'items': {'type': 'map', 'values': ['float', 'int', 'string', 'null']}
-                }}
-            ],
-        }
-    },
+    "PRODUCER_CONFIG": PRODUCER_CONFIG,
+    "COMMIT": False,
 }
