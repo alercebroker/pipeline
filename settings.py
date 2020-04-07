7da0df49dfcd0abb37054242fc4ffe69e8cf546c
JavierArredondo <javier.arredondo.c@usach.cl>
2020-04-07 15:05:59 -0400

    CHG: os.getenv for os.environ

--- settings.py
+++ settings.py
@@ -4,30 +4,28 @@
 import os
 FEATURE_VERSION = "v0.1"
 
-CONSUMER_CONFIG = {"TOPICS": [os.getenv("CONSUMER_TOPICS")],
-                   "PARAMS": {
-    'bootstrap.servers': os.getenv("CONSUMER_SERVER"),
-    'group.id': os.getenv("CONSUMER_GROUP_ID")
-}
-}
 DB_CONFIG = {
     "PSQL": {
-        "HOST": os.getenv("DB_HOST"),
-        "USER": os.getenv("DB_USER"),
-        "PASSWORD": os.getenv("DB_PASSWORD"),
-        "PORT": os.getenv("DB_PORT"),
-        "DB_NAME": os.getenv("DB_NAME")
+        "HOST": os.environ["DB_HOST"],
+        "USER": os.environ["DB_USER"],
+        "PASSWORD": os.environ["DB_PASSWORD"],
+        "PORT": int(os.environ["DB_PORT"]),
+        "DB_NAME": os.environ["DB_NAME"]
     }
 }
-ES_CONFIG = {"INDEX_PREFIX": os.getenv("ES_PREFIX"),
-             "host": os.getenv("ES_NETWORK_HOST"),
-             "port": os.getenv("ES_NETWORK_PORT")
-             }
+
+CONSUMER_CONFIG = {
+    "TOPICS": os.environ["CONSUMER_TOPICS"].strip().split(","),
+    "PARAMS": {
+         "bootstrap.servers": os.environ["CONSUMER_SERVER"],
+         "group.id": os.environ["CONSUMER_GROUP_ID"]
+    }
+}
 
 PRODUCER_CONFIG = {
-    "TOPIC": os.getenv("PRODUCER_TOPIC"),
+    "TOPIC": os.environ["PRODUCER_TOPIC"],
     "PARAMS": {
-        'bootstrap.servers': os.getenv("PRODUCER_SERVER"),
+        'bootstrap.servers': os.environ["PRODUCER_SERVER"],
     },
     "SCHEMA": {
         'doc': 'Features',
@@ -44,11 +42,16 @@ PRODUCER_CONFIG = {
     }
 }
 
+ES_CONFIG = {
+    "INDEX_PREFIX": os.environ["ES_PREFIX"],
+    "host": os.environ["ES_NETWORK_HOST"],
+    "port": os.environ["ES_NETWORK_PORT"]
+}
+
 STEP_CONFIG = {
     "CONSUMER_CONFIG": CONSUMER_CONFIG,
     "DB_CONFIG": DB_CONFIG,
     "ES_CONFIG": ES_CONFIG,
     "PRODUCER_CONFIG": PRODUCER_CONFIG,
     "FEATURE_VERSION": FEATURE_VERSION,
-    "COMMIT": False
 }
