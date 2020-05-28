ecd91a061e8a52e295f25418cd387dd6ae78ca80
Diego <diego@pop-os.localdomain>
2020-05-28 16:30:18 -0400

    added more serializers

--- db_plugins/db/sql/serializers.py
+++ db_plugins/db/sql/serializers.py
@@ -1,31 +1,47 @@
-from flask import Flask
-from marshmallow_sqlalchemy import SQLAlchemyAutoSchema, auto_field
+from marshmallow_sqlalchemy import SQLAlchemyAutoSchema
 from db_plugins.db.sql.models import *
+
 
 class ClassSchema(SQLAlchemyAutoSchema):
     class Meta:
         model = Class
 
+
 class TaxonomySchema(SQLAlchemyAutoSchema):
     class Meta:
         model = Taxonomy
+
 
 class ClassifierSchema(SQLAlchemyAutoSchema):
     class Meta:
         model = Classifier
 
+
 class AstroObjectSchema(SQLAlchemyAutoSchema):
     class Meta:
         model = AstroObject
+
 
 class ClassificationSchema(SQLAlchemyAutoSchema):
     class Meta:
         model = Classification
 
+
 class MagnitudeStatisticsSchema(SQLAlchemyAutoSchema):
     class Meta:
         model = MagnitudeStatistics
 
+
 class FeaturesSchema(SQLAlchemyAutoSchema):
     class Meta:
         model = FeaturesObject
+
+
+class DetectionSchema(SQLAlchemyAutoSchema):
+    class Meta:
+        model = Detection
+
+
+class NonDetectionSchema(SQLAlchemyAutoSchema):
+    class Meta:
+        model = NonDetection
