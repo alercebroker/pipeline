674c6922c856dfc08fb5104ff589f090a9f3f0c7
Camilo Valenzuela <cacke91@gmail.com>
2020-07-15 12:34:44 -0400

    Changes merge into new schema

--- db_plugins/db/sql/models.py
+++ db_plugins/db/sql/models.py
@@ -1,6 +1,7 @@
 from sqlalchemy import (
     Column,
     Integer,
+    BigInteger,
     String,
     Table,
     ForeignKey,
@@ -15,55 +16,53 @@ from .. import generic
 
 from db_plugins.db.sql import Base
 
-
-taxonomy_class = Table(
-    "taxonomy_class",
-    Base.metadata,
-    Column("class_name", String, ForeignKey("class.name")),
-    Column("taxonomy_name", String, ForeignKey("taxonomy.name")),
-)
-
-
 class Commons:
     def __getitem__(self, field):
         return self.__dict__[field]
 
 
-class Class(Base, generic.AbstractClass):
-    __tablename__ = "class"
+# taxonomy_class = Table(
+# "taxonomy_class",
+# Base.metadata,
+# Column("class_name", String, ForeignKey("class.name")),
+# Column("taxonomy_name", String, ForeignKey("taxonomy.name")),
+# )
 
-    name = Column(String, primary_key=True)
-    acronym = Column(String)
-    taxonomies = relationship(
-        "Taxonomy", secondary=taxonomy_class, back_populates="classes"
-    )
-    classifications = relationship("Classification")
-
-    def __repr__(self):
-        return "<Class(name='%s', acronym='%s')>" % (self.name, self.acronym)
-
-
-class Taxonomy(Base, generic.AbstractTaxonomy):
-    __tablename__ = "taxonomy"
-
-    name = Column(String, primary_key=True)
-    classes = relationship(
-        "Class", secondary=taxonomy_class, back_populates="taxonomies"
-    )
-    classifiers = relationship("Classifier")
-
-    def __repr__(self):
-        return "<Taxonomy(name='%s')>" % (self.name)
-
-
-class Classifier(Base, generic.AbstractClassifier):
-    __tablename__ = "classifier"
-    name = Column(String, primary_key=True)
-    taxonomy_name = Column(String, ForeignKey("taxonomy.name"))
-    classifications = relationship("Classification")
-
-    def __repr__(self):
-        return "<Classifier(name='%s')>" % (self.name)
+# class Class(Base, generic.AbstractClass):
+#     __tablename__ = "class"
+#
+#     name = Column(String, primary_key=True)
+#     acronym = Column(String)
+#     taxonomies = relationship(
+#         "Taxonomy", secondary=taxonomy_class, back_populates="classes"
+#     )
+#     classifications = relationship("Classification")
+#
+#     def __repr__(self):
+#         return "<Class(name='%s', acronym='%s')>" % (self.name, self.acronym)
+#
+#
+# class Taxonomy(Base, generic.AbstractTaxonomy):
+#     __tablename__ = "taxonomy"
+#
+#     name = Column(String, primary_key=True)
+#     classes = relationship(
+#         "Class", secondary=taxonomy_class, back_populates="taxonomies"
+#     )
+#     classifiers = relationship("Classifier")
+#
+#     def __repr__(self):
+#         return "<Taxonomy(name='%s')>" % (self.name)
+#
+#
+# class Classifier(Base, generic.AbstractClassifier):
+#     __tablename__ = "classifier"
+#     name = Column(String, primary_key=True)
+#     taxonomy_name = Column(String, ForeignKey("taxonomy.name"))
+#     classifications = relationship("Classification")
+#
+#     def __repr__(self):
+#         return "<Classifier(name='%s')>" % (self.name)
 
 
 class Object(Base, generic.AbstractObject):
@@ -85,7 +84,7 @@ class Object(Base, generic.AbstractObject):
     meandec = Column(Float)
     sigmara = Column(Float)
     sigmadec = Column(Float)
-    deltamjd = Column(Float)
+    deltajd = Column(Float)
     firstmjd = Column(Float)
     lastmjd = Column(Float)
     step_id_corr = Column(String)
@@ -96,12 +95,12 @@ class Object(Base, generic.AbstractObject):
         Index("object_lastmjd", "lastmjd", postgresql_using="btree"),
     )
 
-    xmatches = relationship("Xmatch")
-    magnitude_statistics = relationship("MagnitudeStatistics", uselist=True)
-    classifications = relationship("Classification")
+    # xmatches = relationship("Xmatch")
+    magstats = relationship("MagStats", uselist=True)
+    # classifications = relationship("Classification")
     non_detections = relationship("NonDetection")
     detections = relationship("Detection")
-    features = relationship("FeaturesObject")
+    # features = relationship("FeaturesObject")
 
     def get_lightcurve(self):
         return {
@@ -113,44 +112,44 @@ class Object(Base, generic.AbstractObject):
         return "<Object(oid='%s')>" % (self.oid)
 
 
-class Classification(Base, generic.AbstractClassification):
-    __tablename__ = "classification"
-
-    object = Column(String, ForeignKey("object.oid"), primary_key=True)
-    classifier_name = Column(String, ForeignKey("classifier.name"), primary_key=True)
-    class_name = Column(String, ForeignKey("class.name"), primary_key=True)
-    probability = Column(Float)
-    probabilities = Column(JSON)
-
-    classes = relationship("Class", back_populates="classifications")
-    objects = relationship("Object", back_populates="classifications")
-    classifiers = relationship("Classifier", back_populates="classifications")
-
-    def __repr__(self):
-        return (
-            "<Classification(class_name='%s', probability='%s', object='%s', classifier_name='%s')>"
-            % (
-                self.class_name,
-                self.probability,
-                self.object,
-                self.classifier_name,
-            )
-        )
-
-
-class Xmatch(Base, generic.AbstractXmatch):
-    __tablename__ = "xmatch"
-
-    oid = Column(String, ForeignKey("object.oid"))
-    catalog_id = Column(String, primary_key=True)
-    catalog_oid = Column(String, primary_key=True)
+# class Classification(Base, generic.AbstractClassification):
+#     __tablename__ = "classification"
+#
+#     object = Column(String, ForeignKey("object.oid"), primary_key=True)
+#     classifier_name = Column(String, ForeignKey("classifier.name"), primary_key=True)
+#     class_name = Column(String, ForeignKey("class.name"), primary_key=True)
+#     probability = Column(Float)
+#     probabilities = Column(JSON)
+#
+#     classes = relationship("Class", back_populates="classifications")
+#     objects = relationship("Object", back_populates="classifications")
+#     classifiers = relationship("Classifier", back_populates="classifications")
+#
+#     def __repr__(self):
+#         return (
+#             "<Classification(class_name='%s', probability='%s', object='%s', classifier_name='%s')>"
+#             % (
+#                 self.class_name,
+#                 self.probability,
+#                 self.object,
+#                 self.classifier_name,
+#             )
+#         )
 
 
-class MagnitudeStatistics(Base, generic.AbstractMagnitudeStatistics):
-    __tablename__ = "magnitude_statistics"
+# class Xmatch(Base, generic.AbstractXmatch):
+#     __tablename__ = "xmatch"
+#
+#     oid = Column(String, ForeignKey("object.oid"), primary_key=True)
+#     catalog_id = Column(String, primary_key=True)
+#     catalog_oid = Column(String, primary_key=True)
+
+
+class MagStats(Base, generic.AbstractMagnitudeStatistics):
+    __tablename__ = "magstats"
 
     oid = Column(String, ForeignKey('object.oid'), primary_key=True)
-    fid = Column(Integer)
+    fid = Column(Integer, primary_key=True)
     stellar = Column(Boolean)
     corrected = Column(Boolean)
     ndet = Column(Integer)
@@ -166,9 +165,16 @@ class MagnitudeStatistics(Base, generic.AbstractMagnitudeStatistics):
     magsigma = Column(Float)
     maglast = Column(Float)
     magfirst = Column(Float)
+    magmean_corr = Column(Float)
+    magmedian_corr = Column(Float)
+    magmax_corr = Column(Float)
+    magmin_corr = Column(Float)
+    magsigma_corr = Column(Float)
+    maglast_corr = Column(Float)
+    magfirst_corr = Column(Float)
     firstmjd = Column(Float)
     lastmjd = Column(Float)
-    step_id_corr = Column(String)
+    step_id = Column(String)
 
     __table_args__ = (
         Index("mag_mean", "magmean", postgresql_using="btree"),
@@ -180,19 +186,19 @@ class MagnitudeStatistics(Base, generic.AbstractMagnitudeStatistics):
     )
 
 
-class Features(Base, generic.AbstractFeatures):
-    __tablename__ = "features"
-
-    version = Column(String, primary_key=True)
-
-
-class FeaturesObject(Base):
-    __tablename__ = "features_object"
-
-    object_id = Column(String, ForeignKey("object.oid"), primary_key=True)
-    features_version = Column(String, ForeignKey("features.version"), primary_key=True)
-    data = Column(JSON)
-    features = relationship("Features")
+# class Features(Base, generic.AbstractFeatures):
+#     __tablename__ = "features"
+#
+#     version = Column(String, primary_key=True)
+#
+#
+# class FeaturesObject(Base):
+#     __tablename__ = "features_object"
+#
+#     object_id = Column(String, ForeignKey("object.oid"), primary_key=True)
+#     features_version = Column(String, ForeignKey("features.version"), primary_key=True)
+#     data = Column(JSON)
+#     features = relationship("Features")
 
 
 class NonDetection(Base, generic.AbstractNonDetection, Commons):
@@ -200,8 +206,7 @@ class NonDetection(Base, generic.AbstractNonDetection, Commons):
 
     oid = Column(String, ForeignKey("object.oid"), primary_key=True)
     fid = Column(Integer, primary_key=True)
-    datetime = Column(DateTime, primary_key=True)
-    mjd = Column(Float)
+    mjd = Column(Float, primary_key=True)
     diffmaglim = Column(Float)
     __table_args__ = (Index("non_det_oid", "oid", postgresql_using="hash"),)
 
@@ -209,7 +214,7 @@ class NonDetection(Base, generic.AbstractNonDetection, Commons):
 class Detection(Base, generic.AbstractDetection, Commons):
     __tablename__ = "detection"
 
-    candid = Column(String, primary_key=True)
+    candid = Column(BigInteger, primary_key=True)
     oid = Column(String, ForeignKey("object.oid"))
     avro = Column(String)
     mjd = Column(Float)
@@ -237,67 +242,69 @@ class Detection(Base, generic.AbstractDetection, Commons):
     sigmapsf_corr_ext = Column(Float)
     corrected = Column(Boolean)
     dubious = Column(Boolean)
-    parent_candid = Column(String)
+    parent_candid = Column(BigInteger)
     has_stamp = Column(Boolean)
     step_id_corr = Column(String)
 
     __table_args__ = (
         Index("object_id", "oid", postgresql_using="hash"),)
 
+    dataquality = relationship("Dataquality")
+
     def __repr__(self):
-        return "<Detection(candid='%s', fid='%i', oid='%s')>" % (self.candid, self.fid, self.oid)
+        return "<Detection(candid='%i', fid='%i', oid='%s')>" % (self.candid, self.fid, self.oid)
 
-class OutlierDetector(Base, generic.AbstractOutlierDetector):
-    __tablename__ = "outlier_detector"
-
-    name = Column(String, primary_key=True)
-
-
-class OutlierScore(Base, generic.AbstractOutlierScore):
-    __tablename__ = "outlier_score"
-    object_ = Column("object", String, ForeignKey("object.oid"), primary_key=True)
-    detector_name = Column(
-        String, ForeignKey("outlier_detector.name"), primary_key=True
-    )
-    score = Column(Float, primary_key=True)
-    scores = Column(JSON)
+# class OutlierDetector(Base, generic.AbstractOutlierDetector):
+#     __tablename__ = "outlier_detector"
+#
+#     name = Column(String, primary_key=True)
+#
+#
+# class OutlierScore(Base, generic.AbstractOutlierScore):
+#     __tablename__ = "outlier_score"
+#     object_ = Column("object", String, ForeignKey("object.oid"), primary_key=True)
+#     detector_name = Column(
+#         String, ForeignKey("outlier_detector.name"), primary_key=True
+#     )
+#     score = Column(Float, primary_key=True)
+#     scores = Column(JSON)
 
 
 class Dataquality(Base, generic.AbstractDataquality):
     __tablename__ = 'dataquality'
 
-    candid = Column(String, primary_key=True)
-    oid = Column(String)
-    fid = Column(Integer)
-    xpos = Column(Float, nullable=False)
-    ypos = Column(Float, nullable=False)
-    chipsf = Column(Float, nullable=False)
-    sky = Column(Float, nullable=False)
-    fwhm = Column(Float, nullable=False)
-    classtar = Column(Float, nullable=False)
-    mindtoedge = Column(Float, nullable=False)
-    seeratio = Column(Float, nullable=False)
-    aimage = Column(Float, nullable=False)
-    bimage = Column(Float, nullable=False)
-    aimagerat = Column(Float, nullable=False)
-    bimagerat = Column(Float, nullable=False)
-    nneg = Column(Integer, nullable=False)
-    nbad = Column(Integer, nullable=False)
-    sumrat = Column(Float, nullable=False)
-    scorr = Column(Float, nullable=False)
-    dsnrms = Column(Float, nullable=False)
-    ssnrms = Column(Float, nullable=False)
-    magzpsci = Column(Float, nullable=False)
-    magzpsciunc = Column(Float, nullable=False)
-    magzpscirms = Column(Float, nullable=False)
-    nmatches = Column(Integer, nullable=False)
-    clrcoeff = Column(Float, nullable=False)
-    clrcounc = Column(Float, nullable=False)
-    zpclrcov = Column(Float, nullable=False)
-    zpmed = Column(Float, nullable=False)
-    clrmed = Column(Float, nullable=False)
-    clrrms = Column(Float, nullable=False)
-    exptime = Column(Float, nullable=False)
+    candid = Column(BigInteger, ForeignKey("detection.candid"), primary_key=True)
+    oid = Column(String, nullable=False)
+    fid = Column(Integer, nullable=False)
+    xpos = Column(Float)
+    ypos = Column(Float)
+    chipsf = Column(Float)
+    sky = Column(Float)
+    fwhm = Column(Float)
+    classtar = Column(Float)
+    mindtoedge = Column(Float)
+    seeratio = Column(Float)
+    aimage = Column(Float)
+    bimage = Column(Float)
+    aimagerat = Column(Float)
+    bimagerat = Column(Float)
+    nneg = Column(Integer)
+    nbad = Column(Integer)
+    sumrat = Column(Float)
+    scorr = Column(Float)
+    dsnrms = Column(Float)
+    ssnrms = Column(Float)
+    magzpsci = Column(Float)
+    magzpsciunc = Column(Float)
+    magzpscirms = Column(Float)
+    nmatches = Column(Integer)
+    clrcoeff = Column(Float)
+    clrcounc = Column(Float)
+    zpclrcov = Column(Float)
+    zpmed = Column(Float)
+    clrmed = Column(Float)
+    clrrms = Column(Float)
+    exptime = Column(Float)
 
     __table_args__ = (
 
@@ -308,8 +315,7 @@ class Dataquality(Base, generic.AbstractDataquality):
 class Gaia_ztf(Base, generic.AbstractGaia_ztf):
     __tablename__ = 'gaia_ztf'
 
-    oid = Column(String, primary_key=True)
-    candid = Column(String, primary_key=True)
+    oid = Column(String, ForeignKey("object.oid"), primary_key=True)
     neargaia = Column(Float, nullable=False)
     neargaiabright = Column(Float, nullable=False)
     maggaia = Column(Float, nullable=False)
@@ -320,8 +326,7 @@ class Gaia_ztf(Base, generic.AbstractGaia_ztf):
 class Ss_ztf(Base, generic.AbstractSs_ztf):
     __tablename__ = 'ss_ztf'
 
-    oid = Column(String, primary_key=True)
-    candid = Column(String)
+    oid = Column(String, ForeignKey("object.oid"), primary_key=True)
     ssdistnr = Column(Float, nullable=False)
     ssmagnr = Column(Float, nullable=False)
     ssnamenr = Column(String)
@@ -330,8 +335,8 @@ class Ss_ztf(Base, generic.AbstractSs_ztf):
 class Ps1_ztf(Base, generic.AbstractPs1_ztf):
     __tablename__ = 'ps1_ztf'
 
-    oid = Column(String, primary_key=True)
-    candid = Column(Integer, primary_key=True)
+    oid = Column(String, ForeignKey("object.oid"), primary_key=True)
+    candid = Column(BigInteger, primary_key=True)
     objectidps1 = Column(Float, nullable=False)
     sgmag1 = Column(Float, nullable=False)
     srmag1 = Column(Float, nullable=False)
@@ -355,14 +360,16 @@ class Ps1_ztf(Base, generic.AbstractPs1_ztf):
     distpsnr3 = Column(Float, nullable=False)
     nmtchps = Column(Integer)
     unique1 = Column(Boolean)
+    unique2 = Column(Boolean)
+    unique3 = Column(Boolean)
 
 
 class Reference(Base, generic.AbstractReference):
     __tablename__ = 'reference'
 
-    oid = Column(String, primary_key=True)
-    rfid = Column(String)
-    candiid = Column(String)
+    oid = Column(String, ForeignKey("object.oid"), primary_key=True)
+    rfid = Column(BigInteger, primary_key=True)
+    candid = Column(BigInteger)
     fid = Column(Integer)
     rcid = Column(Integer, nullable=False)
     field = Column(Integer, nullable=False)
