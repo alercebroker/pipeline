4249be1254d573b6a92d36be9afeeb919710d372
Diego <diegorodriguezmancini@gmail.com>
2020-09-30 11:49:32 -0300

    added description for pypi and links to repo and documentation

--- setup.py
+++ setup.py
@@ -1,17 +1,31 @@
 #!/usr/bin/env python
 
 from setuptools import setup, find_namespace_packages
+
 with open("requirements.txt") as f:
     required_packages = f.readlines()
 
+# read the contents of your README file
+from os import path
+
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, "README.md"), encoding="utf-8") as f:
+    long_description = f.read()
+
 setup(
-   name='apf_base',
-   version="1.0.0",
-   description='ALeRCE Alert Processing Framework.',
-   author='ALeRCE Team',
-   author_email='contact@alerce.online',
-   packages=find_namespace_packages(include=['apf.*']),
-   scripts=['scripts/apf'],
-   install_requires=required_packages,
-   build_requires=required_packages
+    name="apf_base",
+    version="1.0.0",
+    description="ALeRCE Alert Processing Framework.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="ALeRCE Team",
+    author_email="contact@alerce.online",
+    packages=find_namespace_packages(include=["apf.*"]),
+    scripts=["scripts/apf"],
+    install_requires=required_packages,
+    build_requires=required_packages,
+    project_urls={
+        "Github": "https://github.com/alercebroker/APF",
+        "Documentation": "https://apf.readthedocs.io/en/latest/index.html",
+    },
 )
