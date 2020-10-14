034957d16a53d0facbd8ba6efc7c95e823fa9019
Diego Rodriguez <diegorodriguezmancini@gmail.com>
2020-10-14 16:22:16 -0300

    added extra metrics with apf 1.0.1

--- settings.py
+++ settings.py
@@ -16,6 +16,10 @@ DB_CONFIG = {
 
 METRICS_CONFIG = {
     "CLASS": "apf.metrics.KafkaMetricsProducer",
+    "EXTRA_METRICS": [
+        {"key": "candid", "format": lambda x: str(x)},
+        {"key": "objectId", "alias": "oid"},
+    ],
     "PARAMS": {
         "PARAMS": {
             "bootstrap.servers": os.environ["METRICS_HOST"],
