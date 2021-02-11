628eb60923d2258971e2e5214f1b0a584d6dcf97
Ignacio Reyes <ireyesj93@gmail.com>
2021-02-11 11:55:03 -0300

    gcc flags for speed up

--- setup.py
+++ setup.py
@@ -1,28 +1,42 @@
 from setuptools import setup
 from setuptools.extension import Extension
 from Cython.Build import cythonize
-import numpy
+import os
+import numpy as np
 
 module_name = "mhps"
 pyx_path = [f"{module_name}/{module_name}_wrapper.pyx", f"{module_name}/lib/functions.c"]
 headers_path = [f"{module_name}/lib/functions.h"]
 lib_path = f"{module_name}/lib"
 
-paps_extension = Extension(
+library_dirs = []
+if os.name == 'nt':  # Windows, assumming MSVC compiler
+    libraries = []
+    compiler_args = ['/Ox', '/fp:fast']
+elif os.name == 'posix':  # UNIX, assumming GCC compiler
+    libraries = ['m']
+    compiler_args = ['-O3', '-ffast-math', '-march=native']
+else:
+    raise Exception('Unsupported operating system')
+
+mhps_extension = Extension(
     name=f"{module_name}.{module_name}_wrapper",
     sources=pyx_path,
+    extra_compile_args=compiler_args,
+    libraries=libraries,
+    library_dirs=library_dirs,
     depends=headers_path,
-    include_dirs=[lib_path,numpy.get_include(), lib_path]
+    include_dirs=[lib_path, np.get_include()]
 )
 
 setup(
     name=module_name,
-    ext_modules=cythonize([paps_extension]),
+    ext_modules=cythonize([mhps_extension], force=True),
     install_requires=["numpy>=1.17.4", "Cython>=0.29.12"],
     build_requires=["Cython>=0.29.12", "numpy>=1.17.4"],
     packages=[module_name],
-    url='https://github.com/alercebroker/paps',
-    description='Patricia Arevalo Power Spectra https://arxiv.org/abs/1207.5825',
+    url='https://github.com/alercebroker/mhps',
+    description='Mexican Hat Power Spectra https://arxiv.org/abs/1207.5825',
     author='ALeRCE',
     version='0.0.1',
 )
