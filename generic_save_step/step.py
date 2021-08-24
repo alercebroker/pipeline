bbf1f4529239650742780cc40cd28054e0807fff
EC2 Default User <ec2-user@ip-10-0-3-119.ec2.internal>
2021-08-24 04:41:24 +0000

    new file:   Dockerfile 	new file:   generic_save_step/__init__.py 	new file:   generic_save_step/step.py 	new file:   requirements.txt 	new file:   scripts/run_multiprocess.py 	new file:   scripts/run_step.py 	new file:   settings.py

---
+++ generic_save_step/step.py
@@ -0,0 +1,24 @@
+from apf.core.step import GenericStep
+import logging
+
+
+class GenericSaveStep(GenericStep):
+    """GenericSaveStep Description
+
+    Parameters
+    ----------
+    consumer : GenericConsumer
+        Description of parameter `consumer`.
+    **step_args : type
+        Other args passed to step (DB connections, API requests, etc.)
+
+    """
+    def __init__(self,consumer = None, config = None,level = logging.INFO,**step_args):
+        super().__init__(consumer,config=config, level=level)
+
+    def execute(self,message):
+        ################################
+        #   Here comes the Step Logic  #
+        ################################
+
+        pass
