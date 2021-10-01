4727f0334d3371069c4d59b6265b5b5a7d779e30
amoya87 <albertomoya87@gmail.com>
2021-10-01 12:38:26 -0300

    integrated with atlas alerts

--- generic_save_step/step.py
+++ generic_save_step/step.py
@@ -2,6 +2,8 @@ from apf.core.step import GenericStep
 import logging
 from apf.core import get_class
 from apf.producers import KafkaProducer
+import sys
+sys.path.insert(0, '../../../../')
 
 from db_plugins.db.generic import new_DBConnection
 from db_plugins.db.models import (
@@ -12,6 +14,128 @@ from db_plugins.db.mongo.connection import MongoDatabaseCreator
 
 import numpy as np
 import pandas as pd
+
+class GeneratorID:
+    @classmethod
+    def generate_aid(self, ra, dec):
+        # 19 Digit ID - two spare at the end for up to 100 duplicates
+        id = 1000000000000000000
+
+        # 2013-11-15 KWS Altered code to fix the negative RA problem
+        if ra < 0.0:
+            ra += 360.0
+
+        if ra > 360.0:
+            ra -= 360.0
+
+        # Calculation assumes Decimal Degrees:
+
+        ra_hh = int(ra / 15)
+        ra_mm = int((ra / 15 - ra_hh) * 60)
+        ra_ss = int(((ra / 15 - ra_hh) * 60 - ra_mm) * 60)
+        ra_ff = int((((ra / 15 - ra_hh) * 60 - ra_mm) * 60 - ra_ss) * 100)
+
+        if dec >= 0:
+            h = 1
+        else:
+            h = 0
+            dec = dec * -1
+
+        dec_deg = int(dec)
+        dec_mm = int((dec - dec_deg) * 60)
+        dec_ss = int(((dec - dec_deg) * 60 - dec_mm) * 60)
+        dec_f = int(((((dec - dec_deg) * 60 - dec_mm) * 60) - dec_ss) * 10)
+
+        id += (ra_hh * 10000000000000000)
+        id += (ra_mm * 100000000000000)
+        id += (ra_ss * 1000000000000)
+        id += (ra_ff * 10000000000)
+
+        id += (h * 1000000000)
+        id += (dec_deg * 10000000)
+        id += (dec_mm * 100000)
+        id += (dec_ss * 1000)
+        id += (dec_f * 100)
+
+        return id
+
+class ParserSelector:
+    parsers=[]
+
+    def registerParser(self, parser):
+        self.parsers.append(parser)
+
+    def parse(self, alerts):
+        for parser in self.parsers:
+            if parser.canParseFile(alerts):
+                return parser.parse(alerts)
+
+import abc
+class ParserGeneric(abc.ABC):
+    @abc.abstractmethod
+    def parse(self):
+        """
+        Note that the Creator may also provide some default implementation of
+        the factory method.
+        """
+        pass
+
+    @abc.abstractmethod
+    def canParseFile(self):
+        """
+        Note that the Creator may also provide some default implementation of
+        the factory method.
+        """
+        pass
+
+class ParserATLAS(ParserGeneric):
+    source = "ATLAS"
+
+    @classmethod
+    def parse(self, messages):
+        for eachdict in messages:
+            eachdict['candidate']['ra'] = eachdict['candidate']['RA']
+            del eachdict['candidate']['RA']
+            eachdict['candidate']['dec'] = eachdict['candidate']['Dec']
+            del eachdict['candidate']['Dec']
+            eachdict['candidate']['xpos'] = eachdict['candidate']['X']
+            del eachdict['candidate']['X']
+            eachdict['candidate']['ypos'] = eachdict['candidate']['Y']
+            del eachdict['candidate']['Y']
+            eachdict['candidate']['magpsf'] = eachdict['candidate']['Mag']
+            del eachdict['candidate']['Mag']
+            eachdict['candidate']['sigmapsf'] = eachdict['candidate']['Dmag']
+            del eachdict['candidate']['Dmag']
+            eachdict['candidate']['aimage'] = eachdict['candidate']['Major']
+            del eachdict['candidate']['Major']
+            eachdict['candidate']['bimage'] = eachdict['candidate']['Minor']
+            del eachdict['candidate']['Minor']
+            eachdict['surveyId'] = eachdict['objectId']
+            eachdict['alerceId'] = eachdict['objectId']
+        return messages
+
+    @classmethod
+    def canParseFile(self, messages):
+        for eachdict in messages:
+            if eachdict['publisher'] == self.source:
+                return True
+
+class ParserZTF(ParserGeneric):
+    source = "ZTF"
+
+    @classmethod
+    def parse(self, messages):
+        for eachdict in messages:
+            eachdict['surveyId'] = eachdict['objectId']
+            eachdict['alerceId'] = GeneratorID.generate_aid(eachdict['ra'],eachdict['dec'])
+            eachdict['mjd'] = eachdict["jd"] - 2400000.5
+        return messages
+
+    @classmethod
+    def canParseFile(self, messages):
+        for eachdict in messages:
+            if eachdict['publisher'] == self.source:
+                return True
 
 class GenericSaveStep(GenericStep):
     """GenericSaveStep Description
@@ -24,6 +148,8 @@ class GenericSaveStep(GenericStep):
         Other args passed to step (DB connections, API requests, etc.)
 
     """
+    parser = ParserSelector()
+
     def __init__(self, consumer = None, config = None, level = logging.INFO, producer=None, db_connection=None,
                  **step_args):
         super().__init__(consumer,config=config, level=level)
@@ -33,11 +159,12 @@ class GenericSaveStep(GenericStep):
         else:
             Producer = KafkaProducer
 
-        self.producer = producer or Producer(config["PRODUCER_CONFIG"])
+        self.producer = producer# or Producer(config["PRODUCER_CONFIG"])
         self.driver = db_connection or new_DBConnection(MongoDatabaseCreator)
-        self.driver.connect(config["DATABASE"])
+        self.driver.connect(config["DB_CONFIG"])
         self.version = config["STEP_METADATA"]["STEP_VERSION"]
         self.logger.info(f"SAVE {self.version}")
+        self.parser.registerParser(ParserATLAS)
 
     """
     def insert_step_metadata(self):
@@ -56,17 +183,9 @@ class GenericSaveStep(GenericStep):
         return pd.read_sql(query.statement, self.driver.engine)
 
     def remove_stamps(self, alerts):
-        for k in ["cutoutDifference", "cutoutScience", "cutoutTemplate"]:
+        #TODO añadir cutoutTemplate a atlas alerts
+        for k in ["cutoutDifference", "cutoutScience"]:#, "cutoutTemplate"]:
             del alerts[k]
-
-    def generate_aid(self, ra, dec):
-        pass
-
-    def preprocess_detections(self, detections) -> None:
-        detections.loc[:, "mjd"] = detections["jd"] - 2400000.5
-        detections.loc[:, "has_stamp"] = True
-        detections.loc[:, "step_id"] = self.version
-        return detections
     
     def insert_objects(self, objects):
         new_objects = objects["new"]
@@ -111,6 +230,14 @@ class GenericSaveStep(GenericStep):
 
         return pd.Series(response)
 
+    def get_last_alert(self, alerts):
+        last_alert = alerts.candid.values.argmax()
+        filtered_alerts = alerts.loc[
+                          :, ["oid", "ndethist", "ncovhist", "jdstarthist", "jdendhist"]
+                          ]
+        last_alert = filtered_alerts.iloc[last_alert]
+        return last_alert
+
     def preprocess_objects(self, objects, detections, alerts):
         oids = objects.oid.unique()
         apply_last_alert = lambda x: self.get_last_alert(x)
@@ -134,14 +261,30 @@ class GenericSaveStep(GenericStep):
 
         return new_objects
 
+    def preprocess_detections(self, detections):
+        oids = detections.oid.values
+        detectionsDB = self.get_detections(oids)
+
+        index_detections = pd.MultiIndex.from_frame(
+            detections[["oid", "candid"]]
+        )
+        index_light_curve_detections = pd.MultiIndex.from_frame(
+            detectionsDB[["oid", "candid"]]
+        )
+
+        all_detections = pd.concat(
+            [detectionsDB, detections], ignore_index=True
+        )
+
+        return all_detections
+
     def execute(self, messages):
-        global MAGSTATS_UPDATE_KEYS
-        global MAGSTATS_UPDATE_KEYS_STMT
         self.logger.info(f"Processing {len(messages)} alerts")
 
         # Casting to a dataframe
         self.logger.info(f"Preprocessing alerts")
 
+        self.parser.parse(messages)
         alerts = pd.DataFrame(messages)
         alerts.drop_duplicates("candid", inplace=True)
         alerts.reset_index(inplace=True)
@@ -151,25 +294,23 @@ class GenericSaveStep(GenericStep):
         # Getting just the detections
         self.logger.info(f"Doing correction to detections")
         detections = pd.DataFrame(list(alerts["candidate"]))
-        detections.loc[:, "survey_id"] = alerts["objectId"]
-        detections.loc[:, "alerce_id"] = self.generate_aid(alerts["ra"], alerts["dec"])
-        detections = self.preprocess_detections(detections)
+        detections.loc[:, "sid"] = alerts["objectId"]
+        detections.loc[:, "aid"] = alerts["alerceId"]
 
         # Getting data from database, and processing prv_candidates
         self.logger.info(f"Processing light curves")
-        light_curves = self.preprocess_lightcurves(detections, alerts)
+        all_detections = self.preprocess_detections(detections)
 
         # Getting other tables
         objects = self.get_objects(detections["oid"].unique())
-        objects = self.preprocess_objects(objects, light_curves, detections)
+        objects = self.preprocess_objects(objects, all_detections, detections)
 
         # Insert new objects and update old objects
         self.insert_objects(objects)
-        new_detections = light_curves["detections"]["new"]
-        self.insert_detections(light_curves["detections"].loc[new_detections])
+        new_detections = all_detections["new"]
+        self.insert_detections(all_detections.loc[new_detections])
 
-        self.produce(alerts, light_curves)
+        #self.produce(alerts, all_detections)
 
         del alerts
         del detections
-        del light_curves
