8beaf7d8ffb9aa94aefe497bf3f018fe6e18732b
Javier <javier.arredondo.c@usach.cl>
2021-10-13 12:26:46 -0300

    FIX: Fixed setup.py for install package

--- setup.py
+++ setup.py
@@ -12,8 +12,7 @@ setup(
     description="ALeRCE Survey Parser Plugins",
     author="ALeRCE Team",
     author_email="contact@alerce.online",
-    packages=find_namespace_packages(include=["db_plugins.*"]),
-    scripts=["scripts/spp"],
+    packages=find_namespace_packages(include=["survey_parser_plugins.*"]),
     install_requires=required_packages,
     build_requires=required_packages,
     project_urls={
