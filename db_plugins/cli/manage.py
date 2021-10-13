83f5812255fbaec7399d10bc43122c6dd9250d30
Diego Rodriguez Mancini <diegorodriguezmancini@gmail.com>
2021-10-13 12:29:21 -0300

    adding mongo db initialization

--- db_plugins/cli/manage.py
+++ db_plugins/cli/manage.py
@@ -1,25 +1,11 @@
-from db_plugins.db.sql.connection import SQLConnection, satisfy_keys, settings_map
-import alembic.config
 import click
 import sys
 import os
-
-
-MANAGE_PATH = os.path.dirname(os.path.abspath(__file__))
-MIGRATIONS_PATH = os.path.abspath(os.path.join(MANAGE_PATH, "../db/sql/"))
-
-
-def init_sql(config, db=None):
-    db = db or SQLConnection()
-    db.connect(config=config)
-    db.create_db()
-    db.session.close()
-    os.chdir(MIGRATIONS_PATH)
-    try:
-        os.makedirs(os.getcwd() + "/migrations/versions")
-    except FileExistsError:
-        pass
-    alembic.config.main(["stamp", "head"])
+from db_plugins.db.sql import (
+    init as init_sql,
+    make_migrations as make_sql_migrations,
+    migrate as migrate_sql,
+)
 
 
 @click.group()
@@ -35,21 +21,10 @@ def initdb(settings_path):
     sys.path.append(os.path.dirname(os.path.expanduser(settings_path)))
     from settings import DB_CONFIG
 
-    db_config = DB_CONFIG["SQL"]
-
     if "SQL" in DB_CONFIG:
-        required_key = satisfy_keys(set(db_config.keys()))
-        if len(required_key) == 0:
-            db_config["SQLALCHEMY_DATABASE_URL"] = settings_map(db_config)
-
-        if "SQLALCHEMY_DATABASE_URL" in db_config:
-            init_sql(db_config)
-        else:
-            raise Exception(
-                f"Missing arguments 'SQLALCHEMY_DATABASE_URL' or {required_key}"
-            )
-
+        init_sql
         click.echo("Database created with credentials from {}".format(settings_path))
+
     elif "MONGO" in DB_CONFIG:
         pass
 
@@ -64,9 +39,14 @@ def make_migrations(settings_path):
         raise Exception("Settings file not found")
     sys.path.append(os.path.abspath(settings_path))
 
-    os.chdir(MIGRATIONS_PATH)
-    alembicArgs = ["--raiseerr", "revision", "--autogenerate", "-m", "tables"]
-    alembic.config.main(alembicArgs)
+    from settings import DB_CONFIG
+
+    if "SQL" in DB_CONFIG:
+        make_sql_migrations()
+        click.echo("Migrations made with config from {}".format(settings_path))
+
+    else:
+        raise Exception("Invalid settings file")
 
 
 @cli.command()
@@ -76,6 +56,11 @@ def migrate(settings_path):
         raise Exception("Settings file not found")
     sys.path.append(os.path.abspath(settings_path))
 
-    os.chdir(MIGRATIONS_PATH)
-    alembicArgs = ["--raiseerr", "upgrade", "head"]
-    alembic.config.main(alembicArgs)
+    from settings import DB_CONFIG
+
+    if "SQL" in DB_CONFIG:
+        migrate_sql()
+        click.echo("Migrated database with config from {}".format(settings_path))
+
+    else:
+        raise Exception("Invalid settings file")
---
+++ db_plugins/db/sql/__init__.py
@@ -0,0 +1,5 @@
+from .connection import *
+from .query import *
+from .models import *
+from .serializers import *
+from .initialization import init, make_migrations, migrate
