2698dc9a9e36b6bad8afaf3f0f7b74102b924f8a
reyesdejong <reyes.dejong@gmail.com>
2021-10-13 15:32:32 -0300

    feature(ATLASParser): parse_message

--- survey_parser_plugins/parsers/ATLASParser.py
+++ survey_parser_plugins/parsers/ATLASParser.py
@@ -3,25 +3,37 @@ from survey_parser_plugins.core import GenericAlert, SurveyParser
 
 class ATLASParser(SurveyParser):
     _source = "ATLAS"
+    _exclude_keys = ["candid", "mjd", "RA", "Dec", "Mag", "Dmag",
+                     "Major", "Minor", "X", "Y"]
 
     @classmethod
     def parse_message(cls, message: dict, extra_fields: bool = False) -> GenericAlert:
-        return GenericAlert(
-            "",  # surveyid
-            cls._source,
-            0,  # candid
-            0,  # mjd
-            0,  # fid
-            0,  # ra
-            0,  # dec
-            0,  # rb
-            0,  # mag
-            0,  # sigmag
-            0,
-            0,
-            0,
-            0,
-        )
+        try:
+            oid = message['objectId']
+            message = message['candidate'].copy()
+            return GenericAlert(
+                survey_id=oid,
+                survey_name=cls._source,
+                candid=message['candid'],
+                mjd=message['mjd'],
+                fid=-1,
+                ra=message['RA'],
+                dec=message['Dec'],
+                rb=-1,
+                mag=message['Mag'],
+                sigmag=message['Dmag'],
+                aimage=message['Major'],
+                bimage=message['Minor'],
+                xpos=message['X'],
+                ypos=message['Y'],
+                extra_fields={
+                    k: message[k]
+                    for k in message.keys()
+                    if k not in cls._exclude_keys
+                    } if extra_fields else {}
+                )
+        except KeyError:
+            raise KeyError("This parser can't parse message")
 
     @classmethod
     def get_source(cls):
