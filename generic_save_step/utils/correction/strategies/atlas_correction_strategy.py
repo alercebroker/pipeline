47b01f3cae0293be70f347edc5840d04cc813f3a
Javier <javier.arredondo.c@usach.cl>
2021-11-04 12:31:50 -0300

    fix(atlas correction): dummy correction in atlas

--- generic_save_step/utils/correction/strategies/atlas_correction_strategy.py
+++ generic_save_step/utils/correction/strategies/atlas_correction_strategy.py
@@ -1,4 +1,3 @@
-import numpy as np
 import pandas as pd
 
 from .base_correction_strategy import BaseCorrectionStrategy
@@ -6,11 +5,4 @@ from .base_correction_strategy import BaseCorrectionStrategy
 
 class ATLASCorrectionStrategy(BaseCorrectionStrategy):
     def do_correction(self, detections: pd.DataFrame) -> pd.DataFrame:
-        detections["magpsf_corr"] = np.nan
-        detections["sigmapsf_corr"] = np.nan
-        detections["sigmapsf_corr_ext"] = np.nan
-        detections["corrected"] = np.nan
-        detections["dubious"] = np.nan
-        detections["parent_candid"] = np.nan
-        detections["step_id_corr"] = np.nan
         return detections
