94ec7b8fecaf1239d4468668fab59216ee5b971a
Diego Rodriguez Mancini <diegorodriguezmancini@gmail.com>
2021-11-05 17:32:51 -0300

    Add: tests for inserting objects, detections and non_detections

--- tests/integration/test_db.py
+++ tests/integration/test_db.py
@@ -3,7 +3,8 @@ import unittest
 from apf.producers.kafka import KafkaProducer
 from unittest import mock
 from generic_save_step.step import GenericSaveStep
-from db_plugins.db.mongo.models import Object, Detection
+from db_plugins.db.mongo.models import Object, Detection, NonDetection
+import pandas as pd
 
 
 @pytest.mark.usefixtures("mongo_service")
@@ -55,6 +56,7 @@ class MongoIntegrationTest(unittest.TestCase):
         self.step.driver.create_db()
         self.insert_objects()
         self.insert_detections()
+        self.insert_non_detections()
 
     def tearDown(self):
         self.step.driver.drop_db()
@@ -100,6 +102,18 @@ class MongoIntegrationTest(unittest.TestCase):
             },
         )
 
+    def insert_non_detections(self):
+        ins, created = self.step.driver.query().get_or_create(
+            model=NonDetection,
+            filter_by={
+                "aid": "alerce1",
+                "tid": "ZTF",
+            },
+            mjd=1,
+            diffmaglim=1,
+            fid=1,
+        )
+
     def test_get_objects(self):
         objs = self.step.get_objects(["alerce1"])
         self.assertEqual(len(objs), 1)
@@ -107,3 +121,90 @@ class MongoIntegrationTest(unittest.TestCase):
     def test_get_detections(self):
         dets = self.step.get_detections(["alerce1"])
         self.assertEqual(len(dets), 1)
+
+    def test_get_non_detections(self):
+        non_dets = self.step.get_detections(["alerce1"])
+        self.assertEqual(len(non_dets), 1)
+
+    def test_insert_objects(self):
+        objs = pd.DataFrame(
+            {
+                "aid": ["alerce1", "alerce2"],
+                "oid": ["ZTF2", "ATLAS1"],
+                "lastmjd": [1, 2],
+                "firstmjd": [1, 2],
+                "ndet": [1, 2],
+                "meanra": [1, 2],
+                "meandec": [1, 2],
+                "new": [False, True],
+            }
+        )
+        self.step.insert_objects(objs)
+        found = list(
+            self.step.driver.query().find_all(
+                model=Object, filter_by={"aid": "alerce1"}, paginate=False
+            )
+        )
+        self.assertEqual(len(found), 1)
+        self.assertEqual(found[0]["oid"], objs.iloc[0].oid)
+        found = list(
+            self.step.driver.query().find_all(
+                model=Object, filter_by={"aid": "alerce2"}, paginate=False
+            )
+        )
+        self.assertEqual(len(found), 1)
+        self.assertEqual(found[0]["oid"], objs.iloc[1].oid)
+
+    def test_insert_detections(self):
+        objs = pd.DataFrame(
+            {
+                "aid": ["alerce2"],
+                "tid": ["ZTF"],
+                "candid": ["candid1"],
+                "mjd": [1],
+                "fid": [1],
+                "ra": [0],
+                "dec": [0],
+                "rb": [0],
+                "mag": [0],
+                "e_mag": [0],
+                "rfid": [0],
+                "e_ra": [0],
+                "e_dec": [0],
+                "isdiffpos": [0],
+                "corrected": [0],
+                "parent_candid": [0],
+                "has_stamp": [0],
+                "step_id_corr": [0],
+                "rbversion": [0],
+            }
+        )
+        self.step.insert_detections(objs)
+        found = list(
+            self.step.driver.query().find_all(
+                model=Detection, filter_by={"aid": "alerce2"}, paginate=False
+            )
+        )
+        self.assertEqual(len(found), 1)
+        self.assertEqual(found[0]["aid"], objs.iloc[0].aid)
+
+    def test_insert_non_detections(self):
+        objs = pd.DataFrame(
+            {
+                "aid": ["alerce2"],
+                "tid": ["ZTF"],
+                "mjd": [1],
+                "fid": [1],
+                "diffmaglim": [0],
+            }
+        )
+        self.step.insert_non_detections(objs)
+        found = list(
+            self.step.driver.query().find_all(
+                model=NonDetection,
+                filter_by={"aid": "alerce2"},
+                paginate=False,
+            )
+        )
+        self.assertEqual(len(found), 1)
+        self.assertEqual(found[0]["aid"], objs.iloc[0].aid)
