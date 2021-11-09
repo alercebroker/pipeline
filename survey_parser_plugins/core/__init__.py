f5e91333f43643cd2101d0c18f197edc0e891f81
Javier <javier.arredondo.c@usach.cl>
2021-11-09 09:27:10 -0300

    refactor(remove aid): parser don't create aid

--- survey_parser_plugins/core/__init__.py
+++ survey_parser_plugins/core/__init__.py
@@ -1,5 +1,4 @@
 from .generic import SurveyParser, GenericAlert
 from .selector import ParserSelector, ALeRCEParser
-from .id_generator import id_generator
 
-__all__ = ["SurveyParser", "GenericAlert", "ParserSelector", "ALeRCEParser", "id_generator"]
+__all__ = ["SurveyParser", "GenericAlert", "ParserSelector", "ALeRCEParser"]
