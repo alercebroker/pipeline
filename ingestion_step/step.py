fix(e_coords): fix formula of error in coordinates

ingestion_step/step.py
@@ -225,10 +225,12 @@ class IngestionStep(GenericStep):
 
     @classmethod
     def calculate_stats_coordinates(cls, coordinates, e_coordinates):
+        e_coordinates = e_coordinates / 3600
         num_coordinate = np.sum(coordinates / e_coordinates ** 2)
         den_coordinate = np.sum(1 / e_coordinates ** 2)
         mean_coordinate = num_coordinate / den_coordinate
-        return mean_coordinate, den_coordinate
+        e_coord = np.sqrt(1 / den_coordinate) * 3600
+        return mean_coordinate, e_coord
 
     def compute_meanra(self, ras, e_ras):
         mean_ra, e_ra = self.calculate_stats_coordinates(ras, e_ras)