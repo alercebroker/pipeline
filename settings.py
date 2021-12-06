755558ea3143f93ad1e60e86629137ad1592cc1f
amoya87 <albertomoya87@gmail.com>
2021-12-06 14:25:08 -0300

    fix(concat/setting) prepare to run with docker-compose

--- settings.py
+++ settings.py
@@ -3,13 +3,12 @@
 ##################################################
 import os
 
-## Set the global logging level to debug
-#LOGGING_DEBUG = True
+# Set the global logging level to debug
+LOGGING_DEBUG = os.getenv("LOGGING_DEBUG", False)
 
 ## Consumer configuration
 ### Each consumer has different parameters and can be found in the documentation
 CONSUMER_CONFIG = {
-    "TOPICS": os.environ["CONSUMER_TOPICS"].strip().split(","),
     "PARAMS": {
         "bootstrap.servers": os.environ["CONSUMER_SERVER"],
         "group.id": os.environ["CONSUMER_GROUP_ID"],
@@ -20,10 +19,24 @@ CONSUMER_CONFIG = {
     "consume.messages": int(os.getenv("CONSUME_MESSAGES", 2200)),
 }
 
+if os.getenv("TOPIC_STRATEGY_FORMAT"):
+    CONSUMER_CONFIG["TOPIC_STRATEGY"] = {
+        "CLASS": "apf.core.topic_management.DailyTopicStrategy",
+        "PARAMS": {
+            "topic_format": os.environ["TOPIC_STRATEGY_FORMAT"].strip().split(","),
+            "date_format": "%Y%m%d",
+            "change_hour": 23,
+        },
+    }
+elif os.getenv("CONSUMER_TOPICS"):
+    CONSUMER_CONFIG["TOPICS"] = os.environ["CONSUMER_TOPICS"].strip().split(",")
+else:
+    raise Exception("Add TOPIC_STRATEGY or CONSUMER_TOPICS")
+
 STEP_METADATA = {
     "STEP_VERSION": os.getenv("STEP_VERSION", "dev"),
-    "STEP_ID": os.getenv("STEP_ID", "preprocess"),
-    "STEP_NAME": os.getenv("STEP_NAME", "preprocess"),
+    "STEP_ID": os.getenv("STEP_ID", "archiving"),
+    "STEP_NAME": os.getenv("STEP_NAME", "archiving"),
     "STEP_COMMENTS": os.getenv("STEP_COMMENTS", ""),
 }
 
@@ -34,4 +47,6 @@ STEP_CONFIG = {
     "N_PROCESS": os.getenv("N_PROCESS"),            # Number of process for multiprocess script
     "CONSUMER_CONFIG": CONSUMER_CONFIG,
     "STEP_METADATA": STEP_METADATA,
+    "FORMAT": "avro",
+    "BUCKET_NAME": "astro-alerts-archive",
 }
