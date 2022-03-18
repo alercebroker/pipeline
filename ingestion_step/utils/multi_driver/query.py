8f70312aa0ce30c7858750aa8e1e7a8b234e15d5
Javier <javier.arredondo.c@usach.cl>
2022-03-18 17:15:50 -0300

    feat(multi query): do update, insert and selects

--- ingestion_step/utils/multi_driver/query.py
+++ ingestion_step/utils/multi_driver/query.py
@@ -1,11 +1,58 @@
 from db_plugins.db.generic import BaseQuery
 from db_plugins.db.mongo import MongoConnection
 from db_plugins.db.sql import SQLConnection
+from typing import List
+from sqlalchemy import and_
+from sqlalchemy.sql.expression import bindparam
+
+import db_plugins.db.mongo.models as mongo_models
+import db_plugins.db.sql.models as psql_models
+
+MODELS = {
+    "psql": {
+        "Object": psql_models.Object,
+        "Detection": psql_models.Detection
+    },
+    "mongo": {
+        "Object": mongo_models.Object
+    }
+}
+
+
+def filter_to_psql(model: object, filter_by: dict):
+    filters = []
+    for attribute, _filter in filter_by.items():
+        if "$in" in _filter:
+            f = getattr(model, "oid").in_(_filter["$in"])
+            filters.append(f)
+    if len(filters) == 1:
+        return filters[0]
+    elif len(filters) > 1:
+        return and_(*filters)
+    return {}
+
+
+def update_to_psql(model: object, filter_by: List[dict]):
+    filters = []
+    for attribute, _filter in filter_by[0].items():
+        if attribute == "_id" and model is psql_models.Object:
+            f = getattr(model, "oid") == bindparam("oid")
+            filters.append(f)
+    if len(filters) == 1:
+        return filters[0]
+    elif len(filters) > 1:
+        return and_(*filters)
+    return {}
 
 
 class MultiQuery(BaseQuery):
-    def __init__(self, psql_driver: SQLConnection, mongo_driver: MongoConnection, model=None, *args, **kwargs):
-        self.query_class = model
+    def __init__(self,
+                 psql_driver: SQLConnection,
+                 mongo_driver: MongoConnection,
+                 model=None,
+                 *args,
+                 **kwargs):
+        self.model = model
         self.psql = psql_driver
         self.mongo = mongo_driver
         self.engine = kwargs.get("engine", "mongo")
@@ -21,29 +68,53 @@ class MultiQuery(BaseQuery):
         """Update a model instance with specified args."""
         raise NotImplementedError()
 
+    def bulk_update(self, to_update: List, filter_by: List[dict]):
+        try:
+            model = MODELS[self.engine][self.model]
+        except Exception as e:
+            raise Exception(f"Indicates model on query() method: {e}")
+
+        if self.engine == "mongo":
+            to_update = [model(**x) for x in to_update]
+            return self.mongo.query().bulk_update(to_update, to_update, filter_fields=filter_by)
+        elif self.engine == "psql":
+            bind_object = to_update[0]
+            where_clause = update_to_psql(model, filter_by)
+            params_statement = dict(zip(bind_object.keys(), map(bindparam, bind_object.keys())))
+            statement = model.__table__.update().where(where_clause).values(params_statement)
+            return self.psql.engine.execute(statement, to_update)
+        raise NotImplementedError()
+
     def paginate(self, page=1, per_page=10, count=True):
         """Return a pagination object from this query."""
         raise NotImplementedError()
 
-    def bulk_insert(self, objects, model):
-        self.mongo.query().bulk_insert(objects, model)
-        # objects will be change
-        # model
-        self.psql.query().bulk_insert(objects, model)
+    def bulk_insert(self, objects: List[object]):
+        try:
+            model = MODELS[self.engine][self.model]
+        except Exception as e:
+            raise Exception(f"Indicates model on query() method: {e}")
+
+        if self.engine == "mongo":
+            self.mongo.query().bulk_insert(objects, model)
+        elif self.engine == "psql":
+            self.psql.query().bulk_insert(objects, model)
+        else:
+            raise NotImplementedError()
 
     def find_all(self, filter_by={}, paginate=True):
         """Retrieve all items from the result of this query."""
+        try:
+            model = MODELS[self.engine][self.model]
+        except Exception as e:
+            raise Exception(f"Indicates model on query() method: {e}")
+
         if self.engine == "mongo":
-            if self.query_class:
-                return self.mongo.query().find_all(model=self.query_class, filter_by=filter_by, paginate=paginate)
-            else:
-                return self.mongo.query().find_all(filter_by=filter_by, paginate=paginate)
-
+            cursor = self.mongo.query().find_all(model=model, filter_by=filter_by, paginate=paginate)
+            return [x for x in cursor]
         elif self.engine == "psql":
-            return self.psql.query(self.query_class).find_all()
-
-        elif self.engine == "both":
-            pass
+            filter_by = filter_to_psql(model, filter_by)
+            return self.psql.query().find_all(model=model, filter_by=filter_by, paginate=paginate)
         else:
             raise NotImplementedError()
 
--- tests/integration/test_multi_driver.py
+++ tests/integration/test_multi_driver.py
@@ -4,7 +4,6 @@ import db_plugins.db.mongo.models as mongo_models
 import db_plugins.db.sql.models as psql_models
 
 from ingestion_step.utils.multi_driver.connection import MultiDriverConnection
-from pymongo.cursor import Cursor as MongoCursor
 
 CONFIG = {
     "PSQL": {
@@ -35,7 +34,9 @@ class MultiDriverTest(unittest.TestCase):
         cls.driver.connect()
         cls.driver.create_db()
         cls.n_objects_mongo = 100
+        cls.n_objects_psql = 100
         cls.insert_data_mongo()
+        cls.insert_data_psql()
 
     @classmethod
     def insert_data_mongo(cls):
@@ -44,7 +45,7 @@ class MultiDriverTest(unittest.TestCase):
                 model=mongo_models.Object,
                 filter_by={
                     "aid": f"alerce{o}",
-                    "oid": "ZTF1",
+                    "oid": f"ZTF{o}",
                     "lastmjd": 1,
                     "firstmjd": 1,
                     "ndet": 1,
@@ -55,12 +56,101 @@ class MultiDriverTest(unittest.TestCase):
             )
 
     @classmethod
-    def tearDownClass(cls):
-        cls.driver.drop_db()
+    def insert_data_psql(cls):
+        for o in range(0, cls.n_objects_psql):
+            cls.driver.psql_driver.query().get_or_create(
+                psql_models.Object,
+                {
+                    "oid": f"ZTF{o}",
+                    "lastmjd": 1,
+                    "firstmjd": 1,
+                    "ndet": 1,
+                    "meanra": 0,
+                    "meandec": 0,
+                }
+            )
 
-    def test_find_all(self):
-        mongo_objects = self.driver.query(mongo_models.Object, engine="mongo").find_all(paginate=False)
-        self.assertIsInstance(mongo_objects, MongoCursor)
+    def test_bulk_insert_mongo(self):
+        data = [
+            {
+                "aid": f"alerce{o}",
+                "oid": f"ZTF{o}",
+                "lastmjd": 1,
+                "firstmjd": 1,
+                "ndet": 1,
+                "meanra": 0,
+                "meandec": 0,
+                "_id": f"alerce{o}"
+            }
+            for o in range(self.n_objects_mongo, self.n_objects_mongo+100)
+        ]
+        self.driver.query("Object", engine="mongo").bulk_insert(data)
+
+    def test_bulk_insert_psql(self):
+        data = [
+            {
+                "aid": f"alerce{o}",
+                "oid": f"ZTF{o}",
+                "lastmjd": 1,
+                "firstmjd": 1,
+                "ndet": 1,
+                "meanra": 0,
+                "meandec": 0,
+            }
+            for o in range(self.n_objects_mongo, self.n_objects_psql+100)
+        ]
+        self.driver.query("Object", engine="psql").bulk_insert(data)
+
+    def test_bulk_update_mongo(self):
+        data = [
+            {
+                "aid": f"alerce{o}",
+                "oid": f"ZTF{o}",
+                "lastmjd": 10,
+                "firstmjd": 12,
+                "ndet": 10,
+                "meanra": 0,
+                "meandec": 0,
+            }
+            for o in range(0, 10)
+        ]
+        filter_by = [{"_id": f"alerce{x}"} for x in range(0, 10)]
+        self.driver.query("Object", engine="mongo").bulk_update(data, filter_by)
+
+    def test_bulk_update_psql(self):
+        data = [
+            {
+                "oid": f"ZTF{o}",
+                "lastmjd": 10,
+                "firstmjd": 12,
+                "ndet": 10,
+                "meanra": 0,
+                "meandec": 0,
+            }
+            for o in range(0, 10)
+        ]
+        filter_by = [{"_id": f"ZTF{x}"} for x in range(0, 10)]
+        self.driver.query("Object", engine="psql").bulk_update(data, filter_by)
+
+    def test_find_all_mongo(self):
+        mongo_objects = self.driver.query("Object", engine="mongo").find_all(paginate=False)
+        self.assertIsInstance(mongo_objects, list)
         mongo_objects = [x for x in mongo_objects]
-        self.assertEqual(len(mongo_objects), self.n_objects_mongo)
+        self.assertEqual(len(mongo_objects), 200)
+
+        filter_by = {"aid": {"$in": ["alerce1", "alerce2"]}}
+        mongo_objects = self.driver.query("Object", engine="mongo").find_all(filter_by=filter_by, paginate=False)
+        self.assertIsInstance(mongo_objects, list)
+        mongo_objects = [x for x in mongo_objects]
+        self.assertEqual(len(mongo_objects), 2)
+
+    def test_find_all_psql(self):
+        psql_objects = self.driver.query("Object", engine="psql").find_all(paginate=False)
+        self.assertIsInstance(psql_objects, list)
+        self.assertEqual(len(psql_objects), 200)
+
+        filter_by = {"aid": {"$in": ["ZTF0", "ZTF3"]}}
+        psql_objects = self.driver.query("Object", engine="psql").find_all(filter_by=filter_by, paginate=False)
+        self.assertIsInstance(psql_objects, list)
+        self.assertEqual(len(psql_objects), 2)
 
