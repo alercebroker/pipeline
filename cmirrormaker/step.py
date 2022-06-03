0034d2ed9a937d0d470a647d1f58104a96e222ae
Pablo Castellanos <pablocastellanos@gmail.com>
2022-06-03 16:59:27 -0400

    Added producer

--- cmirrormaker/step.py
+++ cmirrormaker/step.py
@@ -1,4 +1,5 @@
 from apf.core.step import GenericStep
+from apf.producers import KafkaProducer
 import logging
 
 
@@ -15,8 +16,14 @@ class CustomMirrormaker(GenericStep):
         Logging level.
     """
 
-    def __init__(self, consumer=None, config=None, level=logging.INFO):
+    def __init__(self, consumer=None, config=None, producer=None, level=logging.INFO):
         super().__init__(consumer, config=config, level=level)
+        self.producer = producer
+        if config.get("PRODUCER_CONFIG", False):
+            self.producer = KafkaProducer(config["PRODUCER_CONFIG"])
+
+    def produce(self, message):
+        pass
 
     def execute(self, message):
         pass
