d904b90111dc70eb47913bd37274e94f21f65b00
Pablo Castellanos <pablocastellanos@gmail.com>
2022-06-07 11:04:39 -0400

    Added step metadata

--- settings.py
+++ settings.py
@@ -20,21 +20,20 @@ CONSUMER_CONFIG = {
     'consume.messages': int(os.getenv('CONSUME_MESSAGES', 1000))
 }
 
-if os.getenv("TOPIC_STRATEGY_FORMAT"):
-    CONSUMER_CONFIG["TOPIC_STRATEGY"] = {
-        "CLASS": "apf.core.topic_management.DailyTopicStrategy",
-        "PARAMS": {
-            "topic_format": os.environ["TOPIC_STRATEGY_FORMAT"].strip().split(","),
-            "date_format": "%Y%m%d",
-            "change_hour": 23,
+# TODO: Check whether both possibilities can apply or only one should be used
+if os.getenv('TOPIC_STRATEGY_FORMAT'):
+    CONSUMER_CONFIG['TOPIC_STRATEGY'] = {
+        'CLASS': 'apf.core.topic_management.DailyTopicStrategy',
+        'PARAMS': {
+            'topic_format': os.environ['TOPIC_STRATEGY_FORMAT'].strip().split(','),
+            'date_format': '%Y%m%d',
+            'change_hour': 23,
         },
     }
-elif os.getenv("CONSUMER_TOPICS"):
-    CONSUMER_CONFIG["TOPICS"] = (
-        os.environ["CONSUMER_TOPICS"].strip().split(",")
-    )
+elif os.getenv('CONSUMER_TOPICS'):
+    CONSUMER_CONFIG['TOPICS'] = os.environ['CONSUMER_TOPICS'].strip().split(',')
 else:
-    raise Exception("Add TOPIC_STRATEGY or CONSUMER_TOPICS")
+    raise Exception('Add TOPIC_STRATEGY or CONSUMER_TOPICS')
 
 PRODUCER_CONFIG = {
     'CLASS': 'cmirrormaker.utils.CustomKafkaProducer',
@@ -55,10 +54,18 @@ METRICS_CONFIG = {
     }
 }
 
+STEP_METADATA = {
+    'STEP_VERSION': os.getenv('STEP_VERSION', 'dev'),
+    'STEP_ID': os.getenv('STEP_ID', 'cmirrormaker'),
+    'STEP_NAME': os.getenv('STEP_NAME', 'cmirrormaker'),
+    'STEP_COMMENTS': os.getenv('STEP_COMMENTS', ''),
+}
+
 # Step Configuration
 STEP_CONFIG = {
     'CONSUMER_CONFIG': CONSUMER_CONFIG,
     'PRODUCER_CONFIG': PRODUCER_CONFIG,
     'METRICS_CONFIG': METRICS_CONFIG,
-    "N_PROCESS": os.getenv('N_PROCESS')
+    'N_PROCESS': os.getenv('N_PROCESS'),
+    'STEP_METADATA': STEP_METADATA
 }
