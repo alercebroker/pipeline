a55c6f725181ea590b1907b71395aacc6c3d67bf
Pablo Castellanos <pablocastellanos@gmail.com>
2022-06-07 10:53:36 -0400

    Configuration for consumer topics

--- settings.py
+++ settings.py
@@ -20,6 +20,22 @@ CONSUMER_CONFIG = {
     'consume.messages': int(os.getenv('CONSUME_MESSAGES', 1000))
 }
 
+if os.getenv("TOPIC_STRATEGY_FORMAT"):
+    CONSUMER_CONFIG["TOPIC_STRATEGY"] = {
+        "CLASS": "apf.core.topic_management.DailyTopicStrategy",
+        "PARAMS": {
+            "topic_format": os.environ["TOPIC_STRATEGY_FORMAT"].strip().split(","),
+            "date_format": "%Y%m%d",
+            "change_hour": 23,
+        },
+    }
+elif os.getenv("CONSUMER_TOPICS"):
+    CONSUMER_CONFIG["TOPICS"] = (
+        os.environ["CONSUMER_TOPICS"].strip().split(",")
+    )
+else:
+    raise Exception("Add TOPIC_STRATEGY or CONSUMER_TOPICS")
+
 PRODUCER_CONFIG = {
     'CLASS': 'cmirrormaker.utils.CustomKafkaProducer',
     'TOPIC': os.environ['PRODUCER_TOPIC'],
@@ -33,7 +49,7 @@ METRICS_CONFIG = {
     'PARAMS': {
         'PARAMS': {
             'bootstrap.servers': os.environ['METRICS_HOST'],
-            'auto.offset.reset': 'smallest'
+            # 'auto.offset.reset': 'smallest'
         },
         'TOPIC': os.environ['METRICS_TOPIC']
     }
