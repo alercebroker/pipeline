eb5029ea6e677ea1c9dd78142db80c87ce72384e
Pablo Castellanos <pablocastellanos@gmail.com>
2022-06-10 16:56:58 -0400

    Fix for black formatter

--- cmirrormaker/utils/consumer.py
+++ cmirrormaker/utils/consumer.py
@@ -3,5 +3,6 @@ from apf.consumers import KafkaConsumer
 
 class RawKafkaConsumer(KafkaConsumer):
     """Consumer that prevents deserialization of the message"""
+
     def _deserialize_message(self, message):
         return message
