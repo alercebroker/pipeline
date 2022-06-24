804dc9daaa33b7812e9e06980e9b70048cfaa4bc
Pablo Castellanos <pablocastellanos@gmail.com>
2022-06-24 14:44:43 -0400

    Finalized integration tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,7 @@
-import psycopg2
-import pytest
 import os
+import pytest
+
+from confluent_kafka.admin import AdminClient, NewTopic
 
 
 @pytest.fixture(scope="session")
@@ -10,25 +11,26 @@ def docker_compose_file(pytestconfig):
     )
 
 
-def is_responsive_psql(url):
-    try:
-        conn = psycopg2.connect(
-            "dbname='postgres' user='postgres' host=localhost password='postgres'"
-        )
-        conn.close()
-        return True
-    except Exception as e:
-        print(e)
-        return False
+def is_responsive_kafka(url):
+    client = AdminClient({"bootstrap.servers": url})
+    topics = ["test_topic"]
+    new_topics = [NewTopic(topic, num_partitions=1) for topic in topics]
+    fs = client.create_topics(new_topics)
+    for topic, f in fs.items():
+        try:
+            f.result()
+        except Exception as err:
+            print(err)
+            return False
+    return True
 
 
 @pytest.fixture(scope="session")
-def psql_service(docker_ip, docker_services):
+def kafka_service(docker_ip, docker_services):
     """Ensure that psql service is up and responsive."""
-    port = docker_services.port_for("postgres", 5432)
+    port = docker_services.port_for("kafka", 9092)
     server = "{}:{}".format(docker_ip, port)
-    print("psql", server)
     docker_services.wait_until_responsive(
-        timeout=30.0, pause=0.1, check=lambda: is_responsive_psql(server)
+        timeout=30.0, pause=0.1, check=lambda: is_responsive_kafka(server)
     )
     return server
