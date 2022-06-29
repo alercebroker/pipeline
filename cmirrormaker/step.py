27bea78c3e8a9df7af2292c448f421e44eaf9c55
Pablo Castellanos <pablocastellanos@gmail.com>
2022-06-29 13:29:15 -0400

    Applied black changes

--- cmirrormaker/step.py
+++ cmirrormaker/step.py
@@ -24,16 +24,16 @@ class CustomMirrormaker(GenericStep):
 
         if producer and "PRODUCER_CONFIG" in self.config:
             self.logger.warning(
-                'Producer is defined twice. Using PRODUCER_CONFIG'
+                "Producer is defined twice. Using PRODUCER_CONFIG"
             )
         self.use_message_topic = True
         if "PRODUCER_CONFIG" in self.config:
             pconfig = self.config["PRODUCER_CONFIG"]
             if "TOPIC" in pconfig or "TOPIC_STRATEGY" in pconfig:
                 self.use_message_topic = False
-            producer = get_class(pconfig.pop(
-                    "CLASS", "cmirrormaker.utils.RawKafkaProducer"
-            ))(pconfig)
+            producer = get_class(
+                pconfig.pop("CLASS", "cmirrormaker.utils.RawKafkaProducer")
+            )(pconfig)
         if producer is None:
             raise Exception("Kafka producer not configured in settings.py")
         self.producer = producer
