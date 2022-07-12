9bca9125701c0f110c4062095b24b882773d5a47
JavierArredondo <javier.arredondo.c@usach.cl>
2022-07-12 12:54:36 -0400

    chore(project): structure of plugin

---
+++ consolidated_metrics_step/step.py
@@ -0,0 +1,26 @@
+from apf.core.step import GenericStep
+
+import logging
+
+
+class ConsolidatedMetricsStep(GenericStep):
+    """ConsolidatedMetricsStep Description
+
+    Parameters
+    ----------
+    consumer : GenericConsumer
+        Description of parameter `consumer`.
+    **step_args : type
+        Other args passed to step (DB connections, API requests, etc.)
+
+    """
+
+    def __init__(self, consumer=None, config=None, level=logging.INFO, **step_args):
+        super().__init__(consumer, config=config, level=level)
+
+    def execute(self, message):
+        ################################
+        #   Here comes the Step Logic  #
+        ################################
+
+        pass
---
+++ settings.py
@@ -0,0 +1,16 @@
+##################################################
+#       consolidated_metrics_step   Settings File
+##################################################
+
+## Set the global logging level to debug
+# LOGGING_DEBUG = True
+
+## Consumer configuration
+### Each consumer has different parameters and can be found in the documentation
+CONSUMER_CONFIG = {}
+
+## Step Configuration
+STEP_CONFIG = {
+    # "N_PROCESS": 4,            # Number of process for multiprocess script
+    # "COMMIT": False,           #Disables commit, useful to debug a KafkaConsumer
+}
