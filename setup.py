8fd88ab25cce1e81591bd8f18e9e633b111ac303
Pablo Castellanos <pablocastellanos@gmail.com>
2022-07-13 14:36:48 -0400

    Updated version

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ with open(path.join(this_directory, "README.rst"), encoding="utf-8") as f:
 
 setup(
     name="db-plugins",
-    version="2.1.4",
+    version="2.1.5",
     description="ALeRCE Database Plugins.",
     long_description=long_description,
     long_description_content_type="text/x-rst",
