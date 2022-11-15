5f166cadb87fde6cf4758b3ea48036748a319183
Pedro <pegallardo@inf.udec.cl>
2022-11-15 11:08:20 -0300

    refactor(helpers): bulk insertion refactored

--- db_plugins/db/mongo/helpers/update_probs.py
+++ db_plugins/db/mongo/helpers/update_probs.py
@@ -1,12 +1,12 @@
 from db_plugins.db.mongo.connection import MongoConnection
-from db_plugins.db.mongo import models
+from pymongo import UpdateOne
 
 """
 Helper function to create or update the probabilities for an object
 """
 
 
-def create_or_update_probabilities(
+def _create_or_update_probabilities(
     connection: MongoConnection,
     classifier: str,
     version: str,
@@ -87,7 +87,7 @@ def create_or_update_probabilities(
             )
 
 
-def create_or_update_probabilities_bulk(
+def _create_or_update_probabilities_bulk(
     connection: MongoConnection,
     classifier: str,
     version: str,
@@ -106,6 +106,65 @@ def create_or_update_probabilities_bulk(
     """
 
     for indx in range(len(aids)):
-        create_or_update_probabilities(
+        _create_or_update_probabilities(
             connection, classifier, version, aids[indx], probabilities[indx]
         )
+
+
+def get_db_operations(
+    classifier: str,
+    version: str,
+    aid: str,
+    probabilities: dict
+):
+    """
+    Check if this is really efficient
+    """
+
+    db_operations = []
+
+    sorted_classes_and_prob_list = sorted(
+        probabilities.items(), key=lambda val: val[1], reverse=True
+    )
+
+    for n_item, (class_name, prob) in enumerate(sorted_classes_and_prob_list):
+        db_operations.append(
+            UpdateOne({
+                "aid": aid,
+                "probabilities": {
+                    "$elemMatch": {
+                        "classifier_name": classifier,
+                        "classifier_version": version,
+                        "class_name": class_name,
+                    }
+                },
+            },
+                {
+                    "$set": {
+                        "probabilities.$.probability": prob,
+                        "probabilities.$.ranking": n_item + 1,
+                    }
+            })
+        )
+
+    return db_operations
+
+
+def create_or_update_probabilities_bulk(
+    connection: MongoConnection,
+    classifier: str,
+    version: str,
+    aids: list,
+    probabilities: list
+):
+    """
+    Bulk update using the actual bulk object of pymongo
+    """
+    db_operations = []
+
+    for aid, probs in zip(aids, probabilities):
+        db_operations.extend(
+            get_db_operations(classifier, version, aid, probs)
+        )
+
+    connection.database["object"].bulk_write(db_operations)
