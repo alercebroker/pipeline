ea7bbfe2080735331d8961616778c9fe2b0cec79
Pedro Gallardo <pegallardo@inf.udec.cl>
2023-01-13 15:39:43 -0300

    fix: missing files added

---
+++ tests/unittests/mockdata.py
@@ -0,0 +1,13 @@
+valid_data_dict = {
+    "type": "insert",
+    "criteria": {"_id": "AID51423"},
+    "data": {"field1": "some_field", "field2": "some_other_field"},
+}
+
+valid_data_json = """
+{
+    "type": "insert",
+    "criteria": {"_id": "AID51423"},
+    "data": {"field1": "some_field", "field2": "some_other_field"}
+}
+"""
