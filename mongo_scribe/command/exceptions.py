2476b0fd753e57639d52c92de8fd9a853aea3b04
Pedro Gallardo <pegallardo@inf.udec.cl>
2023-02-13 15:48:23 -0300

    fix: some other minor changes

--- mongo_scribe/command/exceptions.py
+++ mongo_scribe/command/exceptions.py
@@ -1,23 +1,43 @@
 class NoDataProvidedException(Exception):
+    """
+    Exception to raise when the command doesn't have any data
+    """
+
     def __init__(self):
         super().__init__("The command must contain data to insert or update")
 
 
 class UpdateWithNoCriteriaException(Exception):
+    """
+    Exception to raise when an update command doesn't have a filter (or criteria)
+    """
+
     def __init__(self):
         super().__init__("The Update command must contain an update criteria")
 
 
 class MisformattedCommandExcepction(Exception):
+    """
+    Exception to raise when a dictionary doesn't have valid command fields
+    """
+
     def __init__(self):
         super().__init__("Received a misformatted message")
 
 
 class NonExistantCollectionException(Exception):
+    """
+    Exception to raise when trying to obtain a non-existant collection
+    """
+
     def __init__(self):
         super().__init__("Collection doesn't exist")
 
 
 class NoCollectionProvidedException(Exception):
+    """
+    Exception to raise when a command doesn't provide a collection to write on
+    """
+
     def __init__(self):
         super().__init__("No collection provided in the command")
--- mongo_scribe/db/update_one.py
+++ mongo_scribe/db/update_one.py
@@ -1,8 +1,11 @@
 from pymongo import UpdateOne
-from ..command.commands import DbCommand
+from mongo_scribe.command.commands import DbCommand
 
 
 def update_one_factory(command: DbCommand) -> UpdateOne:
+    """
+    Creates a Pymongo operation UpdateOne based on a update command
+    """
     if command.type != "update":
         raise Exception(
             "Can't create an UpdateOne instance from an insert command."
