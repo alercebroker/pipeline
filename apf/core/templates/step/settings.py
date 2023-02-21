92542ddc1cfc469288d48317f941eb8548b76005
Diego <diegorodriguezmancini@gmail.com>
2023-02-21 11:39:34 -0300

    feat: add producer and metrics config

--- apf/core/templates/step/settings.py
+++ apf/core/templates/step/settings.py
@@ -5,12 +5,13 @@
 ## Set the global logging level to debug
 # LOGGING_DEBUG = True
 
-## Consumer configuration
-### Each consumer has different parameters and can be found in the documentation
 CONSUMER_CONFIG = {}
+PRODUCER_CONFIG = {}
+METRICS_CONFIG = {}
 
 ## Step Configuration
 STEP_CONFIG = {
-    # "N_PROCESS": 4,            # Number of process for multiprocess script
-    # "COMMIT": False,           #Disables commit, useful to debug a KafkaConsumer
+    "CONSUMER_CONFIG": CONSUMER_CONFIG,
+    "PRODUCER_CONFIG": PRODUCER_CONFIG,
+    "METRICS_CONFIG": METRICS_CONFIG,
 }
