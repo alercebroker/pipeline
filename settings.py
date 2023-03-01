d31fad868d2c3e925f318540edbea6aaf51fed8c
Pedro Gallardo <pegallardo@inf.udec.cl>
2023-03-01 15:14:19 -0300

    chore: step settings

--- settings.py
+++ settings.py
@@ -1,16 +1,70 @@
+import os
+
 ##################################################
 #       prv_detection_step   Settings File
 ##################################################
 
 ## Set the global logging level to debug
-#LOGGING_DEBUG = True
+# LOGGING_DEBUG = True
 
 ## Consumer configuration
 ### Each consumer has different parameters and can be found in the documentation
-CONSUMER_CONFIG = {}
+CONSUMER_CONFIG = {
+    "PARAMS": {
+        "bootstrap.servers": os.environ["CONSUMER_SERVER"],
+        "group.id": os.environ["CONSUMER_GROUP_ID"],
+        "auto.offset.reset": "beginning",
+    },
+    "TOPICS": os.environ["CONSUMER_TOPICS"].split(","),
+}
+
+METRICS_CONFIG = {
+    "CLASS": os.getenv("METRICS_CLASS", "apf.metrics.KafkaMetricsProducer"),
+    "EXTRA_METRICS": [
+        {"key": "candid", "format": lambda x: str(x)},
+    ],
+    "PARAMS": {
+        "PARAMS": {
+            "bootstrap.servers": os.getenv("METRICS_HOST"),
+            "auto.offset.reset": "smallest",
+        },
+        "TOPIC": os.getenv("METRICS_TOPIC", "metrics"),
+        "SCHEMA": {
+            "$schema": "http://json-schema.org/draft-07/schema",
+            "$id": "http://example.com/example.json",
+            "type": "object",
+            "title": "The root schema",
+            "description": "The root schema comprises the entire JSON document.",
+            "default": {},
+            "examples": [
+                {"timestamp_sent": "2020-09-01", "timestamp_received": "2020-09-01"}
+            ],
+            "required": ["timestamp_sent", "timestamp_received"],
+            "properties": {
+                "timestamp_sent": {
+                    "$id": "#/properties/timestamp_sent",
+                    "type": "string",
+                    "title": "The timestamp_sent schema",
+                    "description": "Timestamp sent refers to the time at which a message is sent.",
+                    "default": "",
+                    "examples": ["2020-09-01"],
+                },
+                "timestamp_received": {
+                    "$id": "#/properties/timestamp_received",
+                    "type": "string",
+                    "title": "The timestamp_received schema",
+                    "description": "Timestamp received refers to the time at which a message is received.",
+                    "default": "",
+                    "examples": ["2020-09-01"],
+                },
+            },
+            "additionalProperties": True,
+        },
+    },
+}
 
 ## Step Configuration
 STEP_CONFIG = {
-    # "N_PROCESS": 4,            # Number of process for multiprocess script
-    # "COMMIT": False,           #Disables commit, useful to debug a KafkaConsumer
+    "CONSUMER_CONFIG": CONSUMER_CONFIG,
+    "METRICS_CONFIG": METRICS_CONFIG,
 }
