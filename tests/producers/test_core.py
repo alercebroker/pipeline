a04eca1e2659ca465d9b2875e200f7979cdbf5d6
Diego <diegorodriguezmancini@gmail.com>
2023-03-02 14:56:22 -0300

    tests: add test with key

--- tests/producers/test_core.py
+++ tests/producers/test_core.py
@@ -4,13 +4,13 @@ import unittest
 
 
 class Producer(GenericProducer):
-    def produce(self, message, key):
+    def produce(self, message, **kwargs):
         pass
 
 
 class GenericProducerTest(unittest.TestCase):
     component: GenericProducer
 
-    def test_produce(self, use: GenericProducer = Producer()):
+    def test_produce(self, use: GenericProducer = Producer(), **kwargs):
         self.component = use
-        self.component.produce({"key": "test", "int": 1}, key="key")
+        self.component.produce({"key": "test", "int": 1}, **kwargs)
