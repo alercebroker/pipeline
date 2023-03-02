c678cfc048674847432998504b674988137b31f0
Diego <diegorodriguezmancini@gmail.com>
2023-03-02 14:43:38 -0300

    feat: producer_key is property of generic producer

--- apf/producers/generic.py
+++ apf/producers/generic.py
@@ -9,6 +9,15 @@ class GenericProducer(ABC):
         self.logger = logging.getLogger(self.__class__.__name__)
         self.logger.info(f"Creating {self.__class__.__name__}")
         self.config = config
+        self._producer_key = None
+
+    @property
+    def producer_key(self):
+        return self._producer_key
+
+    @producer_key.setter
+    def producer_key(self, key: str):
+        self._producer_key = key
 
     @abstractmethod
     def produce(self, message=None, **kwargs):
