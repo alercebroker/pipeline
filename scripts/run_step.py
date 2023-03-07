abf9156f52e5dc741613536a8bd16860280d0c3e
Diego <diegorodriguezmancini@gmail.com>
2023-03-07 13:20:39 -0300

    tests: fix integration tests
    Fixed running all integration tests in single command

--- scripts/run_step.py
+++ scripts/run_step.py
@@ -7,22 +7,26 @@ SCRIPT_PATH = os.path.dirname(os.path.abspath(__file__))
 PACKAGE_PATH = os.path.abspath(os.path.join(SCRIPT_PATH, ".."))
 
 sys.path.append(PACKAGE_PATH)
-from settings import *
 
-level = logging.INFO
-if "LOGGING_DEBUG" in locals():
-    if LOGGING_DEBUG:
-        level = logging.DEBUG
-
-logging.basicConfig(
-    level=level,
-    format="%(asctime)s %(levelname)s %(name)s.%(funcName)s: %(message)s",
-    datefmt="%Y-%m-%d %H:%M:%S",
-)
 
 from prv_candidates_step import PrvCandidatesStep
 
-step = PrvCandidatesStep(config=STEP_CONFIG, level=level)
+
+def step_creator():
+    from settings import settings_creator
+
+    settings = settings_creator()
+    level = logging.INFO
+    logging.basicConfig(
+        level=level,
+        format="%(asctime)s %(levelname)s %(name)s.%(funcName)s: %(message)s",
+        datefmt="%Y-%m-%d %H:%M:%S",
+    )
+    if "LOGGING_DEBUG" in locals():
+        if settings["LOGGING_DEBUG"]:
+            level = logging.DEBUG
+    return PrvCandidatesStep(config=settings, level=level)
+
 
 if __name__ == "__main__":
-    step.start()
+    step_creator().start()
