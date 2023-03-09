db0861bc4be22dc459d1a57fd83378264c3aa600
Pablo Castellanos <pablocastellanos@gmail.com>
2023-03-09 11:54:33 -0300

    test(executor): Added executor tests with elements

--- tests/unittest/test_executor.py
+++ tests/unittest/test_executor.py
@@ -1,10 +1,10 @@
-from unittest.mock import patch
 import unittest
+from unittest import mock
 
 from mongo_scribe.db.executor import ScribeCommandExecutor
 
 
-class ExecutorTest(unittest.TestCase):
+class TestExecutor(unittest.TestCase):
     def setUp(self):
         db_config = {
             "MONGO": {
@@ -17,6 +17,14 @@ class ExecutorTest(unittest.TestCase):
         }
         executor = ScribeCommandExecutor(db_config)
         self.executor = executor
+        self.executor.connection = mock.MagicMock()
 
-    def test_bulk_execute(self):
+    def test_bulk_execute_skips_empty(self):
         self.executor.bulk_execute("object", [])
+        self.executor.connection.database.__getitem__.return_value.bulk_write.assert_not_called()
+
+    def test_bulk_execute_runs_bulk_write(self):
+        command = mock.MagicMock()
+        command.get_operations.return_value = [mock.MagicMock(), mock.MagicMock()]
+        self.executor.bulk_execute("object", [command])
+        self.executor.connection.database.__getitem__.return_value.bulk_write.assert_called_once()
