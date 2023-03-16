136c8c0f271e5b4043d2ac1d931f78ffe1278b69
Pablo Castellanos <pablocastellanos@gmail.com>
2023-03-16 15:20:09 -0300

    refactor(script): Follow python standards

--- scripts/run_step.py
+++ scripts/run_step.py
@@ -1,15 +1,16 @@
-import os
-import sys
-
 import logging
 
-SCRIPT_PATH = os.path.dirname(os.path.abspath(__file__))
-PACKAGE_PATH = os.path.abspath(os.path.join(SCRIPT_PATH, ".."))
+try:
+    from correction_step import CorrectionStep
+except ImportError:
+    import os
+    import sys
 
-sys.path.append(PACKAGE_PATH)
+    SCRIPT_PATH = os.path.dirname(os.path.abspath(__file__))
+    PACKAGE_PATH = os.path.abspath(os.path.join(SCRIPT_PATH, ".."))
+    sys.path.append(PACKAGE_PATH)
+    from correction_step import CorrectionStep
 
-
-from correction_step import CorrectionStep
 
 def step_creator():
     from settings import settings_creator
@@ -21,9 +22,8 @@ def step_creator():
         format="%(asctime)s %(levelname)s %(name)s.%(funcName)s: %(message)s",
         datefmt="%Y-%m-%d %H:%M:%S",
     )
-    if "LOGGING_DEBUG" in locals():
-        if settings["LOGGING_DEBUG"]:
-            level = logging.DEBUG
+    if settings["LOGGING_DEBUG"]:
+        level = logging.DEBUG
     return CorrectionStep(config=settings, level=level)
 
 
