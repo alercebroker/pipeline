1e1b9a8aafe25b6b4c290d8916389ea4b7f865ff
Pablo Castellanos <pablocastellanos@gmail.com>
2023-03-23 15:12:31 -0300

    feat: Step basics

---
+++ lightcurve_step/step.py
@@ -0,0 +1,24 @@
+from apf.core.step import GenericStep
+import logging
+
+
+class LightcurveStep(GenericStep):
+    """LightcurveStep Description
+
+    Parameters
+    ----------
+    consumer : GenericConsumer
+        Description of parameter `consumer`.
+    **step_args : type
+        Other args passed to step (DB connections, API requests, etc.)
+
+    """
+    def __init__(self, config: dict, level: int = logging.INFO, **step_args):
+        super().__init__(config=config, level=level, **step_args)
+
+    def execute(self, message: dict):
+        ################################
+        #   Here comes the Step Logic  #
+        ################################
+
+        pass
