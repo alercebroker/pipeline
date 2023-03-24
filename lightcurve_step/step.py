c332db32713254efbbcafe7296deef87ce03970b
Diego <diegorodriguezmancini@gmail.com>
2023-03-24 10:38:02 -0300

    fix: pre_execute must return

--- lightcurve_step/step.py
+++ lightcurve_step/step.py
@@ -18,12 +18,15 @@ class LightcurveStep(GenericStep):
     @staticmethod
     def unique_non_detections(old_non_detections, new_non_detections):
         """Return only non-duplicate non-detections (based on oid, fid and mjd). Keeps the ones in old"""
+
         def create_id(detection):
             return {k: v for k, v in detection.items() if k in ["oid", "fid", "mjd"]}
 
         ids = [create_id(det) for det in old_non_detections]
 
-        new_non_detections = [det for det in new_non_detections if create_id(det) not in ids]
+        new_non_detections = [
+            det for det in new_non_detections if create_id(det) not in ids
+        ]
         return old_non_detections + new_non_detections
 
     def pre_execute(self, messages: list[dict]):
@@ -32,22 +35,33 @@ class LightcurveStep(GenericStep):
         for message in messages:
             if message["aid"] in aids:
                 idx = aids[message["aid"]]
-                output[idx]["detections"] = self.unique_detections(output[idx]["detections"], message["detections"])
-                output[idx]["non_detections"] = self.unique_non_detections(output[idx]["non_detections"], message["non_detections"])
+                output[idx]["detections"] = self.unique_detections(
+                    output[idx]["detections"], message["detections"]
+                )
+                output[idx]["non_detections"] = self.unique_non_detections(
+                    output[idx]["non_detections"], message["non_detections"]
+                )
             else:
                 output.append(message)
                 aids[message["aid"]] = len(output) - 1
+        return output
 
     def execute(self, messages: list[dict]):
         for message in messages:
             # TODO: Connection to DB are placeholders
             detections_in_db = []  # self.db_client.query_detections(message["aid"])
             self.clean_detections_from_db(detections_in_db)
-            non_detections_in_db = []  # self.db_client.query_non_detections(message["aid"])
+            non_detections_in_db = (
+                []
+            )  # self.db_client.query_non_detections(message["aid"])
             self.clean_non_detections_from_db(non_detections_in_db)
 
-            message["detections"] = self.unique_detections(detections_in_db, message["detections"])
-            message["non_detections"] = self.unique_non_detections(non_detections_in_db, message["non_detections"])
+            message["detections"] = self.unique_detections(
+                detections_in_db, message["detections"]
+            )
+            message["non_detections"] = self.unique_non_detections(
+                non_detections_in_db, message["non_detections"]
+            )
 
         return messages
 
@@ -63,4 +77,3 @@ class LightcurveStep(GenericStep):
         """Modifies inplace"""
         for non_detection in non_detections:
             non_detection.pop("_id")
-
