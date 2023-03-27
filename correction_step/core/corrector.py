fc08e273285c4bd65b084ec18fd9ca882604ebda
Pablo Castellanos <pablocastellanos@gmail.com>
2023-03-27 15:31:21 -0300

    test(corrector): Added tests for corrector

--- correction_step/core/corrector.py
+++ correction_step/core/corrector.py
@@ -12,24 +12,24 @@ class Corrector:
         self._non_detections = pd.DataFrame.from_records(non_detections)
         self._non_detections = self._non_detections.drop_duplicates(["oid", "fid", "mjd"])
 
-        extras = [{**alert["extra_fields"], "candid": alert["candid"]} for alert in detections]
-        extras_df = pd.DataFrame.from_records(extras, index="candid")
-        self._extra_columns = extras_df.columns  # Only used to format output
-
+        self.__extra_fields = {alert["candid"]: alert["extra_fields"] for alert in detections}
+        extras_df = pd.DataFrame.from_dict(self.__extra_fields, orient="index")
         detections_df = pd.DataFrame.from_records(detections, exclude={"extra_fields"}, index="candid")
         # Remove duplicate detections by candid, always keeping those with stamps
         self._detections = detections_df.join(extras_df).sort_values("has_stamp", ascending=False)
         self._detections = self._detections[~self._detections.index.duplicated(keep="first")]
 
+        # Used only for output formatting
+        self.__extra_columns = extras_df.columns
+
     def _survey_mask(self, survey: str):
         return self._detections["tid"].str.lower().str.startswith(survey.lower())
 
-    def _apply_all_surveys(self, function: str, *, default=None, columns=None):
+    def _apply_all_surveys(self, function: str, *, default=None, columns=None, dtype=object):
         if columns:
-            basic = pd.DataFrame(default, index=self._detections.index, columns=columns)
+            basic = pd.DataFrame(default, index=self._detections.index, columns=columns, dtype=dtype)
         else:
-            basic = pd.Series(default, index=self._detections.index)
-
+            basic = pd.Series(default, index=self._detections.index, dtype=dtype)
         for name in strategy.__dict__:
             if name.startswith("_"):
                 continue
@@ -37,27 +37,28 @@ class Corrector:
             if mask.any():
                 module = getattr(strategy, name)
                 basic[mask] = getattr(module, function)(self._detections[mask])
-
-        return basic
+        # The way of computing can sometimes change the types
+        return basic.astype(dtype)
 
     @property
     def corrected(self) -> pd.Series:
         """Whether the detection has a nearby known source"""
-        return self._apply_all_surveys("is_corrected", default=False)
+        return self._apply_all_surveys("is_corrected", default=False, dtype=bool)
 
     @property
     def dubious(self) -> pd.Series:
         """Whether the correction or lack thereof is dubious"""
-        return self._apply_all_surveys("is_dubious", default=False)
+        return self._apply_all_surveys("is_dubious", default=False, dtype=bool)
 
     @property
     def stellar(self) -> pd.Series:
         """Whether the source is likely stellar"""
-        return self._apply_all_surveys("is_stellar", default=False)
+        return self._apply_all_surveys("is_stellar", default=False, dtype=bool)
 
     def _correct(self) -> pd.DataFrame:
         """Convert difference magnitude into apparent magnitude"""
-        return self._apply_all_surveys("correct", columns=["mag_corr", "e_mag_corr", "e_mag_corr_ext"])
+        cols = ["mag_corr", "e_mag_corr", "e_mag_corr_ext"]
+        return self._apply_all_surveys("correct", columns=cols, dtype=float)
 
     def corrected_dataframe(self) -> pd.DataFrame:
         """Alert dataframe including corrected magnitudes.
@@ -67,5 +68,4 @@ class Corrector:
         corrected = self._correct().replace(np.inf, self._ZERO_MAG)
         corrected[~self.corrected] = np.nan
         corrected = corrected.assign(corrected=self.corrected, dubious=self.dubious, stellar=self.stellar)
-
-        return self._detections.join(corrected, lsuffix="_old").replace(np.nan, None)
+        return self._detections.join(corrected).replace(np.nan, None).drop(columns=self.__extra_columns)
---
+++ tests/unittest/test_corrector.py
@@ -0,0 +1,127 @@
+from copy import deepcopy
+from unittest import mock
+
+import pandas as pd
+import numpy as np
+from pandas.testing import assert_frame_equal
+
+from correction_step.core import Corrector
+from tests.utils import ztf_alert, atlas_alert
+
+detections = [ztf_alert(candid="c1"), atlas_alert(candid="c2")]
+MAG_CORR_COLS = ["mag_corr", "e_mag_corr", "e_mag_corr_ext"]
+
+
+def test_mask_survey_returns_only_alerts_from_requested_survey():
+    corrector = Corrector(detections, [])
+    expected_ztf = pd.Series([True, False], index=["c1", "c2"])
+    assert (corrector._survey_mask("ZtF") == expected_ztf).all()
+    assert (corrector._survey_mask("ATla") == ~expected_ztf).all()
+
+
+@mock.patch("correction_step.core.corrector.strategy")
+def test_apply_all_calls_requested_function_to_masked_detections_for_each_submodule(mock_strategy):
+    mock_strategy.ztf = mock.MagicMock()
+    mock_strategy.ztf.function = mock.MagicMock()
+    mock_strategy.ztf.function.return_value = None
+    mock_strategy.dummy = mock.MagicMock()
+    mock_strategy.dummy.function = mock.MagicMock()
+
+    corrector = Corrector(detections, [])
+    corrector._apply_all_surveys("function")
+    called, = mock_strategy.ztf.function.call_args.args
+    assert_frame_equal(called, corrector._detections.loc[["c1"]])
+    mock_strategy.dummy.function.assert_not_called()
+
+
+@mock.patch("correction_step.core.corrector.strategy")
+def test_apply_all_returns_a_series_with_default_value_and_dtype_if_no_columns_are_given(mock_strategy):
+    corrector = Corrector(detections, [])
+    output = corrector._apply_all_surveys("function", default=-1, dtype=float)
+    assert isinstance(output, pd.Series)
+    assert output.dtype == float
+    assert (output.index == ["c1", "c2"]).all()
+    assert (output == -1).all()
+
+
+@mock.patch("correction_step.core.corrector.strategy")
+def test_apply_all_returns_a_df_with_default_value_and_dtype_if_columns_are_given(mock_strategy):
+    corrector = Corrector(detections, [])
+    output = corrector._apply_all_surveys("function", default=-1, columns=["a", "b"], dtype=float)
+    assert isinstance(output, pd.DataFrame)
+    assert (output.dtypes == float).all()
+    assert (output.columns == ["a", "b"]).all()
+    assert (output.index == ["c1", "c2"]).all()
+    assert (output == -1).all().all()
+
+
+def test_corrected_calls_apply_all_with_function_is_corrected():
+    corrector = Corrector(detections, [])
+    corrector._apply_all_surveys = mock.MagicMock()
+    _ = corrector.corrected
+    corrector._apply_all_surveys.assert_called_with("is_corrected", default=False, dtype=bool)
+
+
+def test_corrected_is_false_for_surveys_without_strategy():
+    corrector = Corrector(detections, [])
+    assert (corrector.corrected == pd.Series([True, False], index=["c1", "c2"])).all()
+
+
+def test_dubious_calls_apply_all_with_function_is_dubious():
+    corrector = Corrector(detections, [])
+    corrector._apply_all_surveys = mock.MagicMock()
+    _ = corrector.dubious
+    corrector._apply_all_surveys.assert_called_with("is_dubious", default=False, dtype=bool)
+
+
+def test_dubious_is_false_for_surveys_without_strategy():
+    corrector = Corrector(detections, [])
+    assert (corrector.dubious == pd.Series([False, False], index=["c1", "c2"])).all()
+
+
+def test_stellar_calls_apply_all_with_function_is_stellar():
+    corrector = Corrector(detections, [])
+    corrector._apply_all_surveys = mock.MagicMock()
+    _ = corrector.stellar
+    corrector._apply_all_surveys.assert_called_with("is_stellar", default=False, dtype=bool)
+
+
+def test_stellar_is_false_for_surveys_without_strategy():
+    corrector = Corrector(detections, [])
+    assert (corrector.stellar == pd.Series([True, False], index=["c1", "c2"])).all()
+
+
+def test_correct_calls_apply_all_with_function_correct():
+    corrector = Corrector(detections, [])
+    corrector._apply_all_surveys = mock.MagicMock()
+    corrector._correct()
+    corrector._apply_all_surveys.assert_called_with("correct", columns=MAG_CORR_COLS, dtype=float)
+
+
+def test_correct_is_nan_for_surveys_without_strategy():
+    corrector = Corrector(detections, [])
+    assert ~corrector._correct().loc["c1"].isna().any()
+    assert corrector._correct().loc["c2"].isna().all()
+
+
+def test_corrected_dataframe_has_generic_columns_and_new_ones_from_corrected():
+    corrector = Corrector(detections, [])
+    generic = ["aid", "oid", "tid", "fid", "mjd", "has_stamp", "isdiffpos", "mag", "e_mag"]
+    new_columns = MAG_CORR_COLS + ["corrected", "dubious", "stellar"]
+    assert (corrector.corrected_dataframe().columns.isin(generic + new_columns)).all()
+
+
+def test_corrected_dataframe_sets_non_corrected_detections_to_nan():
+    altered_detections = deepcopy(detections)
+    altered_detections[0]["extra_fields"]["distnr"] = 2
+    corrector = Corrector(altered_detections, [])
+    assert ~corrector._correct().loc["c1"].isna().any()
+    assert corrector.corrected_dataframe().loc["c1"][MAG_CORR_COLS].isna().all()
+
+
+def test_corrected_dataframe_sets_infinite_values_to_zero_magnitude():
+    altered_detections = deepcopy(detections)
+    altered_detections[0]["isdiffpos"] = -1
+    corrector = Corrector(altered_detections, [])
+    assert np.isinf(corrector._correct().loc["c1"]).any()
+    assert (corrector.corrected_dataframe().loc["c1"][MAG_CORR_COLS] == Corrector._ZERO_MAG).all()
