489d15e0a8ba6be0fc381cf5fe7ad3ec0ed29f79
Pablo Castellanos <pablocastellanos@gmail.com>
2023-03-28 17:09:21 -0300

    refactor(magstats): Separated grouping method

--- magstats_step/core/magstats.py
+++ magstats_step/core/magstats.py
@@ -1,10 +1,10 @@
 import warnings
 from functools import reduce
 from methodtools import lru_cache
-from typing import Literal
+from typing import Literal, Union
 
 import pandas as pd
-from pandas.core.groupby import DataFrameGroupBy
+from pandas.core.groupby import DataFrameGroupBy, SeriesGroupBy
 
 Which = Literal["first", "last"]
 
@@ -18,33 +18,37 @@ class MagnitudeStatistics:
         self._exclude = exclude
 
     @lru_cache(1)
-    def _corrected_detections(self):
+    def _corrected(self):
         return self._detections[self._detections["corrected"]]
 
     @lru_cache(4)
-    def _idx_by_fid(self, which: Which, corrected: bool = False) -> pd.Series:
+    def _grouped_index(self, which: Which, corrected: bool = False) -> pd.Series:
         if which == "first":
             function = "idxmin"
         elif which == "last":
             function = "idxmax"
         else:
             raise ValueError(f"Unrecognized value for 'which': {which}")
-        return self._detections_by_fid(corrected)["mjd"].agg(function)
+        return self._grouped_detections(corrected)["mjd"].agg(function)
 
     @lru_cache(20)
-    def _val_by_fid(self, source: str, which: Which, corrected: bool = False) -> pd.Series:
-        idx = self._idx_by_fid(which, corrected)
-        df = self._corrected_detections() if corrected else self._detections
+    def _grouped_value(self, source: str, which: Which, corrected: bool = False) -> pd.Series:
+        idx = self._grouped_index(which, corrected)
+        df = self._corrected() if corrected else self._detections
         return df[source][idx].set_axis(idx.index)
 
     @lru_cache(2)
-    def _detections_by_fid(self, corrected: bool = False) -> DataFrameGroupBy:
-        return self._corrected_detections().groupby("fid") if corrected else self._detections.groupby("fid")
+    def _grouped_detections(self, corrected: bool = False) -> DataFrameGroupBy:
+        return self._group(self._corrected()) if corrected else self._group(self._detections)
+
+    @staticmethod
+    def _group(df: Union[pd.DataFrame, pd.Series]) -> Union[DataFrameGroupBy, SeriesGroupBy]:
+        return df.groupby("fid")
 
     def _calculate_stats(self, corrected: bool = False) -> pd.DataFrame:
         suffix = "_corr" if corrected else ""
 
-        grouped = self._detections_by_fid(corrected)
+        grouped = self._grouped_detections(corrected)
         functions = {"mean": "mean", "median": "median", "max": "max", "min": "min", "std": "sigma"}
 
         aggregated = grouped[f"mag{suffix}"].agg(list(functions.keys()))
@@ -54,8 +58,8 @@ class MagnitudeStatistics:
 
     def _calculate_stats_over_time(self, corrected: bool = False):
         suffix = "_corr" if corrected else ""
-        first_mag = self._val_by_fid(f"mag{suffix}", which="first", corrected=corrected)
-        last_mag = self._val_by_fid(f"mag{suffix}", which="last", corrected=corrected)
+        first_mag = self._grouped_value(f"mag{suffix}", which="first", corrected=corrected)
+        last_mag = self._grouped_value(f"mag{suffix}", which="last", corrected=corrected)
         return pd.DataFrame({f"magfirst{suffix}": first_mag, f"maglast{suffix}": last_mag})
 
     def calculate_stats(self) -> pd.DataFrame:
@@ -65,27 +69,27 @@ class MagnitudeStatistics:
         return stats.join(self._calculate_stats_over_time(corrected=True))
 
     def calculate_mjd(self) -> pd.DataFrame:
-        first_mjd = self._val_by_fid("mjd", which="first")
-        last_mjd = self._val_by_fid("mjd", which="last")
+        first_mjd = self._grouped_value("mjd", which="first")
+        last_mjd = self._grouped_value("mjd", which="last")
         return pd.DataFrame({"firstmjd": first_mjd, "lastmjd": last_mjd})
 
     def calculate_corrected(self) -> pd.DataFrame:
-        return pd.DataFrame({"corrected": self._val_by_fid("corrected", which="first")})
+        return pd.DataFrame({"corrected": self._grouped_value("corrected", which="first")})
 
     def calculate_stellar(self) -> pd.DataFrame:
-        return pd.DataFrame({"stellar": self._val_by_fid("stellar", which="first")})
+        return pd.DataFrame({"stellar": self._grouped_value("stellar", which="first")})
 
     def calculate_ndet(self) -> pd.DataFrame:
         # The column selected for ndet is irrelevant as long as it has no NaN values
-        return pd.DataFrame({"ndet": self._detections_by_fid()["oid"].count()})
+        return pd.DataFrame({"ndet": self._grouped_detections()["oid"].count()})
 
     def calculate_ndubious(self) -> pd.DataFrame:
-        return pd.DataFrame({"ndubious": self._detections_by_fid()["dubious"].sum()})
+        return pd.DataFrame({"ndubious": self._grouped_detections()["dubious"].sum()})
 
     def calculate_saturation_rate(self) -> pd.DataFrame:
-        # Doesn't matter if there are NaNs (non-corrected), since the comparison is always false
-        saturated = (self._detections.set_index("fid")["mag_corr"] < self.MAGNITUDE_THRESHOLD).groupby("fid").sum()
-        total = self._detections_by_fid()["mag_corr"].count()  # Count also excludes NaNs
+        mask = self._detections["mag_corr"] < self.MAGNITUDE_THRESHOLD
+        saturated = self._group(self._detections[mask])["mag_corr"].count()
+        total = self._grouped_detections()["mag_corr"].count()  # Count also excludes NaNs
         with warnings.catch_warnings():
             # possible 0 divided by 0; this is expected and returned NaN is correct value
             warnings.filterwarnings("ignore", category=RuntimeWarning)
@@ -96,9 +100,9 @@ class MagnitudeStatistics:
 
         if self._non_detections.size == 0:  # Handle no non-detection case
             return pd.DataFrame(columns=["dt_first", "dm_first", "sigmadm_first", "dmdt_first"])
-        first_mag = self._val_by_fid("mag", which="first")
-        first_e_mag = self._val_by_fid("e_mag", which="first")
-        first_mjd = self._val_by_fid("mjd", which="first")
+        first_mag = self._grouped_value("mag", which="first")
+        first_e_mag = self._grouped_value("e_mag", which="first")
+        first_mjd = self._grouped_value("mjd", which="first")
 
         nd = self._non_detections.set_index("fid")  # Index by fid to compute based on it
 
@@ -110,7 +114,7 @@ class MagnitudeStatistics:
         # Include back fid for grouping and unique identification
         results = pd.DataFrame({"dt": dt, "dm": dm, "sigmadm": sigmadm, "dmdt": dmdt}).reset_index()
         # Only include non-detections before dt_min
-        idx = results[results["dt"] > dt_min].groupby("fid")["dmdt"].idxmin().dropna()
+        idx = self._group(results[results["dt"] > dt_min])["dmdt"].idxmin().dropna()
 
         # Drop NaN, since they result from no non-detection before first detection
         results = results.dropna().loc[idx].set_index("fid")
