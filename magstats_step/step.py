50de1ac16201eb079b29232050715352822b0601
Pablo Castellanos <pablocastellanos@gmail.com>
2023-03-30 16:49:32 -0300

    feat(step): functional step

--- magstats_step/step.py
+++ magstats_step/step.py
@@ -25,6 +25,11 @@ class MagstatsStep(GenericStep):
         return {"detections": detections, "non_detections": non_detections}
 
     def execute(self, messages: dict):
-        main = ObjectStatistics(messages["detections"])
-        magstats = MagnitudeStatistics(**messages)
-        return magstats
+        obj_calculator = ObjectStatistics(messages["detections"])
+        stats = obj_calculator.generate_statistics(self.excluded).to_dict("index")
+
+        magstats_calculator = MagnitudeStatistics(**messages)
+        magstats = magstats_calculator.generate_statistics(self.excluded).reset_index()
+        magstats = magstats.set_index(magstats["aid"] + magstats["fid"].astype(str)).to_dict("index")
+
+        return {"objects": stats, "magstats": magstats}
