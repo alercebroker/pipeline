70e939df318b138dc3c728c28ac6941c0cb0daff
Pablo Castellanos <pablocastellanos@gmail.com>
2023-03-30 16:36:47 -0300

    feat(settings): Added scribe settings

--- settings.py
+++ settings.py
@@ -1,5 +1,7 @@
 import os
 
+from fastavro import schema
+
 
 def settings_factory():
     # Set the global logging level to debug
@@ -9,41 +11,38 @@ def settings_factory():
     # Consumer configuration
     # Each consumer has different parameters and can be found in the documentation
     consumer_config = {
+        "CLASS": "apf.consumers.KafkaConsumer",
         "PARAMS": {
             "bootstrap.servers": os.environ["CONSUMER_SERVER"],
             "group.id": os.environ["CONSUMER_GROUP_ID"],
             "auto.offset.reset": "beginning",
-            "max.poll.interval.ms": 3600000,
+            "enable.partition.eof": bool(os.getenv("ENABLE_PARTITION_EOF")),
         },
+        "TOPICS": os.environ["CONSUMER_TOPICS"].split(","),
+        "consume.messages": int(os.getenv("CONSUME_MESSAGES", 50)),
         "consume.timeout": int(os.getenv("CONSUME_TIMEOUT", 10)),
-        "consume.messages": int(os.getenv("CONSUME_MESSAGES", 10)),
     }
 
-    if os.getenv("TOPIC_STRATEGY_FORMAT"):
-        consumer_config["TOPIC_STRATEGY"] = {
-            "CLASS": "apf.core.topic_management.DailyTopicStrategy",
-            "PARAMS": {
-                "topic_format": os.environ["TOPIC_STRATEGY_FORMAT"].strip().split(","),
-                "date_format": "%Y%m%d",
-                "change_hour": 23,
-            },
-        }
-    elif os.getenv("CONSUMER_TOPICS"):
-        consumer_config["TOPICS"] = os.environ["CONSUMER_TOPICS"].strip().split(",")
-    else:
-        raise Exception("Add TOPIC_STRATEGY or CONSUMER_TOPICS")
+    scribe_config = {
+        "CLASS": "apf.producers.KafkaProducer",
+        "PARAMS": {
+            "bootstrap.servers": os.environ["SCRIBE_SERVER"],
+        },
+        "TOPIC": os.environ["SCRIBE_TOPIC"],
+        "SCHEMA": schema.load_schema("scribe_schema.avsc"),
+    }
 
     metrics_config = {
         "CLASS": "apf.metrics.KafkaMetricsProducer",
         "EXTRA_METRICS": [
-            {"key": "oid", "alias": "oid"},
+            {"key": "aid"},
         ],
         "PARAMS": {
             "PARAMS": {
-                "bootstrap.servers": os.environ["METRICS_HOST"],
+                "bootstrap.servers": os.getenv("METRICS_SERVER"),
                 "auto.offset.reset": "smallest",
             },
-            "TOPIC": os.environ["METRICS_TOPIC"],
+            "TOPIC": os.getenv("METRICS_TOPIC", "metrics"),
             "SCHEMA": {
                 "$schema": "http://json-schema.org/draft-07/schema",
                 "$id": "http://example.com/example.json",
@@ -52,10 +51,7 @@ def settings_factory():
                 "description": "The root schema comprises the entire JSON document.",
                 "default": {},
                 "examples": [
-                    {
-                        "timestamp_sent": "2020-09-01",
-                        "timestamp_received": "2020-09-01",
-                    }
+                    {"timestamp_sent": "2020-09-01", "timestamp_received": "2020-09-01"}
                 ],
                 "required": ["timestamp_sent", "timestamp_received"],
                 "properties": {
@@ -84,9 +80,10 @@ def settings_factory():
     # Step Configuration
     step_config = {
         "CONSUMER_CONFIG": consumer_config,
+        "SCRIBE_CONFIG": scribe_config,
         "METRICS_CONFIG": metrics_config,
         "LOGGING_DEBUG": logging_debug,
-        "EXCLUDED_CALCULATORS": filter(bool, excluded_calculators)
+        "EXCLUDED_CALCULATORS": set(excluded_calculators),
     }
 
     return step_config
