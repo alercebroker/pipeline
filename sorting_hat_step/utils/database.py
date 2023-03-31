c120251699b95dd68287b11c5b15db060541f4fb
Pablo Castellanos <pablocastellanos@gmail.com>
2023-03-31 15:37:55 -0300

    refactor(database): extracted queries from wrapper

--- sorting_hat_step/utils/database.py
+++ sorting_hat_step/utils/database.py
@@ -1,7 +1,54 @@
+import math
 from typing import Callable, Dict, List, Union
-from db_plugins.db.mongo.connection import DatabaseConnection
 
+from db_plugins.db.mongo.connection import DatabaseConnection
 from db_plugins.db.mongo.models import Object
+
+
+def oid_query(db: DatabaseConnection, oid: list) -> Union[int, None]:
+    """
+    Query the database and check if any of the OIDs is already in database
+
+    :param db: Database connection
+    :param oid: oid of any survey
+
+    :return: existing aid if exists else is None
+    """
+    mongo_query = db.query(Object)
+    found = mongo_query.find_one({"oid": {"$in": oid}}, {"_id": "aid"})
+    if found:
+        return found["aid"]
+    return None
+
+
+def conesearch_query(db: DatabaseConnection, ra: float, dec: float, radius: float) -> List[dict]:
+    """
+    Query the database and check if there is an alerce_id
+    for the specified coordinates and search radius
+
+    :param db: Database connection
+    :param ra: first coordinate argument (RA)
+    :param dec: first coordinate argument (Dec)
+    :param radius: search radius (arcsec)
+
+    :return: existing aid if exists else is None
+    """
+    mongo_query = db.query(Object)
+    cursor = mongo_query.collection.find(
+        {
+            "loc": {
+                "$geoWithin": {
+                    "$centerSphere": [
+                        [ra - 180, dec],
+                        math.radians(radius / 3600),
+                    ]
+                }
+            },
+        },
+        {"_id": "aid"},  # rename _id to aid
+    )
+    spatial = [i for i in cursor]
+    return spatial
 
 
 def db_queries(db: DatabaseConnection) -> Dict[str, Callable]:
