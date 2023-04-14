706940af3026911279e75370d5fc4a6f8dc25415
Alex Alvarez Toledo <otajla@hotmail.com>
2023-04-14 09:59:56 -0400

    testing(parsers): testing for parse output, some small changes in unparse and parse output for using aid instead of oid}

--- tests/unittest/mocks/data.py
+++ tests/unittest/mocks/data.py
@@ -9,6 +9,7 @@ mock_lightcurve = [
                 "candid": "aaa",
                 "tid": "ZTF",
                 "oid": "oid",
+                "aid": "aid",
                 "mag": 0.1,
                 "e_mag": 0.01,
                 "extra_fields": {"field1": "f", "field2": "f2"},
@@ -17,3 +18,65 @@ mock_lightcurve = [
         ],
     }
 ]
+
+mock_lightcurves_list = [
+    {
+        "aid": "aid1",
+        "oid": "oid1",
+        "metadata": {},
+        "candid": 859632,
+        "detections": [
+            {
+                "candid": "aaa",
+                "tid": "ZTF",
+                "oid": "oid1",
+                "aid": "aid1",
+                "mag": 0.1,
+                "e_mag": 0.01,
+                "extra_fields": {"field1": "f", "field2": "f2"},
+            },
+            {"candid": "aaa", "tid": "ATLAS-01a"},
+        ],
+        "non_detections": []
+    },
+    {
+        "aid": "aid2",
+        "oid": "oid2",
+        "metadata": {},
+        "candid": 236958,
+        "detections": [
+            {
+                "candid": "eee",
+                "tid": "ZTF",
+                "oid": "oid2",
+                "aid": "aid2",
+                "mag": 0.1,
+                "e_mag": 0.01,
+                "extra_fields": {"field1": "f", "field2": "f2"},
+            },
+            {"candid": "aaa2", "tid": "ATLAS-01a"},
+        ],
+        "non_detections": []
+    }
+]
+
+mock_xmatch_list = [
+    {
+        "ra_in": 123,
+        "dec_in": 456,
+        "col1": "col",
+        "oid_in": "oid1",
+        "aid_in": "aid1",
+        "AllWISE": 999.888,
+        "W1mag": 777.555,
+        "W2mag": "J124131"
+    },
+    {
+        "ra_in": 321,
+        "dec_in": 654,
+        "col1": "col2",
+        "oid_in": "oid2",
+        "aid_in": "aid2",
+    }
+]
+
--- tests/unittest/test_parsers.py
+++ tests/unittest/test_parsers.py
@@ -1,20 +1,32 @@
 import pandas as pd
 from unittest import mock
-from mocks.data import mock_lightcurve
+from mocks.data import mock_lightcurve, mock_lightcurves_list, mock_xmatch_list
 
-from xmatch_step.core.parsing import unparse
+from xmatch_step.core.parsing import unparse, parse_output
 
 
 def test_unparse():
     lc = pd.DataFrame.from_records(mock_lightcurve, index="aid")
     unparsed = unparse(lc, "detections")
     unparsed = unparsed["detections"].to_dict()
-    detections = unparsed["oid"]
+    detections = unparsed["aid"]
     assert len(detections) == 1  # ATLAS detection was ignored
     keys = detections[0].keys()
     assert "extra_fields" not in keys
     assert all([elem in keys for elem in ["magpsf", "sigmapsf"]])
 
 
-def parse_output():
-    pass
+def test_parse_output():
+    lightcurve_dataframe = pd.DataFrame.from_records(
+        mock_lightcurves_list
+    )
+    xmatch_dataframe = pd.DataFrame.from_records(
+        mock_xmatch_list
+    )
+    result_dataframe = parse_output(lightcurve_dataframe, xmatch_dataframe)
+    
+    assert len(result_dataframe) == 2
+    assert "xmatches" in result_dataframe[0].keys()
+    assert "xmatches" in result_dataframe[1].keys()
+    assert "allwise" in result_dataframe[0]["xmatches"].keys()
+    assert "allwise" in result_dataframe[0]["xmatches"].keys()
