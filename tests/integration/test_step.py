e84469033a006280d3fd6231238d135ed83947e7
Pedro Gallardo <pegallardo@inf.udec.cl>
2023-04-18 15:00:05 -0400

    test: added integration tests assertions

--- tests/integration/test_step.py
+++ tests/integration/test_step.py
@@ -1,5 +1,19 @@
+import json
 from scripts.run_step import step_factory
 from apf.consumers import KafkaConsumer
+
+
+def assert_message_schema(command):
+    assert command["collection"] == "object"
+    assert command["type"] == "update"
+    assert "_id" in command["criteria"]
+    assert "data" in command
+
+
+def assert_command_data_schema(data):
+    expected_fields = ["lastmjd", "meandec", "meanra", "sigmadec", "corrected", "firstmjd", "oid", "tid", "magstats"]
+    for field in expected_fields:
+        assert field in data
 
 
 def test_step(kafka_service, env_variables, kafka_consumer):
@@ -7,5 +21,7 @@ def test_step(kafka_service, env_variables, kafka_consumer):
     step.start()
 
     for msg in kafka_consumer.consume():
-        print(msg)
+        loaded_message = json.loads(msg["payload"])
+        assert_message_schema(loaded_message)
+        assert_command_data_schema(loaded_message["data"])
         kafka_consumer.commit()
