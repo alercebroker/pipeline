69c754c95fb45c2009fc7cd9fafcc325dada0a7f
Pablo Castellanos <pablocastellanos@gmail.com>
2023-04-19 13:10:38 -0400

    feat(decat): Removed

--- survey_parser_plugins/parsers/__init__.py
+++ survey_parser_plugins/parsers/__init__.py
@@ -1,6 +1,5 @@
 from .ATLASParser import ATLASParser
 from .ZTFParser import ZTFParser
-from .DECATParser import DECATParser
 from .LSSTParser import LSSTParser
 
-__all__ = ["ATLASParser", "DECATParser", "ZTFParser", "LSSTParser"]
+__all__ = ["ATLASParser", "ZTFParser", "LSSTParser"]
