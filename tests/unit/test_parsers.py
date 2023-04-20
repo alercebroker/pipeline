b2566b37d72f6a24869713189dec4853edd78a05
Pablo Castellanos <pablocastellanos@gmail.com>
2023-04-20 15:23:16 -0400

    test(unit): adapted tests

--- tests/unit/test_parsers.py
+++ tests/unit/test_parsers.py
@@ -1,9 +1,5 @@
-from prv_candidates_step.core.strategy.utils.ztf_non_detections_parser import (
-    ZTFNonDetectionsParser,
-)
-from prv_candidates_step.core.strategy.utils.ztf_prv_detections_parser import (
-    ZTFPreviousDetectionsParser,
-)
+from prv_candidates_step.core.strategy.ztf import ZTFNonDetectionsParser
+from prv_candidates_step.core.strategy.ztf import ZTFPreviousDetectionsParser
 from fastavro.utils import generate_many
 from fastavro.schema import load_schema
 
@@ -12,19 +8,17 @@ def test_prv_detections_parser():
     schema = load_schema("tests/shared/prv_candidate_schema.avsc")
     data = list(generate_many(schema, 1))
     data = list(map(lambda x: {**x, "fid": 1}, data))
-    parser = ZTFPreviousDetectionsParser()
-    result = parser.parse(data, "oid", "aid", "parent_candid")
-    assert result[0]["aid"] == "aid"
-    assert result[0]["oid"] == "oid"
-    assert result[0]["extra_fields"]["parent_candid"] == "parent_candid"
+    result = ZTFPreviousDetectionsParser.parse_to_dict(data[0], "aid", "oid", "parent_candid")
+    assert result["aid"] == "aid"
+    assert result["oid"] == "oid"
+    assert result["extra_fields"]["parent_candid"] == "parent_candid"
 
 
 def test_non_detections_parser():
     schema = load_schema("tests/shared/prv_candidate_schema.avsc")
     data = list(generate_many(schema, 1))
     data = list(map(lambda x: {**x, "fid": 1}, data))
-    parser = ZTFNonDetectionsParser()
-    result = parser.parse(data, "aid", "oid")
-    assert result[0]["aid"] == "aid"
-    assert result[0]["tid"] == "tid"
-    assert result[0]["oid"] == "oid"
+    result = ZTFNonDetectionsParser.parse_non_detection(data[0], "aid", "oid")
+    assert result["aid"] == "aid"
+    assert result["tid"] == "ZTF"
+    assert result["oid"] == "oid"
--- tests/unit/test_strategy.py
+++ tests/unit/test_strategy.py
@@ -1,25 +1,22 @@
 from fastavro.utils import generate_one
-from prv_candidates_step.core import (
-    ZTFPrvCandidatesStrategy,
-    ATLASPrvCandidatesStrategy,
-)
+from prv_candidates_step.core.extractor import PreviousCandidatesExtractor
 from tests.mocks.mock_alerts import extra_fields_generator
 from tests.shared.sorting_hat_schema import SCHEMA
 
 
 def test_compute_ztf():
-    strat = ZTFPrvCandidatesStrategy()
     alert = generate_one(SCHEMA)
+    alert["sid"] = "ZTF"
     alert["extra_fields"] = extra_fields_generator()
-    prv_detections, non_detections = strat.process_prv_candidates(alert)
-    assert len(prv_detections) == 2
-    assert len(non_detections) == 2
+    result = PreviousCandidatesExtractor([alert]).extract_all()
+    assert len(result[0]["detections"]) == 3
+    assert len(result[0]["non_detections"]) == 2
 
 
 def test_compute_atlas():
-    strat = ATLASPrvCandidatesStrategy()
     alert = generate_one(SCHEMA)
+    alert["sid"] = "ATLAS"
     alert["extra_fields"] = extra_fields_generator()
-    prv_detections, non_detections = strat.process_prv_candidates(alert)
-    assert len(prv_detections) == 0
-    assert len(non_detections) == 0
+    result = PreviousCandidatesExtractor([alert]).extract_all()
+    assert len(result[0]["detections"]) == 1
+    assert len(result[0]["non_detections"]) == 0
