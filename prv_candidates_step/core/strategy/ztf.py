a19b64c4914bd12bb3dfc74e7b0465c54fe4904f
Pablo Castellanos <pablocastellanos@gmail.com>
2023-05-05 12:27:18 -0400

    feat: Added is_forced field to ZTF previous detections

--- prv_candidates_step/core/strategy/ztf.py
+++ prv_candidates_step/core/strategy/ztf.py
@@ -65,7 +65,7 @@ def extract_detections_and_non_detections(alert: dict) -> dict:
     for candidate in prv_candidates:
         if candidate["candid"]:
             candidate = ZTFPreviousDetectionsParser.parse(candidate, oid)
-            candidate.update({"aid": aid, "has_stamp": False})
+            candidate.update({"aid": aid, "has_stamp": False, "is_forced": False})
             candidate["extra_fields"].update({"parent_candid": parent})
             alert.pop("stamps", None)
             detections.append(candidate)
