0c7d7bd48d7cca3485134c988b7cd3a96360dc0f
Pablo Castellanos <pablocastellanos@gmail.com>
2023-05-08 14:51:40 -0400

    refactor(extractor): back to only detections

--- prv_candidates_step/core/extractor.py
+++ prv_candidates_step/core/extractor.py
@@ -10,13 +10,11 @@ class PreviousCandidatesExtractor:
         for alert in self._alerts:
             survey = alert["sid"].lower()
 
-            alert["has_stamp"] = bool(alert.pop("stamps", False))
+            alert.update({"has_stamp": bool(alert.pop("stamps", False)), "forced": False})
             try:
                 module = getattr(strategy, survey)
             except AttributeError:
-                messages.append(
-                    {"aid": alert["aid"], "detections": [alert], "non_detections": [], "forced_photometries": []}
-                )
+                messages.append({"aid": alert["aid"], "detections": [alert], "non_detections": []})
             else:
                 messages.append(module.extract_detections_and_non_detections(alert))
         return messages
