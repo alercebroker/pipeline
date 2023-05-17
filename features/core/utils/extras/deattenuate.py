99bc7a9a43056822b748b279e6e063e9c48ba49c
Pablo Castellanos <pablocastellanos@gmail.com>
2023-05-17 16:40:26 -0400

    fix(deattenuate): Fix search for wavelength

--- features/core/utils/extras/deattenuate.py
+++ features/core/utils/extras/deattenuate.py
@@ -3,10 +3,14 @@ import numpy as np
 from astropy.cosmology import WMAP5
 
 
+def _distmod(z):
+    return WMAP5.distmod(z).to("mag").value
+
+
 def lsst(flux: np.ndarray, error: np.ndarray, band: np.ndarray, mwebv: float, zhost: float):
     """Modifies flux and error inplace"""
     zhost = zhost if zhost >= 0.003 else 0
-    z_deatt = 10 ** (-(WMAP5.distmod(0.3).to("mag").value - WMAP5.distmod(zhost).to("mag")) / 2.5) if zhost else 1
+    z_deatt = 10 ** (-(_distmod(0.3) - _distmod(zhost)) / 2.5) if zhost else 1
 
     rv = 3.1
     av = rv * mwebv
@@ -22,7 +26,7 @@ def lsst(flux: np.ndarray, error: np.ndarray, band: np.ndarray, mwebv: float, zh
 
     for fid in np.unique(band):
         mask = band == fid
-        dust_deatt, = 10 ** (extinction.odonnell94(np.full(1, wavelengths[band]), av, rv) / 2.5)
+        dust_deatt, = 10 ** (extinction.odonnell94(np.full(1, wavelengths[fid]), av, rv) / 2.5)
 
         flux[mask] *= z_deatt * dust_deatt
         error[mask] *= z_deatt * dust_deatt
