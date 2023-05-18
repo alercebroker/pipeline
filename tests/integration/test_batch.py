4aad1e1b7e5abbd4cabe47e05dbd6eb4855d8087
Pedro Gallardo <pegallardo@inf.udec.cl>
2023-05-18 12:07:20 -0400

    test: test structure changed to functional

--- tests/integration/test_batch.py
+++ tests/integration/test_batch.py
@@ -1,7 +1,6 @@
 from pprint import pprint
 from random import choice
 import pytest
-import unittest
 from mongo_scribe.step import MongoScribe
 from apf.consumers.kafka import KafkaConsumer
 from apf.producers.kafka import KafkaProducer
@@ -42,93 +41,86 @@ PRODUCER_CONFIG = {
 }
 
 
-@pytest.mark.usefixtures("kafka_service")
-@pytest.mark.usefixtures("mongo_service")
-class StepTest(unittest.TestCase):
-    @classmethod
-    def setUpClass(cls):
-        cls.step_config = {
-            "DB_CONFIG": DB_CONFIG,
-            "STEP_METADATA": {
-                "STEP_ID": "scribe",
-                "STEP_NAME": "scribe",
-                "STEP_VERSION": "test",
-                "STEP_COMMENTS": "test ver.",
-            },
-        }
+step_config = {
+    "DB_CONFIG": DB_CONFIG,
+    "STEP_METADATA": {
+        "STEP_ID": "scribe",
+        "STEP_NAME": "scribe",
+        "STEP_VERSION": "test",
+        "STEP_COMMENTS": "test ver.",
+    },
+}
 
-        konsumer = KafkaConsumer(config=CONSUMER_CONFIG)
-        cls.step = MongoScribe(consumer=konsumer, config=cls.step_config)
-        cls.producer = KafkaProducer(config=PRODUCER_CONFIG)
-        cls.generator = CommandGenerator()
+konsumer = KafkaConsumer(config=CONSUMER_CONFIG)
+step = MongoScribe(consumer=konsumer, config=step_config)
+producer = KafkaProducer(config=PRODUCER_CONFIG)
+generator = CommandGenerator()
 
-        # better generate the full batch then check
-        # 0 - X (no options)
-        commands = [cls.generator.generate_insert()]
-        commands.extend(
-            [cls.generator.generate_random_command() for _ in range(125)]
+# better generate the full batch then check
+# 0 - X (no options)
+commands = [generator.generate_insert()]
+commands.extend([generator.generate_random_command() for _ in range(125)])
+# 500 - 500 + X (with upsert)
+generator.set_offset(125)
+commands.append(generator.generate_insert())
+commands.extend(
+    [
+        generator.generate_random_command({"upsert": True}, 125)
+        for _ in range(125)
+    ]
+)
+# 1000 - 1000 + X (upsert and set_on_insert)
+generator.set_offset(250)
+commands.append(generator.generate_insert())
+commands.extend(
+    [
+        generator.generate_random_command(
+            {"upsert": True, "set_on_insert": True}, 250
         )
-        # 500 - 500 + X (with upsert)
-        cls.generator.set_offset(125)
-        commands.append(cls.generator.generate_insert())
-        commands.extend(
-            [
-                cls.generator.generate_random_command({"upsert": True}, 125)
-                for _ in range(125)
-            ]
-        )
-        # 1000 - 1000 + X (upsert and set_on_insert)
-        cls.generator.set_offset(250)
-        commands.append(cls.generator.generate_insert())
-        commands.extend(
-            [
-                cls.generator.generate_random_command(
-                    {"upsert": True, "set_on_insert": True}, 250
-                )
-                for _ in range(125)
-            ]
-        )
-        cls.commands = commands
-        # ask for possible edge cases
+        for _ in range(125)
+    ]
+)
+# ask for possible edge cases
 
-    def test_bulk(self):
-        for i, command in enumerate(self.commands):
-            self.producer.produce(command, key=f"key_{i}")
 
-        self.step.start()
-        collection = self.step.db_client.connection.database["object"]
+def test_bulk(kafka_service, mongo_service):
+    for i, command in enumerate(commands):
+        producer.produce(command, key=f"key_{i}")
 
-        # get any element that have features (obtained from the tracker)
-        updated_feats = {
-            key: val
-            for key, val in self.generator.get_updated_features().items()
-            if val != []
-        }
-        sample_id = choice(list(updated_feats.keys()))
-        result = collection.find_one({"_id": f"ID{sample_id}"})
-        tracked = updated_feats[sample_id]
-        diff = [
-            prob
-            for prob in result["features"] + tracked
-            if prob not in result["features"] or prob not in tracked
-        ]
-        self.assertEqual(len(diff), 0)
+    step.start()
+    collection = step.db_client.connection.database["object"]
 
-        # # the same as above but with probabilities
-        updated_probs = {
-            key: val
-            for key, val in self.generator.get_updated_probabilities().items()
-            if val != []
-        }
-        sample_id_2 = choice(list(updated_probs.keys()))
-        result = collection.find_one({"_id": f"ID{sample_id_2}"})
-        tracked = updated_probs[sample_id_2]
-        diff = [
-            prob
-            for prob in result["probabilities"] + tracked
-            if prob not in result["probabilities"] or prob not in tracked
-        ]
-        self.assertEqual(len(diff), 0)
-        # self.assertIsNotNone(result)
+    # get any element that have features (obtained from the tracker)
+    updated_feats = {
+        key: val
+        for key, val in generator.get_updated_features().items()
+        if val != []
+    }
+    sample_id = choice(list(updated_feats.keys()))
+    result = collection.find_one({"_id": f"ID{sample_id}"})
+    tracked = updated_feats[sample_id]
+    diff = [
+        prob
+        for prob in result["features"] + tracked
+        if prob not in result["features"] or prob not in tracked
+    ]
+    assert len(diff) == 0
 
-        # check edge cases
+    # # the same as above but with probabilities
+    updated_probs = {
+        key: val
+        for key, val in generator.get_updated_probabilities().items()
+        if val != []
+    }
+    sample_id_2 = choice(list(updated_probs.keys()))
+    result = collection.find_one({"_id": f"ID{sample_id_2}"})
+    tracked = updated_probs[sample_id_2]
+    diff = [
+        prob
+        for prob in result["probabilities"] + tracked
+        if prob not in result["probabilities"] or prob not in tracked
+    ]
+    assert len(diff) == 0
+    # assertIsNotNone(result)
+
+    # check edge cases
