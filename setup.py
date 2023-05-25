da31dac75e9768a47652421881373199dc072b42
Diego <diegorodriguezmancini@gmail.com>
2023-05-25 13:37:13 -0400

    chore: update version

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ packages = [sp.replace("/", ".") for sp in sub_packages]
 
 setup(
     name="alerce_classifiers",
-    version="0.0.5",
+    version="0.0.6",
     packages=packages,
     extras_require=extras_require,
     include_package_data=True,
