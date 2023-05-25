b2856c44278f2eb3aa1a307ef51f9b46693cb338
Diego <diegorodriguezmancini@gmail.com>
2023-05-25 10:54:35 -0400

    fix: __getattribute__ for __class__

--- alerce_classifiers/base/dto.py
+++ alerce_classifiers/base/dto.py
@@ -12,6 +12,9 @@ class InputDTO:
 
     def __getattribute__(self, name: str):
         __dict__ = super(InputDTO, self).__getattribute__("__dict__")
+        __class__ = super(InputDTO, self).__getattribute__("__class__")
+        if name == "__class__":
+            return __class__
         if name in __dict__:
             return super(InputDTO, self).__getattribute__(name)._value
         raise AttributeError
