760559fe19d5ba392684660070cff9f619632f7f
Diego <diegorodriguezmancini@gmail.com>
2023-05-25 16:27:01 -0400

    build: fix install with extras

--- setup.py
+++ setup.py
@@ -10,21 +10,24 @@ sub_packages = [
 
 extras_require = {
     sp.split("/")[-1]: [
-        x
-        for x in open(os.path.join(sp, "requirements.txt")).read().split("\n")
-        if not x.startswith("-e")
+        x for x in open(os.path.join(sp, "requirements.txt")).read().split("\n")
     ]
     for sp in sub_packages
     if os.path.exists(os.path.join(sp, "requirements.txt"))
 }
 extras_require["complete"] = sorted({v for req in extras_require.values() for v in req})
+print(extras_require)
 
 packages = [sp.replace("/", ".") for sp in sub_packages]
+
+with open("requirements.txt") as f:
+    install_requires = f.readlines()
 
 setup(
     name="alerce_classifiers",
     version="0.0.6",
     packages=packages,
+    install_requires=install_requires,
     extras_require=extras_require,
     include_package_data=True,
 )
