65ed2ee437cfc42bceeaabcb7a4cf969b728b60f
Diego <diegorodriguezmancini@gmail.com>
2023-05-29 13:07:12 -0400

    refactor: elasticc model uses latest version of alerce_classifiers

--- lc_classification/predictors/elasticc_random_forest/elasticc_random_forest_parser.py
+++ lc_classification/predictors/elasticc_random_forest/elasticc_random_forest_parser.py
@@ -1,3 +1,4 @@
+from alerce_classifiers.base.dto import InputDTO, OutputDTO
 from pandas import DataFrame
 from lc_classification.core.parsers.input_dto import create_input_dto
 from lc_classification.predictors.predictor.predictor_parser import (
@@ -8,17 +9,17 @@ from lc_classification.predictors.predictor.predictor_parser import (
 
 
 class ElasticcRandomForestParser(PredictorParser):
-    def parse_input(self, to_parse, **kwargs) -> PredictorInput[DataFrame]:
+    def parse_input(self, to_parse, **kwargs) -> PredictorInput[InputDTO]:
         dto = create_input_dto(to_parse, **kwargs)
-        features_as_list = dto.features.to_dict(orient="records")
-        features_for_model = DataFrame(
-            {"features": features_as_list}, index=dto.features.index
-        )
-        return PredictorInput(features_for_model)
+        # features_as_list = dto.features.to_dict(orient="records")
+        # features_for_model = DataFrame(
+        #     {"features": features_as_list}, index=dto.features.index
+        # )
+        return PredictorInput(dto)
 
-    def parse_output(self, to_parse, **kwargs) -> PredictorOutput:
+    def parse_output(self, to_parse: OutputDTO, **kwargs) -> PredictorOutput:
         result = {
-            "probabilities": to_parse,
+            "probabilities": to_parse.probabilities,
             "hierarchical": {"top": DataFrame(), "children": DataFrame()},
         }
         return PredictorOutput(result)
