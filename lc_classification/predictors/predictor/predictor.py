724fe68c041bacdbc55bc12579c2bc6184060f60
Diego <diegorodriguezmancini@gmail.com>
2023-05-30 12:09:26 -0400

    fix: case when there are no features
    For ZTF no output is produced.
    
    For ELASTICC output is produced with NO CLASS

--- lc_classification/predictors/predictor/predictor.py
+++ lc_classification/predictors/predictor/predictor.py
@@ -1,13 +1,40 @@
 import abc
 
+from alerce_classifiers.base.dto import InputDTO, OutputDTO
+
+from lc_classification.predictors.predictor.predictor_parser import PredictorInput
+from pandas import DataFrame
+
 
 class Predictor(abc.ABC):
+    """Wrapper for models.
+
+    Abstract class that wraps the predict functionality of different
+    ML model implementations.
+
+    The implementations of this abstract class should implement the _predict method.
+
+    You should interact with this class calling the predict() method.
+
+    Optionally, you can override the can_predict method to suit your actual model needs.
+
+    By default, predict will return an empty list if no classification was made.
+    """
+
     def __init__(self, model=None, **kwargs):
         self.model = model
 
     @abc.abstractmethod
-    def predict(self, model_input):
+    def _predict(self, model_input: PredictorInput):
         raise NotImplementedError()
+
+    def predict(self, model_input: PredictorInput):
+        if self.can_predict(model_input):
+            return self._predict(model_input)
+        return OutputDTO(DataFrame())
+
+    def can_predict(self, model_input: PredictorInput[InputDTO]):
+        return "features" in model_input.value.features
 
     def get_feature_list(self):
         return []
--- tests/unit/test_step.py
+++ tests/unit/test_step.py
@@ -80,6 +80,37 @@ def test_step():
         assert_object_is_correct(obj)
 
 
+@pytest.mark.skipif(os.getenv("MODEL") != "ztf", reason="ztf only")
+def test_step_empty_features():
+    step_mock_config["PREDICTOR_CONFIG"][
+        "CLASS"
+    ] = "lc_classification.predictors.ztf_random_forest.ztf_random_forest_predictor.ZtfRandomForestPredictor"
+    step_mock_config["PREDICTOR_CONFIG"][
+        "PARSER_CLASS"
+    ] = "lc_classification.predictors.ztf_random_forest.ztf_random_forest_parser.ZtfRandomForestParser"
+    step_mock_config[
+        "SCRIBE_PARSER_CLASS"
+    ] = "lc_classification.core.parsers.scribe_parser.ScribeParser"
+    step_mock_config[
+        "STEP_PARSER_CLASS"
+    ] = "lc_classification.core.parsers.alerce_parser.AlerceParser"
+    step = LateClassifier(config=step_mock_config)
+    step.consumer = mock.MagicMock(KafkaConsumer)
+    empty_features = []
+    for msg in messages_ztf:
+        msg["features"] = None
+        empty_features.append(msg)
+    step.consumer.consume.return_value = empty_features
+    step.producer = mock.MagicMock(KafkaProducer)
+    step.scribe_producer = mock.create_autospec(KafkaProducer)
+
+    step.start()
+    scribe_calls = step.scribe_producer.mock_calls
+    assert scribe_calls == []
+    calls = step.producer.mock_calls
+    assert calls == []
+
+
 @pytest.mark.skipif(os.getenv("MODEL") != "elasticc", reason="elasticc only")
 def test_step_elasticc():
     model_path = "https://assets.alerce.online/pipeline/elasticc/random_forest/2.0.1/"
@@ -99,6 +130,47 @@ def test_step_elasticc():
     step = LateClassifier(config=step_mock_config)
     step.consumer = mock.MagicMock(KafkaConsumer)
     step.consumer.consume.return_value = messages_elasticc
+    step.producer = mock.MagicMock(KafkaProducer)
+    step.scribe_producer = mock.create_autospec(KafkaProducer)
+
+    step.start()
+    scribe_calls = step.scribe_producer.mock_calls
+
+    # Tests scribe produces correct commands
+    for call in scribe_calls:
+        message = loads(call.args[0]["payload"])
+        assert_command_is_correct(message)
+
+    # Test producer produces correct data
+    calls = step.producer.mock_calls
+    for call in calls:
+        obj = call.args[0]
+        assert_elasticc_object_is_correct(obj)
+
+
+@pytest.mark.skipif(os.getenv("MODEL") != "elasticc", reason="elasticc only")
+def test_step_elasticc_without_features():
+    model_path = "https://assets.alerce.online/pipeline/elasticc/random_forest/2.0.1/"
+    step_mock_config["PREDICTOR_CONFIG"][
+        "CLASS"
+    ] = "lc_classification.predictors.elasticc_random_forest.elasticc_random_forest_predictor.ElasticcRandomForestPredictor"
+    step_mock_config["PREDICTOR_CONFIG"]["PARAMS"] = {"model_path": model_path}
+    step_mock_config["PREDICTOR_CONFIG"][
+        "PARSER_CLASS"
+    ] = "lc_classification.predictors.elasticc_random_forest.elasticc_random_forest_parser.ElasticcRandomForestParser"
+    step_mock_config[
+        "SCRIBE_PARSER_CLASS"
+    ] = "lc_classification.core.parsers.scribe_parser.ScribeParser"
+    step_mock_config[
+        "STEP_PARSER_CLASS"
+    ] = "lc_classification.core.parsers.elasticc_parser.ElasticcParser"
+    step = LateClassifier(config=step_mock_config)
+    step.consumer = mock.MagicMock(KafkaConsumer)
+    empty_features = []
+    for msg in messages_elasticc:
+        msg["features"] = None
+        empty_features.append(msg)
+    step.consumer.consume.return_value = empty_features
     step.producer = mock.MagicMock(KafkaProducer)
     step.scribe_producer = mock.create_autospec(KafkaProducer)
 
