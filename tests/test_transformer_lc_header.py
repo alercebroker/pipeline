f54d0fe6e045795dc55acaf7307f624a7bdab2fc
Diego <diegorodriguezmancini@gmail.com>
2023-06-08 16:08:38 -0400

    tests: add tests for balto

--- tests/test_transformer_lc_header.py
+++ tests/test_transformer_lc_header.py
@@ -1,10 +1,44 @@
-from alerce_classifiers.transformer_lc_header.model import TranformerLCHeaderClassifier
+from alerce_classifiers.transformer_lc_header.dict_transform import FEAT_DICT
+from alerce_classifiers.transformer_lc_header.model import TransformerLCHeaderClassifier
 from alerce_classifiers.transformer_lc_header.mapper import LCHeaderMapper
+from unittest.mock import patch
+from alerce_classifiers.base.factories import input_dto_factory
+import pandas as pd
+
+from tests.mockdata.detections import DETECTIONS
 
 
-def test_constructor():
+def check_model_quantiles(model: TransformerLCHeaderClassifier):
+    assert len(model.quantiles) == len(FEAT_DICT.values())
+
+
+@patch("alerce_classifiers.transformer_lc_header.model.torch")
+@patch("alerce_classifiers.transformer_lc_header.model.load")
+def test_constructor(_, __):
     mapper = LCHeaderMapper()
-    model = TranformerLCHeaderClassifier(
-        model_path="", header_quantiles_path="", mapper=mapper
+    model = TransformerLCHeaderClassifier(
+        model_path="",
+        header_quantiles_path="",
+        mapper=mapper,
     )
-    assert model
+    check_model_quantiles(model)
+
+
+@patch("alerce_classifiers.transformer_lc_header.model.torch")
+def test_predict(torch):
+    mock_detections = pd.DataFrame(DETECTIONS)
+    input_dto = input_dto_factory(
+        detections=mock_detections,
+        non_detections=pd.DataFrame(),
+        features=pd.DataFrame(),
+        xmatch=pd.DataFrame(),
+        stamps=pd.DataFrame(),
+    )
+    model = TransformerLCHeaderClassifier(
+        "model_path",
+        "https://assets.alerce.online/pipeline/elasticc/balto_classifier_lc_header/3.0.0/HeaderNorm1QT/",
+        LCHeaderMapper(),
+    )
+    output_dto = model.predict(input_dto)
+    model.model.predict_mix.assert_called()
+    assert len(output_dto.probabilities) == len(mock_detections)
