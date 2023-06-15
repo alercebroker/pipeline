fe972774a924ff8ede192e2bf81794ce5f1bed9d
Héctor Larrañaga <hectorPLJ@gmail.com>
2023-06-15 11:46:08 -0400

    tests: add tests for barney

--- tests/unit/test_step_barney.py
+++ tests/unit/test_step_barney.py
@@ -1,57 +1,62 @@
-from unittest import mock
-from lc_classification.core.step import (
-    LateClassifier,
-)
-from apf.producers import KafkaProducer
-from apf.consumers import KafkaConsumer
-from json import loads
-from tests.mockdata.input_elasticc import INPUT_SCHEMA as INPUT_ELASTICC
-from fastavro import utils
-import pytest
 import os
+from json import loads
 
+import pytest
+from fastavro import utils
+
+from tests.mockdata.extra_felds import generate_extra_fields
+from tests.mockdata.input_elasticc import INPUT_SCHEMA as INPUT_ELASTICC
 from tests.test_commons import (
-    assert_elasticc_object_is_correct,
     assert_command_is_correct,
+    assert_elasticc_object_is_correct,
 )
 
-model_path = "TODO"
-step_mock_config = {
-    "SCRIBE_PRODUCER_CONFIG": {"CLASS": "unittest.mock.MagicMock", "TOPIC": "test"},
-    "PRODUCER_CONFIG": {"CLASS": "unittest.mock.MagicMock", "TOPIC": "test2"},
-    "CONSUMER_CONFIG": {"CLASS": "unittest.mock.MagicMock", "TOPIC": "test3"},
-    "MODEL_VERSION": "test",
-    "PREDICTOR_CONFIG": {
-        "PARAMS": {"model_path": model_path},
-        "CLASS": "lc_classification.predictors.barney.barney_predictor.BarneyPredictor",
-        "PARSER_CLASS": "lc_classification.predictors.barney.barney_parser.BarneyParser",
-    },
-    "SCRIBE_PARSER_CLASS": "lc_classification.core.parsers.scribe_parser.ScribeParser",
-    "STEP_PARSER_CLASS": "lc_classification.core.parsers.elasticc_parser.ElasticcParser",
-}
+messages_elasticc = list(utils.generate_many(INPUT_ELASTICC, 2))
 
-messages_elasticc = utils.generate_many(INPUT_ELASTICC, 10)
+for message in messages_elasticc:
+    for det in message["detections"]:
+        det["aid"] = message["aid"]
+        det["extra_fields"] = generate_extra_fields()
+    message["detections"][0]["new"] = True
+    message["detections"][0]["has_stamp"] = True
 
 
 @pytest.mark.skipif(os.getenv("STREAM") != "elasticc", reason="elasticc only")
-@pytest.mark.skip(reason="Model not implemented yet")
-def test_step_elasticc():
-    step = LateClassifier(config=step_mock_config)
-    step.consumer = mock.MagicMock(KafkaConsumer)
-    step.consumer.consume.return_value = messages_elasticc
-    step.producer = mock.MagicMock(KafkaProducer)
-    step.scribe_producer = mock.create_autospec(KafkaProducer)
+def test_step_barney(test_elasticc_model, step_factory_barney):
+    test_elasticc_model(step_factory_barney, messages_elasticc)
 
+
+@pytest.mark.skipif(os.getenv("STREAM") != "elasticc", reason="elasticc only")
+def test_step_toretto_model_input_is_correct(step_factory_barney):
+    step = step_factory_barney(messages_elasticc)
+    step.start()
+    predictor_calls = step.predictor.model.predict.mock_calls
+    assert len(predictor_calls) > 0
+    for call in predictor_calls:
+        # check that there are features in the input of the model
+        assert call[1][0].features.any().any()
+
+
+@pytest.mark.skipif(os.getenv("STREAM") != "elasticc", reason="elasticc only")
+def test_step_elasticc_without_features(step_factory_barney):
+    empty_features = []
+    for msg in messages_elasticc:
+        msg["features"] = None
+        empty_features.append(msg)
+    step = step_factory_barney(empty_features)
     step.start()
     scribe_calls = step.scribe_producer.mock_calls
-
+    scribe_calls = step.scribe_producer.mock_calls
+    predictor_calls = step.predictor.model.predict.mock_calls
+    assert len(predictor_calls) == 0
     # Tests scribe produces correct commands
+    assert len(scribe_calls) == 0
     for call in scribe_calls:
         message = loads(call.args[0]["payload"])
         assert_command_is_correct(message)
-
     # Test producer produces correct data
     calls = step.producer.mock_calls
+    assert len(calls) == len(empty_features)
     for call in calls:
         obj = call.args[0]
         assert_elasticc_object_is_correct(obj)
