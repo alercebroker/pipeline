475b5a550ab8dbc1edbe4bb9fd06f928ebe75318
Héctor Larrañaga <hectorPLJ@gmail.com>
2023-06-22 13:21:00 -0400

    fix: specify model name for `ElasticcRandomForest` in Barney and Toretto

--- alerce_classifiers/rf_features_header_classifier/model.py
+++ alerce_classifiers/rf_features_header_classifier/model.py
@@ -38,7 +38,11 @@ class RandomForestFeaturesHeaderClassifier(AlerceModel[pd.DataFrame]):
 
     def _load_model(self, path_to_model: str) -> None:
         self.model = ElasticcRandomForest(
-            self.taxonomy_dictionary, self.non_used_features, n_trees=500, n_jobs=1
+            self.taxonomy_dictionary,
+            self.non_used_features,
+            n_trees=500,
+            n_jobs=1,
+            model_name="barney_elasticc_rf",
         )
         if validators.url(path_to_model):
             self.model.url_model = path_to_model
