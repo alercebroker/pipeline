88a3bf49d5613abe50bdf80f92d74c021832dd25
Pedro Gallardo <pegallardo@inf.udec.cl>
2023-06-30 16:44:16 -0400

    style: black formatting

--- sorting_hat_step/utils/statistics.py
+++ sorting_hat_step/utils/statistics.py
@@ -3,5 +3,6 @@ def get_prv_candidates_len(row):
     prv_candidates_len = len(row["extra_fields"].get("prv_candidates"))
     return dia_sources_len + prv_candidates_len
 
+
 def get_prv_forced_len(row):
     return len(row["extra_fields"].get("prvDiaForcedSources", []))
