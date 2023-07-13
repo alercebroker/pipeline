cba0a321e392cc23b5cfdb3cf44f3eacf64979b6
Diego Rodriguez <diegorodriguez@Diegos-MacBook-Pro.local>
2023-07-13 10:50:29 -0400

    merge lightcurve-step repo

--- lightcurve-step/lightcurve_step/step.py
+++ lightcurve-step/lightcurve_step/step.py
@@ -1,17 +1,19 @@
-from typing import List
-import pandas as pd
-import pickle
+import logging
 import numpy as np
+import pandas as pd
+
+from typing import List
 from apf.core.step import GenericStep
 from db_plugins.db.mongo import MongoConnection
 from db_plugins.db.mongo.models import Detection, NonDetection, ForcedPhotometry
 
 
 class LightcurveStep(GenericStep):
-    def __init__(self, config: dict, db_client: MongoConnection):
-        super().__init__(config=config)
+    def __init__(self, config: dict, db_client: MongoConnection, **kwargs):
+        super().__init__(config=config, **kwargs)
         self.db_client = db_client
         self.db_client.connect(config["DB_CONFIG"])
+        self.logger = logging.getLogger("alerce.LightcurveStep")
 
     @classmethod
     def pre_execute(cls, messages: List[dict]) -> dict:
@@ -20,6 +22,8 @@ class LightcurveStep(GenericStep):
             aids.add(msg["aid"])
             detections.extend([det | {"new": True} for det in msg["detections"]])
             non_detections.extend(msg["non_detections"])
+        logger = logging.getLogger("alerce.LightcurveStep")
+        logger.debug(f"Received {len(detections)} detections from messages")
         return {
             "aids": aids,
             "detections": detections,
@@ -37,17 +41,17 @@ class LightcurveStep(GenericStep):
                 {"$match": {"aid": {"$in": list(messages["aids"])}}},
                 {
                     "$addFields": {
-                        "candid": {"$toString": "$_id"},
-                        "parent_candid": {"$toString": "$parent_candid"},
+                        "candid": "$_id",
                         "forced": False,
                         "new": False,
                     }
                 },
-                {"$project": {"_id": False}},
+                {"$project": {"_id": False, "evilDocDbHack": False}},
             ]
         )
         db_non_detections = query_non_detections.collection.find(
-            {"aid": {"$in": list(messages["aids"])}}, {"_id": False}
+            {"aid": {"$in": list(messages["aids"])}},
+            {"_id": False, "evilDocDbHack": False},
         )
 
         db_forced_photometries = query_forced_photometries.collection.aggregate(
@@ -55,13 +59,12 @@ class LightcurveStep(GenericStep):
                 {"$match": {"aid": {"$in": list(messages["aids"])}}},
                 {
                     "$addFields": {
-                        "candid": {"$toString": "$_id"},
-                        "parent_candid": {"$toString": "$parent_candid"},
+                        "candid": "$_id",
                         "forced": True,
                         "new": False,
                     }
                 },
-                {"$project": {"_id": False}},
+                {"$project": {"_id": False, "evilDocDbHack": False}},
             ]
         )
 
@@ -71,12 +74,19 @@ class LightcurveStep(GenericStep):
         non_detections = pd.DataFrame(
             messages["non_detections"] + list(db_non_detections)
         )
+        self.logger.debug(f"Retrieved {detections.shape[0]} detections")
+        detections["candid"] = detections["candid"].astype(str)
+        detections["parent_candid"] = detections["parent_candid"].astype(str)
 
         # Try to keep those with stamp coming from the DB if there are clashes
+        # maybe drop duplicates with candid and AID in LSST/ELAsTiCC
         detections = detections.sort_values(
             ["has_stamp", "new"], ascending=[False, True]
         ).drop_duplicates("candid", keep="first")
         non_detections = non_detections.drop_duplicates(["aid", "fid", "mjd"])
+        self.logger.debug(
+            f"Obtained {len(detections[detections['new']])} new detections"
+        )
 
         return {
             "detections": detections,
