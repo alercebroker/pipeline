6589135a03dca7cbe408b8df3f7a89f323b72a89
Pedro Gallardo <pedrogallardorobinson@gmail.com>
2023-07-24 17:04:07 -0400

    Squashed 'lc_classification_step/' content from commit 40ec7fca
    git-subtree-dir: lc_classification_step
    git-subtree-split: 40ec7fca5312706d543fd1b24d7b951c4f6b380a

---
+++ settings.py
@@ -0,0 +1,173 @@
+##################################################
+#       Late Classifier Settings File
+##################################################
+import os
+from features_schema import FEATURES_SCHEMA
+
+CONSUMER_CONFIG = {
+    "CLASS": os.getenv("CONSUMER_CLASS", "apf.consumers.KafkaConsumer"),
+    "TOPICS": os.environ["CONSUMER_TOPICS"].strip().split(","),
+    "PARAMS": {
+        "bootstrap.servers": os.environ["CONSUMER_SERVER"],
+        "group.id": os.environ["CONSUMER_GROUP_ID"],
+        "auto.offset.reset": "smallest",
+    },
+    "consume.timeout": int(os.getenv("CONSUME_TIMEOUT", 10)),
+    "consume.messages": int(os.getenv("CONSUME_MESSAGES", 1000)),
+}
+
+DB_CONFIG = {
+    "SQL": {
+        "ENGINE": os.getenv("DB_ENGINE", "postgresql"),
+        "HOST": os.environ["DB_HOST"],
+        "USER": os.environ["DB_USER"],
+        "PASSWORD": os.environ["DB_PASSWORD"],
+        "PORT": int(os.getenv("DB_PORT", 5432)),
+        "DB_NAME": os.environ["DB_NAME"],
+    }
+}
+
+PRODUCER_CONFIG = {
+    "TOPIC_STRATEGY": {
+        "PARAMS": {
+            "topic_format": os.environ["PRODUCER_TOPIC_FORMAT"],
+            "date_format": os.environ["PRODUCER_DATE_FORMAT"],
+            "change_hour": int(os.environ["PRODUCER_CHANGE_HOUR"]),
+            "retention_days": int(os.environ["PRODUCER_RETENTION_DAYS"]),
+        },
+        "CLASS": os.getenv(
+            "PRODUCER_TOPIC_STRATEGY_CLASS",
+            "apf.core.topic_management.DailyTopicStrategy",
+        ),
+    },
+    "PARAMS": {
+        "bootstrap.servers": os.environ["PRODUCER_SERVER"],
+    },
+    "SCHEMA": {
+        "doc": "Late Classification",
+        "name": "probabilities_and_features",
+        "type": "record",
+        "fields": [
+            {"name": "oid", "type": "string"},
+            {"name": "candid", "type": "long"},
+            FEATURES_SCHEMA,
+            {
+                "name": "lc_classification",
+                "type": {
+                    "type": "record",
+                    "name": "late_record",
+                    "fields": [
+                        {
+                            "name": "probabilities",
+                            "type": {
+                                "type": "map",
+                                "values": ["float"],
+                            },
+                        },
+                        {"name": "class", "type": "string"},
+                        {
+                            "name": "hierarchical",
+                            "type": {
+                                "name": "root",
+                                "type": "map",
+                                "values": [
+                                    {"type": "map", "values": "float"},
+                                    {
+                                        "type": "map",
+                                        "values": {"type": "map", "values": "float"},
+                                    },
+                                ],
+                            },
+                        },
+                    ],
+                },
+            },
+        ],
+    },
+}
+
+METRICS_CONFIG = {
+    "CLASS": "apf.metrics.KafkaMetricsProducer",
+    "EXTRA_METRICS": [
+        {"key": "candid", "format": lambda x: str(x)},
+        {"key": "oid", "alias": "oid"},
+        {"key": "aid", "alias": "aid"},
+        {"key": "tid", "format": lambda x: str(x)},
+    ],
+    "PARAMS": {
+        "PARAMS": {
+            "bootstrap.servers": os.environ["METRICS_HOST"],
+            "auto.offset.reset": "smallest",
+        },
+        "TOPIC": os.environ["METRICS_TOPIC"],
+        "SCHEMA": {
+            "$schema": "http://json-schema.org/draft-07/schema",
+            "$id": "http://example.com/example.json",
+            "type": "object",
+            "title": "The root schema",
+            "description": "The root schema comprises the entire JSON document.",
+            "default": {},
+            "examples": [
+                {"timestamp_sent": "2020-09-01", "timestamp_received": "2020-09-01"}
+            ],
+            "required": ["timestamp_sent", "timestamp_received"],
+            "properties": {
+                "timestamp_sent": {
+                    "$id": "#/properties/timestamp_sent",
+                    "type": "string",
+                    "title": "The timestamp_sent schema",
+                    "description": "Timestamp sent refers to the time at which a message is sent.",
+                    "default": "",
+                    "examples": ["2020-09-01"],
+                },
+                "timestamp_received": {
+                    "$id": "#/properties/timestamp_received",
+                    "type": "string",
+                    "title": "The timestamp_received schema",
+                    "description": "Timestamp received refers to the time at which a message is received.",
+                    "default": "",
+                    "examples": ["2020-09-01"],
+                },
+            },
+            "additionalProperties": True,
+        },
+    },
+}
+
+if os.getenv("CONSUMER_KAFKA_USERNAME") and os.getenv("CONSUMER_KAFKA_PASSWORD"):
+    CONSUMER_CONFIG["PARAMS"]["security.protocol"] = "SASL_SSL"
+    CONSUMER_CONFIG["PARAMS"]["sasl.mechanism"] = "SCRAM-SHA-512"
+    CONSUMER_CONFIG["PARAMS"]["sasl.username"] = os.getenv("CONSUMER_KAFKA_USERNAME")
+    CONSUMER_CONFIG["PARAMS"]["sasl.password"] = os.getenv("CONSUMER_KAFKA_PASSWORD")
+if os.getenv("PRODUCER_KAFKA_USERNAME") and os.getenv("PRODUCER_KAFKA_PASSWORD"):
+    PRODUCER_CONFIG["PARAMS"]["security.protocol"] = os.getenv(
+        "PRODUCER_SECURITY_PROTOCOL", "SASL_PLAINTEXT"
+    )
+    PRODUCER_CONFIG["PARAMS"]["sasl.mechanism"] = os.getenv(
+        "PRODUCER_SASL_MECHANISM", "SCRAM-SHA-256"
+    )
+    PRODUCER_CONFIG["PARAMS"]["sasl.username"] = os.getenv("PRODUCER_KAFKA_USERNAME")
+    PRODUCER_CONFIG["PARAMS"]["sasl.password"] = os.getenv("PRODUCER_KAFKA_PASSWORD")
+if os.getenv("METRICS_KAFKA_USERNAME") and os.getenv("METRICS_KAFKA_PASSWORD"):
+    METRICS_CONFIG["PARAMS"]["PARAMS"]["security.protocol"] = "SASL_SSL"
+    METRICS_CONFIG["PARAMS"]["PARAMS"]["sasl.mechanism"] = "SCRAM-SHA-512"
+    METRICS_CONFIG["PARAMS"]["PARAMS"]["sasl.username"] = os.getenv(
+        "METRICS_KAFKA_USERNAME"
+    )
+    METRICS_CONFIG["PARAMS"]["PARAMS"]["sasl.password"] = os.getenv(
+        "METRICS_KAFKA_PASSWORD"
+    )
+
+STEP_METADATA = {
+    "STEP_VERSION": os.getenv("STEP_VERSION", "dev"),
+    "STEP_ID": os.getenv("STEP_ID", "lc_classification"),
+    "STEP_NAME": os.getenv("STEP_NAME", "late classification"),
+    "STEP_COMMENTS": os.getenv("STEP_COMMENTS", ""),
+}
+
+STEP_CONFIG = {
+    "DB_CONFIG": DB_CONFIG,
+    "PRODUCER_CONFIG": PRODUCER_CONFIG,
+    "STEP_METADATA": STEP_METADATA,
+    "METRICS_CONFIG": METRICS_CONFIG,
+}
---
+++ tests/integration/test_psql.py
@@ -0,0 +1,80 @@
+import unittest
+import numpy as np
+import pickle
+import pytest
+
+from unittest import mock
+from db_plugins.db.sql.models import Step, Object, Probability
+from lc_classification.step import LateClassifier, KafkaProducer
+
+
+@pytest.mark.usefixtures("psql_service")
+class PSQLIntegrationTest(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(self):
+        db_config = {
+            "SQL": {
+                "ENGINE": "postgresql",
+                "HOST": "localhost",
+                "USER": "postgres",
+                "PASSWORD": "postgres",
+                "PORT": 5432,
+                "DB_NAME": "postgres",
+            }
+        }
+        self.step_config = {
+            "DB_CONFIG": db_config,
+            "STEP_METADATA": {
+                "STEP_ID": "",
+                "STEP_NAME": "",
+                "STEP_VERSION": "",
+                "STEP_COMMENTS": "",
+                "CLASSIFIER_VERSION": "test",
+                "CLASSIFIER_NAME": "hrf_test",
+            },
+            "PRODUCER_CONFIG": {},
+            "N_RETRY": 5,
+        }
+        self.mock_producer = mock.create_autospec(KafkaProducer)
+        self.step = LateClassifier(
+            config=self.step_config,
+            producer=self.mock_producer,
+            test_mode=True
+        )
+
+    @classmethod
+    def tearDownClass(self):
+        self.step.driver.drop_db()
+        self.step.driver.session.close()
+
+    def setUp(self):
+        self.probabilities = {}
+        with open("tests/unittest/response_batch.pickle", "rb") as f:
+            self.probabilities = pickle.load(f)
+
+        self.step.driver.create_db()
+
+    def tearDown(self):
+        self.step.driver.session.close()
+        self.step.driver.drop_db()
+
+    def test_insert_step_metadata(self):
+        self.step.insert_step_metadata()
+        self.assertEqual(len(self.step.driver.query(Step).all()), 1)
+
+    def test_insert_db(self):
+        db_results = self.step.process_results(self.probabilities)
+        oids = [f"ZTF{i}" for i in range(10)]
+
+        for i in oids:
+            self.step.driver.session.query().get_or_create(
+                Object, {
+                    "oid": i
+                }
+            )
+        db_results["oid"] = db_results["oid"].map(lambda x: f"ZTF{x}")
+
+        self.step.insert_db(db_results, oids)
+        self.assertEqual(len(self.step.driver.query(Object).all()), 10)
+        self.assertEqual(len(self.step.driver.query(Probability).all()), 330)
