593e0f35c0d0dc456058177274f911dbb3830192
ASHuenchuleo <arsepulh@gmail.com>
2023-08-01 10:12:13 -0400

    refactor(lc_classifier): Refactored create_features_dto, and added a test for it.

--- lc_classification_step/tests/unit/test_input_dto.py
+++ lc_classification_step/tests/unit/test_input_dto.py
@@ -1,5 +1,6 @@
-from lc_classification.core.parsers.input_dto import create_detections_dto
+from lc_classification.core.parsers.input_dto import create_detections_dto, create_features_dto
 import pickle
+import numpy as np
 
 
 def test_create_detections_dto():
@@ -25,3 +26,31 @@ def test_create_detections_dto():
         {"ef1": {"data1": "data1"}, "ef2": "val2"},
         {},
     ]
+
+def test_create_features_dto():
+    messages = [
+            {
+                'aid': 'aid1',
+                'features': {'feat1': 1, 'feat2': 2}
+            },
+            {
+                'aid': 'aid1',
+                'features': {'feat1': 2, 'feat2': 3}
+            },
+            {
+                'aid': 'aid2',
+                'features': {'feat1': 4, 'feat2': 5}
+            },
+            {
+                'aid': 'aid3',
+            },
+            {
+                'aid': 'aid4',
+                'features': {'feat1': 4}
+            },
+        ]
+    features = create_features_dto(messages)
+    assert features.loc['aid1', 'feat1'] == 2
+    assert features.loc['aid1', 'feat2'] == 3
+    assert np.isnan(features.loc['aid4', 'feat2'])
+    assert features.index.tolist() == ["aid1", "aid2", "aid4"]
