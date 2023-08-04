4b9473ff9814d687a4849f2d928520e06b7e56ce
Diego Rodriguez <diegorodriguez@Diegos-MacBook-Pro.local>
2023-08-04 16:43:09 -0400

    tests: fix by adding producer.flush

--- scribe/tests/integration/test_batch.py
+++ scribe/tests/integration/test_batch.py
@@ -86,6 +86,7 @@ def test_bulk(kafka_service, mongo_service):
     for i, command in enumerate(commands):
         producer.produce(command)
 
+    producer.producer.flush()
     step.start()
     collection = step.db_client.connection.database["object"]
 
