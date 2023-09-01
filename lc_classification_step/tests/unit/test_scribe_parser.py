d144dfea2d41bf69bc85fdae50566fb560c662e4
Pedro Gallardo <pedrogallardorobinson@gmail.com>
2023-09-01 09:21:52 -0400

    test: classifier version test

--- lc_classification_step/tests/unit/test_scribe_parser.py
+++ lc_classification_step/tests/unit/test_scribe_parser.py
@@ -20,7 +20,7 @@ def test_parse():
     parser = ScribeParser(classifier_name="test_classifier")
     result: KafkaOutput = parser.parse(output_dto, classifier_version="test_v1")
     for res in result.value:
-        assert res["data"]["classifier_version"] == "test"
+        assert res["data"]["classifier_version"] == "test_v1"
         assert res["collection"] == "object"
         assert res["type"] == "update_probabilities"
         assert "_id" in res["criteria"]
