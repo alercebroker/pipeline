fbd6c00bd97ac791835ef401c4c682f34e14c976
Pedro Gallardo <pedrogallardorobinson@gmail.com>
2023-09-07 12:24:59 -0300

    style: black formatting

--- correction_step/correction/_step/step.py
+++ correction_step/correction/_step/step.py
@@ -42,7 +42,9 @@ class CorrectionStep(GenericStep):
         logger = logging.getLogger("alerce")
         logger.setLevel(level)
 
-        fmt = logging.Formatter("%(asctime)s %(levelname)7s %(name)36s: %(message)s", "%Y-%m-%d %H:%M:%S")
+        fmt = logging.Formatter(
+            "%(asctime)s %(levelname)7s %(name)36s: %(message)s", "%Y-%m-%d %H:%M:%S"
+        )
         handler = logging.StreamHandler()
         handler.setFormatter(fmt)
         handler.setLevel(level)
@@ -50,18 +52,26 @@ class CorrectionStep(GenericStep):
         logger.addHandler(handler)
 
         prefix = os.getenv("CLASS_PREFIX", "")
-        prometheus_metrics = PrometheusMetrics() if settings["PROMETHEUS"] else DefaultPrometheusMetrics()
+        prometheus_metrics = (
+            PrometheusMetrics()
+            if settings["PROMETHEUS"]
+            else DefaultPrometheusMetrics()
+        )
         if settings["PROMETHEUS"]:
             start_http_server(8000)
 
-        return CorrectionStep(config=settings, prometheus_metrics=prometheus_metrics, prefix=prefix)
+        return CorrectionStep(
+            config=settings, prometheus_metrics=prometheus_metrics, prefix=prefix
+        )
 
     @classmethod
     def pre_produce(cls, result: dict):
         detections = pd.DataFrame(result["detections"]).groupby("aid")
         try:  # At least one non-detection
             non_detections = pd.DataFrame(result["non_detections"]).groupby("aid")
-        except (KeyError):  # to reproduce expected error for missing non-detections in loop
+        except (
+            KeyError
+        ):  # to reproduce expected error for missing non-detections in loop
             non_detections = pd.DataFrame(columns=["aid"]).groupby("aid")
         output = []
         for aid, dets in detections:
@@ -92,7 +102,9 @@ class CorrectionStep(GenericStep):
     def execute(cls, message: dict) -> dict:
         corrector = Corrector(message["detections"])
         detections = corrector.corrected_as_records()
-        non_detections = pd.DataFrame(message["non_detections"]).drop_duplicates(["oid", "fid", "mjd"])
+        non_detections = pd.DataFrame(message["non_detections"]).drop_duplicates(
+            ["oid", "fid", "mjd"]
+        )
         coords = corrector.coordinates_as_records()
         return {
             "detections": detections,
