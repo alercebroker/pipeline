cc7928ee71ba72362951c2281a6c95e52966da77
Diego Rodriguez <diegorodriguez@Diegos-MacBook-Pro.local>
2023-10-06 17:43:54 -0300

    ci: build each step separately :(

--- ci/build_staging.py
+++ ci/build_staging.py
@@ -20,25 +20,11 @@ async def _update_chart_version(packages: list, dry_run: bool):
             tg.start_soon(update_chart, package, dry_run)
 
 
-def build_staging(dry_run: bool):
-    packages = [
-        "correction_step",
-        "feature_step",
-        "lightcurve-step",
-        "magstats_step",
-        "prv_candidates_step",
-        "scribe",
-        "sorting_hat_step",
-        "xmatch_step",
-    ]
-    libs = [
-        "libs/apf",
-        "libs/db-plugins",
-        "libs/lc_classifier",
-        "libs/survey_parser_plugins",
-        "libs/turbo-fats",
-    ]
+def update_packages(packages, libs, dry_run: bool):
     anyio.run(_update_package_version, packages + libs, dry_run)
     anyio.run(_update_chart_version, packages, dry_run)
     anyio.run(git_push, dry_run)
+
+
+def build_staging(packages: list, dry_run: bool):
     anyio.run(_build_package, packages, dry_run)
--- ci/main.py
+++ ci/main.py
@@ -1,11 +1,22 @@
 import sys
-from build_staging import build_staging
+from build_staging import build_staging, update_packages
 from deploy_staging import deploy_staging
 
 
-def build(stage, dry_run):
+def update_versions(stage, packages, libs, dry_run):
     if stage == "staging":
-        build_staging(dry_run)
+        update_packages(packages, libs, dry_run)
+    elif stage == "production":
+        print("Production update not implemented yet")
+    else:
+        raise ValueError(
+            f'Invalid stage "{stage}". Valid stages are: staging, production'
+        )
+
+
+def build(stage, packages, dry_run):
+    if stage == "staging":
+        build_staging(packages, dry_run)
     elif stage == "production":
         print("Production build not implemented yet")
     else:
@@ -28,12 +39,16 @@ def deploy(stage, dry_run):
 if __name__ == "__main__":
     command = sys.argv[1]
     stage = sys.argv[2]
+    packages = sys.argv[3]
+    packages = packages.split(",")
     dry_run = False
-    if len(sys.argv) > 3:
-        dry_run = sys.argv[3]
+    if len(sys.argv) > 4:
+        dry_run = sys.argv[4]
         dry_run = dry_run == "--dry-run"
         print("Running with --dry-run")
+    if command == "update-versions":
+        update_versions(stage, packages, [], dry_run)
     if command == "build":
-        build(stage, dry_run)
+        build(stage, packages, dry_run)
     if command == "deploy":
         deploy(stage, dry_run)
