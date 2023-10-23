49732996d77730e8d686801adc90fe70a79772a3
Diego Rodriguez <diegorodriguez@Diegos-MacBook-Pro.local>
2023-10-23 15:57:18 -0300

    chore: fix black format

--- lightcurve-step/lightcurve_step/step.py
+++ lightcurve-step/lightcurve_step/step.py
@@ -164,7 +164,7 @@ class LightcurveStep(GenericStep):
         return parsed_result
 
     def _parse_ztf_non_detection(self, ztf_models: list, *, oids):
-        FID = {1: 'g', 2: 'r', 0: None, 12: 'gr'}
+        FID = {1: "g", 2: "r", 0: None, 12: "gr"}
         non_dets = []
         for non_det in ztf_models:
             non_det = non_det[0].__dict__
