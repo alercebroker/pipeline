31165da762f91d4f2d4e8eb97220c91d5dcff97c
Pedro Gallardo <pedrogallardorobinson@gmail.com>
2023-10-30 16:32:13 -0300

    feat: added metadata_step structure

--- metadata_step/metadata_step/step.py
+++ metadata_step/metadata_step/step.py
@@ -1,7 +1,8 @@
-from typing import Any, Dict, Iterable, List, Union
+import json
+from typing import Dict, List
 
 from apf.core.step import GenericStep, get_class
-
+from utils.parse import format_detection
 
 class MetadataStep(GenericStep):
     def __init__(self, config, **step_args):
@@ -9,9 +10,28 @@ class MetadataStep(GenericStep):
         cls = get_class(config["SCRIBE_PRODUCER_CONFIG"]["CLASS"])
         self.scribe_producer = cls(config["SCRIBE_PRODUCER_CONFIG"])
 
-    def execute(self, messages: List[Dict]):
-        for message in messages:
-            print(message)
+    def _format_detection(self, d: Dict):
+        d = d.copy()
+        extra_fields = d.pop("extra_fields")
+        extra_fields.pop("fp_hists", {})
+        extra_fields.pop("prv_candidates", {})
+        return format_detection({**d, **extra_fields})
 
+    def _produce_scribe(self, result: List[Dict]):
+        for data in result:
+            payload = {
+                "type": "upsert",
+                "collection": "metadata",
+                "data": data
+            }
+            self.scribe_producer.produce({"payload": json.dumps(payload)})
+
+    # Output format: [{oid: OID, ss: SS_DATA, ...}]
+    def execute(self, messages: List[Dict]):
+        result = [self._format_detection(message) for message in messages]
+        return result
+
+        
     def post_execute(self, result: List[Dict]):
-        pass
+        self._produce_scribe(result)
+        return []
---
+++ metadata_step/metadata_step/utils/parse.py
@@ -0,0 +1,51 @@
+import numpy as np
+
+from typing import Dict, List
+from constants import REFERENCE_KEYS, GAIA_KEYS, SS_KEYS, PS1_KEYS, DATAQUALITY_KEYS
+
+def _clear_nan(value):
+    if value == np.nan:
+        return None
+    return value
+
+def _filter_keys(d: Dict, keys: List):
+    return {k: _clear_nan(v) for k, v in d.items() if d in keys}
+
+# only needs to filter keys
+def format_ss(alert: Dict):
+    alert =  _filter_keys(alert, SS_KEYS)
+    return alert
+
+# only needs to filter keys
+def format_dataquality(alert: Dict):
+    alert = _filter_keys(alert, DATAQUALITY_KEYS)
+    return alert
+
+# filter + calculate mjd
+def format_reference(alert: Dict):
+    new_alert = _filter_keys(alert, REFERENCE_KEYS)
+    new_alert["mjdstartref"] = alert["jdstartref"] - 2400000.5
+    new_alert["mjdendref"] = alert["jdendref"] - 2400000.5
+    return alert
+
+# needs past alerts/database catalog
+def format_gaia(alert: Dict):
+    alert = _filter_keys(alert, GAIA_KEYS)
+    return alert
+
+# this one too
+def format_ps1(alert: Dict):
+    alert = _filter_keys(alert, PS1_KEYS)
+    return alert
+
+
+# formats each alert to send it to scribe psql
+def format_detection(alert: Dict):
+    return {
+        "oid": alert["oid"],
+        "ss": format_ss(alert),
+        "reference": format_reference(alert),
+        "gaia": format_gaia(alert),
+        "ps1": format_ps1(alert),
+        "dataquality": format_dataquality(alert),
+    }
