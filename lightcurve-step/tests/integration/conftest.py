7c1ff7c0fb9dade61a335baf8c8268293459b298
Diego Rodriguez <diegorodriguez@Diegos-MacBook-Pro.local>
2023-10-31 19:35:03 -0300

    feat(lightcurve-step): update fastavro and python version

--- lightcurve-step/tests/integration/conftest.py
+++ lightcurve-step/tests/integration/conftest.py
@@ -42,6 +42,7 @@ def env_variables():
         "MONGODB_SECRET_NAME": "mongo_secret",
         "SQL_SECRET_NAME": "sql_secret",
         "CONSUME_MESSAGES": "10",
+        "CONSUME_TIMEOUT": "0",
     }
     for key in env_variables_dict:
         os.environ[key] = env_variables_dict[key]
@@ -92,15 +93,19 @@ def populate_sql(conn: PsqlDatabase):
             )
         )
         session.execute(
-            text("""
+            text(
+                """
             INSERT INTO non_detection(oid, fid, mjd, diffmaglim) VALUES ('ZTF000llmn', 1, 55000, 42.00)
-            """)
+            """
+            )
         )
         session.execute(
-            text("""
-            INSERT INTO forced_photometry(oid, mjd, pid, fid, ra, dec, isdiffpos, corrected, dubious, has_stamp) 
+            text(
+                """
+            INSERT INTO forced_photometry(oid, mjd, pid, fid, ra, dec, isdiffpos, corrected, dubious, has_stamp)
             VALUES ('ZTF000llmn', 55500, 9182734, 1, 45.0, 45.0, 1, false, false, false)
-            """)
+            """
+            )
         )
         session.commit()
 
