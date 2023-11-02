bdc310e4df98de5a126694de4366a4519f12e531
Pedro Gallardo <pedrogallardorobinson@gmail.com>
2023-11-02 16:20:53 -0300

    test: added mock messages

---
+++ metadata_step/tests/data/messages.py
@@ -0,0 +1,12 @@
+def new_message_batch():
+    return [{
+        "oid": "ZTF00llmn",
+        "aid": "AID1",
+        "candid": 321321321,
+        "extra_fields": {}
+    }, {
+        "oid": "ZTF21waka",
+        "aid": "AID2",
+        "candid": 930930930,
+        "extra_fields": {}
+    }]
---
+++ metadata_step/tests/integration/test_step.py
@@ -0,0 +1,7 @@
+from metadata_step.step import MetadataStep
+
+def _populate():
+    pass
+
+def test_step():
+    pass
