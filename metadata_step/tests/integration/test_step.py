75cfdd3c803b1bf3929991215f4178fdf2680404
Pedro Gallardo <pedrogallardorobinson@gmail.com>
2023-11-03 10:01:30 -0300

    test: added test step structure

--- metadata_step/tests/integration/test_step.py
+++ metadata_step/tests/integration/test_step.py
@@ -1,7 +1,22 @@
 from metadata_step.step import MetadataStep
+from metadata_step.utils.database import PSQLConnection
+from metadata_step.tests.data.messages import new_message_batch
 
-def _populate():
+def _populate_db(connection: PSQLConnection):
     pass
 
-def test_step():
+def _create_connection() -> PSQLConnection:
     pass
+
+def test_step(psql_service):
+    # go step by step
+    db = _create_connection()
+    _populate_db(db)
+    step = MetadataStep({}, db)
+
+    messages = new_message_batch()
+    result = step.execute(messages)
+    step.post_execute(result)
+
+    # assert everything went in correctly
+    assert True
