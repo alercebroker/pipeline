f21e9064a4ecd610800ad6695258b0c81459ce3a
Diego Rodriguez <diegorodriguez@Diegos-MacBook-Pro.local>
2023-11-14 10:33:52 -0300

    tests(lc_classification_step): fix unit tests with schema path

--- lc_classification_step/tests/unit/test_step_messi.py
+++ lc_classification_step/tests/unit/test_step_messi.py
@@ -1,18 +1,7 @@
 import pytest
-from fastavro import utils
-
-from tests.mockdata.extra_felds import generate_extra_fields
-from tests.mockdata.input_elasticc import INPUT_SCHEMA as INPUT_ELASTICC
-
-messages_elasticc = list(utils.generate_many(INPUT_ELASTICC, 2))
-for message in messages_elasticc:
-    for det in message["detections"]:
-        det["aid"] = message["aid"]
-        det["extra_fields"] = generate_extra_fields()
-    message["detections"][0]["new"] = True
-    message["detections"][0]["has_stamp"] = True
+from tests.unit.test_utils import generate_messages_elasticc
 
 
 @pytest.mark.elasticc
 def test_step_messi(test_elasticc_model, step_factory_messi):
-    test_elasticc_model(step_factory_messi, messages_elasticc)
+    test_elasticc_model(step_factory_messi, generate_messages_elasticc())
--- lc_classification_step/tests/unit/test_utils.py
+++ lc_classification_step/tests/unit/test_utils.py
@@ -1,13 +1,60 @@
+import pathlib
+import random
+from typing import List
 import unittest
+
 import pandas as pd
-from tests.mockdata.mock_data_for_utils import (
-    messages_df,
-    complete_classifications_df,
-    incomplete_classifications_df,
-)
+from fastavro import schema, utils
 from lc_classification.core.utils.no_class_post_processor import (
     NoClassifiedPostProcessor,
 )
+from tests.mockdata.extra_felds import generate_extra_fields
+from tests.mockdata.mock_data_for_utils import (
+    complete_classifications_df,
+    incomplete_classifications_df,
+    messages_df,
+)
+
+
+def generate_messages_elasticc() -> List[dict]:
+    schema_path = pathlib.Path(
+        pathlib.Path(__file__).parent.parent.parent.parent,
+        "schemas/feature_step",
+        "output.avsc",
+    )
+    schema_path = str(schema_path)
+    input_schema = schema.load_schema(schema_path)
+    messages_elasticc = list(utils.generate_many(input_schema, 2))
+
+    for message in messages_elasticc:
+        for det in message["detections"]:
+            det["aid"] = message["aid"]
+            det["candid"] = random.randint(0, 100000)
+            det["oid"] = random.randint(0, 100000)
+            det["extra_fields"] = generate_extra_fields()
+        message["detections"][0]["new"] = True
+        message["detections"][0]["has_stamp"] = True
+    return messages_elasticc
+
+
+def generate_messages_ztf() -> List[dict]:
+    schema_path = (
+        pathlib.Path(__file__).parent.parent.parent.parent
+        / "schemas"
+        / "feature_step"
+        / "output.avsc"
+    )
+    schema_path = str(schema_path)
+    input_schema = schema.load_schema(schema_path)
+    messages_ztf = list(utils.generate_many(input_schema, 2))
+
+    for message in messages_ztf:
+        for det in message["detections"]:
+            det["aid"] = message["aid"]
+            det["extra_fields"] = {}
+        message["detections"][0]["new"] = True
+        message["detections"][0]["has_stamp"] = True
+    return messages_ztf
 
 
 class NoClassifiedPostProcessorTestCase(unittest.TestCase):
