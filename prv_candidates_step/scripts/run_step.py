92266d229b4245eb4a696b594c960758fbe4275b
Diego Rodriguez <diegorodriguezmancini@gmail.com>
2023-11-15 13:14:24 -0300

    refactor(prv_candidates_step): allow settings from yaml file

--- prv_candidates_step/scripts/run_step.py
+++ prv_candidates_step/scripts/run_step.py
@@ -4,6 +4,7 @@ import sys
 
 from prometheus_client import start_http_server
 from apf.metrics.prometheus import PrometheusMetrics
+from apf.core.settings import config_from_yaml_file
 
 SCRIPT_PATH = os.path.dirname(os.path.abspath(__file__))
 PACKAGE_PATH = os.path.abspath(os.path.join(SCRIPT_PATH, ".."))
@@ -15,13 +16,29 @@ from prv_candidates_step.step import PrvCandidatesStep
 prometheus_metrics = PrometheusMetrics()
 
 
-def step_creator():
-    from settings import settings_creator
+def update_extra_metrics(settings):
+    extra_metrics = settings["METRICS_CONFIG"]["EXTRA_METRICS"]
+    extra_metrics = list(
+        map(
+            lambda x: {**x, "format": lambda x: str(x)} if x["key"] == "candid" else x,
+            extra_metrics,
+        )
+    )
+    settings["METRICS_CONFIG"]["EXTRA_METRICS"] = extra_metrics
+    return settings
 
-    settings = settings_creator()
+
+def step_creator():
+    if os.getenv("CONFIG_FROM_YAML"):
+        settings = config_from_yaml_file("/config/config.yaml")
+        settings = update_extra_metrics(settings)
+    else:
+        from settings import settings_creator
+
+        settings = settings_creator()
 
     level = logging.INFO
-    if os.getenv("LOGGING_DEBUG"):
+    if settings.get("LOGGING_DEBUG"):
         level = logging.DEBUG
 
     logger = logging.getLogger("alerce")
