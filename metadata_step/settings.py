8340d142554f7b6b39f63a25425a7a42edb944af
Diego Rodriguez <diegorodriguezmancini@gmail.com>
2023-11-17 14:15:31 -0300

    feat(metadata-step): use config from yaml

--- metadata_step/settings.py
+++ metadata_step/settings.py
@@ -1,11 +1,10 @@
 import os
-from fastavro import schema
-from credentials import get_credentials
+import pathlib
 
+metrics_schema_path = pathlib.Path(pathlib.Path(__file__).parent.parent, "schemas/metadata_step", "metrics.json")
 ## Set the global logging level to debug
 LOGGING_DEBUG = os.getenv("LOGGING_DEBUG", False)
 
-DATABASE = get_credentials(os.environ["SQL_SECRET_NAME"])
 
 ## Consumer configuration
 ### Each consumer has different parameters and can be found in the documentation
@@ -19,7 +18,7 @@ CONSUMER_CONFIG = {
     },
     "TOPICS": os.environ["CONSUMER_TOPICS"].split(","),
     "consume.messages": int(os.getenv("CONSUME_MESSAGES", 250)),
-    "consume.timeout": int(os.getenv("CONSUME_TIMEOUT", 0)),
+    "consume.timeout": int(os.getenv("CONSUME_TIMEOUT", 10)),
 }
 
 METRICS_CONFIG = {
@@ -31,38 +30,9 @@ METRICS_CONFIG = {
     "PARAMS": {
         "PARAMS": {
             "bootstrap.servers": os.getenv("METRICS_SERVER"),
-            "auto.offset.reset": "smallest",
         },
         "TOPIC": os.getenv("METRICS_TOPIC", "metrics"),
-        "SCHEMA": {
-            "$schema": "http://json-schema.org/draft-07/schema",
-            "$id": "http://example.com/example.json",
-            "type": "object",
-            "title": "The root schema",
-            "description": "The root schema comprises the entire JSON document.",
-            "default": {},
-            "examples": [{"timestamp_sent": "2020-09-01", "timestamp_received": "2020-09-01"}],
-            "required": ["timestamp_sent", "timestamp_received"],
-            "properties": {
-                "timestamp_sent": {
-                    "$id": "#/properties/timestamp_sent",
-                    "type": "string",
-                    "title": "The timestamp_sent schema",
-                    "description": "Timestamp sent refers to the time at which a message is sent.",
-                    "default": "",
-                    "examples": ["2020-09-01"],
-                },
-                "timestamp_received": {
-                    "$id": "#/properties/timestamp_received",
-                    "type": "string",
-                    "title": "The timestamp_received schema",
-                    "description": "Timestamp received refers to the time at which a message is received.",
-                    "default": "",
-                    "examples": ["2020-09-01"],
-                },
-            },
-            "additionalProperties": True,
-        },
+        "SCHEMA_PATH": os.getenv("METRICS_SCHEMA_PATH", metrics_schema_path),
     },
 }
 
@@ -82,5 +52,5 @@ STEP_CONFIG = {
     "CONSUMER_CONFIG": CONSUMER_CONFIG,
     "METRICS_CONFIG": METRICS_CONFIG,
     "LOGGING_DEBUG": LOGGING_DEBUG,
-    "DATABASE": DATABASE,
+    "DATABASE_SECRET_NAME": os.environ["SQL_SECRET_NAME"],
 }
