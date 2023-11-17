413d6a3d68338ff14981a6a2e83b787ff5ec9d0a
Diego Rodriguez <diegorodriguezmancini@gmail.com>
2023-11-17 15:56:00 -0300

    chore:add staging helm values

--- ci/helm_values/staging/variables.tf
+++ ci/helm_values/staging/variables.tf
@@ -19,6 +19,69 @@ variable "lightcurve_kafka_password" {
   type = string
 }
 
+variable "sorting_hat_kafka_username" {
+  type = string
+}
+variable "sorting_hat_kafka_password" {
+  type = string
+}
+
+variable "prv_candidates_kafka_username" {
+  type = string
+}
+variable "prv_candidates_kafka_password" {
+  type = string
+}
+
+variable "correction_kafka_username" {
+  type = string
+}
+variable "correction_kafka_password" {
+  type = string
+}
+
+variable "magstats_kafka_username" {
+  type = string
+}
+variable "magstats_kafka_password" {
+  type = string
+}
+
+variable "xmatch_kafka_username" {
+  type = string
+}
+variable "xmatch_kafka_password" {
+  type = string
+}
+
+variable "features_kafka_username" {
+  type = string
+}
+variable "features_kafka_password" {
+  type = string
+}
+
+variable "lc_classification_kafka_username" {
+  type = string
+}
+variable "lc_classification_kafka_password" {
+  type = string
+}
+
+variable "metadata_kafka_username" {
+  type = string
+}
+variable "metadata_kafka_password" {
+  type = string
+}
+
+variable "scribe_kafka_username" {
+  type = string
+}
+variable "scribe_kafka_password" {
+  type = string
+}
+
 variable "ghcr_username" {
   type = string
 }
