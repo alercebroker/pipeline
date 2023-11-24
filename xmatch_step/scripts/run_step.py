325290a44b3dd9abd784233d07278a0c045ae15d
Diego Rodriguez <diegorodriguezmancini@gmail.com>
2023-11-24 12:36:10 -0300

    feat: add pyroscope profiling

--- xmatch_step/scripts/run_step.py
+++ xmatch_step/scripts/run_step.py
@@ -3,7 +3,7 @@ import sys
 import logging
 
 from prometheus_client import start_http_server
-from apf.metrics.prometheus import PrometheusMetrics
+from apf.metrics.prometheus import PrometheusMetrics, DefaultPrometheusMetrics
 
 SCRIPT_PATH = os.path.dirname(os.path.abspath(__file__))
 PACKAGE_PATH = os.path.abspath(os.path.join(SCRIPT_PATH, ".."))
@@ -35,9 +35,18 @@ logger.addHandler(handler)
 
 from xmatch_step import XmatchStep
 
-prometheus_metrics = PrometheusMetrics()
+prometheus_metrics = DefaultPrometheusMetrics()
 if STEP_CONFIG.get("FEATURE_FLAGS", {}).get("PROMETHEUS"):
     start_http_server(8000)
+    prometheus_metrics = PrometheusMetrics()
+
+if STEP_CONFIG.get("FEATURE_FLAGS", {}).get("USE_PROFILING", False):
+    from pyroscope import configure
+
+    configure(
+        application_name="step.Lightcurve",
+        server_address=STEP_CONFIG["PYROSCOPE_SERVER"],
+    )
 
 step = XmatchStep(config=STEP_CONFIG, prometheus_metrics=prometheus_metrics)
 step.start()
