4bce93e28d3437d98a06f72b1fd32b8f8abb9047
ale-munozarancibia <amma.19@gmail.com>
2023-12-06 14:09:31 -0300

    Fixed black error

--- prv_candidates_step/prv_candidates_step/core/strategy/ztf.py
+++ prv_candidates_step/prv_candidates_step/core/strategy/ztf.py
@@ -85,11 +85,11 @@ class ZTFForcedPhotometryParser(SurveyParser):
         mag = -2.5 * np.log10(np.abs(forcediffimflux)) + 23.9
         e_mag = 1.0857 * forcediffimfluxunc / np.abs(forcediffimflux)
 
-        if np.isclose(data["forcediffimflux"], -99999.):
+        if np.isclose(data["forcediffimflux"], -99999.0):
             mag = np.nan
             e_mag = np.nan
 
-        if np.isclose(data["forcediffimfluxunc"], -99999.):
+        if np.isclose(data["forcediffimfluxunc"], -99999.0):
             e_mag = np.nan
 
         return mag, e_mag
