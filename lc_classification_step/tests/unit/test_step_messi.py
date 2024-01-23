7e564ad21b2dc4aab7252bd8fb63bc3859af7a69
@alerceadmin <alerceadmin@users.noreply.github.com>
2024-01-23 18:56:08 +0000

    chore: update version

--- lc_classification_step/tests/unit/test_step_messi.py
+++
@@ -1,7 +0,0 @@
-import pytest
-from tests.unit.test_utils import generate_messages_elasticc
-
-
-@pytest.mark.elasticc
-def test_step_messi(test_elasticc_model, step_factory_messi):
-    test_elasticc_model(step_factory_messi, generate_messages_elasticc())
--- lc_classification_step/tests/unit/test_step_mlp.py
+++
@@ -1,7 +0,0 @@
-import pytest
-from tests.unit.test_utils import generate_messages_elasticc
-
-
-@pytest.mark.elasticc
-def test_step_mlp(test_elasticc_model, step_factory_mlp):
-    test_elasticc_model(step_factory_mlp, generate_messages_elasticc())
