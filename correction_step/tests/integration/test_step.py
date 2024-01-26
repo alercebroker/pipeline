9b37ab0a9265abd15ce037b56657d4385ebdc32e
Diego Rodriguez <diegorodriguezmancini@gmail.com>
2024-01-26 17:02:49 -0300

    tests(correction): added check for forced_photometry in scribe messages

--- correction_step/tests/integration/test_step.py
+++ correction_step/tests/integration/test_step.py
@@ -17,22 +17,41 @@ def assert_result_has_extra_fields(message):
             assert "diaObject" in det["extra_fields"]
 
 
-def assert_scribe_has_detections(message):
-    data = json.loads(message["payload"])
-    assert "collection" in data and data["collection"] == "detection"
-    assert "type" in data and data["type"] == "update"
-    assert "criteria" in data and "candid" in data["criteria"] and "oid" in data["criteria"]
-    assert "data" in data and len(data["data"]) > 0
-    assert "options" in data and "upsert" in data["options"] and "set_on_insert" in data["options"]
-    assert data["options"]["upsert"] is True
-    assert "has_stamp" in data["data"]
-    assert "candid" not in data["data"]  # Prevent duplication with _id
-    if data["data"]["has_stamp"]:
-        assert data["options"]["set_on_insert"] is False
-    else:
-        assert data["options"]["set_on_insert"] is True
-    if data["data"]["tid"] == "LSST":
-        assert "diaObject" in data["data"]["extra_fields"]
+def assert_scribe_has_detections(messages):
+    for message in messages:
+        assert "collection" in message and (
+            message["collection"] == "detection" or message["collection"] == "forced_photometry")
+        assert "type" in message and message["type"] == "update"
+        assert "criteria" in message and "candid" in message[
+            "criteria"] and "oid" in message["criteria"]
+        assert "data" in message and len(message["data"]) > 0
+        assert "options" in message and "upsert" in message[
+            "options"] and "set_on_insert" in message["options"]
+        assert message["options"]["upsert"] is True
+        assert "has_stamp" in message["data"]
+        # Prevent duplication with _id
+        assert "candid" not in message["data"]
+        if message["data"]["has_stamp"]:
+            assert message["options"]["set_on_insert"] is False
+        else:
+            assert message["options"]["set_on_insert"] is True
+        if message["data"]["tid"] == "LSST":
+            assert "diaObject" in message["data"]["extra_fields"]
+
+
+def assert_any_forced(messages):
+    any_forced = False
+    for msg in messages:
+        if msg["collection"] == "forced_photometry":
+            any_forced = True
+    assert any_forced
+
+
+def deserialize_messages(messages):
+    result = []
+    for msg in messages:
+        result.append(json.loads(msg["payload"]))
+    return result
 
 
 def test_result_has_everything(kafka_service, env_variables, kafka_consumer):
@@ -52,9 +71,11 @@ def test_result_has_everything(kafka_service, env_variables, kafka_consumer):
 def test_scribe_has_detections(kafka_service, env_variables, scribe_consumer):
     CorrectionStep.create_step().start()
 
-    for message in scribe_consumer.consume():
-        assert_scribe_has_detections(message)
-        scribe_consumer.commit()
+    messages = list(scribe_consumer.consume())
+    messages = deserialize_messages(messages)
+    assert len(messages)
+    assert_scribe_has_detections(messages)
+    assert_any_forced(messages)
 
 
 def test_works_with_batch(kafka_service, env_variables, kafka_consumer: KafkaConsumer, caplog):
