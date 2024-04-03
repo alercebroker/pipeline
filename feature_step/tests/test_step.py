5b6284f1aaf615feea18d6e733c658b4abbfc870
Ignacio Reyes <ireyesj93@gmail.com>
2024-04-03 12:11:54 -0300

    cleaning tests of feature_step

--- feature_step/tests/test_step.py
+++ feature_step/tests/test_step.py
@@ -55,151 +55,6 @@ class StepTestCase(unittest.TestCase):
         self.step.scribe_producer.produce = mock.MagicMock()
 
     def test_execute(self):
-        expected_output = [
-            {
-                "oid": "oid1",
-                "candid": ["1_candid_aid_1", "2_candid_aid_1"],
-                "detections": [
-                    {
-                        "candid": "1_candid_aid_1",
-                        "tid": "ztf",
-                        "aid": "aid1",
-                        "oid": "oid1",
-                        "mjd": 111,
-                        "sid": "sid-aid1",
-                        "fid": "1",
-                        "pid": 222,
-                        "ra": 333,
-                        "e_ra": 444,
-                        "dec": 555,
-                        "e_dec": 666,
-                        "mag": 777,
-                        "e_mag": 888,
-                        "mag_corr": None,
-                        "e_mag_corr": None,
-                        "e_mag_corr_ext": None,
-                        "isdiffpos": 999,
-                        "corrected": False,
-                        "dubious": False,
-                        "has_stamp": True,
-                        "stellar": False,
-                        "extra_fields": {},
-                    },
-                    {
-                        "candid": "2_candid_aid_1",
-                        "tid": "ztf",
-                        "aid": "aid1",
-                        "oid": "oid1",
-                        "mjd": 111,
-                        "sid": "sid-aid1",
-                        "fid": "1",
-                        "pid": 222,
-                        "ra": 333,
-                        "e_ra": 444,
-                        "dec": 555,
-                        "e_dec": 666,
-                        "mag": 777,
-                        "e_mag": 888,
-                        "mag_corr": None,
-                        "e_mag_corr": None,
-                        "e_mag_corr_ext": None,
-                        "isdiffpos": 999,
-                        "corrected": False,
-                        "dubious": False,
-                        "has_stamp": True,
-                        "stellar": False,
-                        "extra_fields": {},
-                    },
-                ],
-                "non_detections": [
-                    {
-                        "aid": "aid1",
-                        "tid": "ztf",
-                        "sid": "sid_aid1",
-                        "oid": "oid1",
-                        "mjd": 999.888,
-                        "fid": "1",
-                        "diffmaglim": 123.123,
-                    },
-                    {
-                        "aid": "aid1",
-                        "tid": "ztf",
-                        "sid": "sid_aid1",
-                        "oid": "oid1",
-                        "mjd": 888,
-                        "fid": "1",
-                        "diffmaglim": 999,
-                    },
-                ],
-                "xmatches": {
-                    "W1mag": 123,
-                    "W2mag": 456,
-                    "W3mag": 789,
-                },
-                "features": {
-                    "Amplitude_1": 123,
-                    "Amplitude_2": 456,
-                    "Multiband_period": 741,
-                    "feat3": 963,
-                    "rb": None,
-                },
-            },
-            {
-                "oid": "oid2",
-                "candid": ["1_candid_aid_2"],
-                "detections": [
-                    {
-                        "candid": "1_candid_aid_2",
-                        "tid": "ztf",
-                        "aid": "aid2",
-                        "oid": "oid2",
-                        "mjd": 111,
-                        "sid": "sid-aid1",
-                        "fid": "1",
-                        "pid": 222,
-                        "ra": 333,
-                        "e_ra": 444,
-                        "dec": 555,
-                        "e_dec": 666,
-                        "mag": 777,
-                        "e_mag": 888,
-                        "mag_corr": None,
-                        "e_mag_corr": None,
-                        "e_mag_corr_ext": None,
-                        "isdiffpos": 999,
-                        "corrected": False,
-                        "dubious": False,
-                        "has_stamp": True,
-                        "stellar": False,
-                        "extra_fields": {},
-                    }
-                ],
-                "non_detections": [
-                    {
-                        "aid": "aid2",
-                        "tid": "ztf",
-                        "sid": "sid_aid2",
-                        "oid": "oid2",
-                        "mjd": 888,
-                        "fid": "1",
-                        "diffmaglim": 999,
-                    }
-                ],
-                "xmatches": {
-                    "W1mag": 123,
-                    "W2mag": 456,
-                    "W3mag": 789,
-                },
-                "features": {
-                    "Amplitude_1": 321,
-                    "Amplitude_2": 654,
-                    "Multiband_period": 147,
-                    "feat3": 369,
-                    "rb": 888,
-                },
-            },
-        ]
-
         messages = generate_input_batch(
             10,
             ["g", "r"],
@@ -207,8 +62,15 @@ class StepTestCase(unittest.TestCase):
         result_messages = self.step.execute(messages)
 
         self.assertEqual(len(messages), len(result_messages))
+        n_features_prev = -1
         for result_message in result_messages:
-            self.assertTrue(len(result_message['features']) > 0)
+            n_features = len(result_message['features'])
+            self.assertTrue(n_features > 0)
+
+            # Check all messages have the same number of features
+            if n_features_prev != -1:
+                self.assertEqual(n_features, n_features_prev)
+                n_features_prev = n_features
 
         self.step.scribe_producer.produce.assert_called()
         scribe_producer_call_count = (
