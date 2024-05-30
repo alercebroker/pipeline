45a2faac6423a1df76610cc82f4be3b45750288e
Alex Alvarez Toledo <otajla@hotmail.com>
2024-05-30 10:14:28 -0400

    added a filter for the forced

--- prv_candidates_step/prv_candidates_step/core/strategy/ztf.py
+++ prv_candidates_step/prv_candidates_step/core/strategy/ztf.py
@@ -175,6 +175,24 @@ def extract_detections_and_non_detections(alert: dict) -> dict:
     prv_candidates = pickle.loads(prv_candidates) if prv_candidates else []
 
     forced_photometries = alert["extra_fields"].pop("fp_hists", [])
+
+    # filter forced photometry by value
+    # potencial bugs from ztf
+    def filter_fp(fp):
+        forcediffimflux_bad_values = [
+            None, 0
+        ]
+        # if the value is not close to -99999 return true
+        good_fp = (
+            fp["forcediffimflux"] not in forcediffimflux_bad_values
+            and
+            not np.isclose(fp["forcediffimflux"], -99999)
+        )
+        return good_fp
+
+    # use the filter funcion to remove bad fp
+    forced_photometries = filter(filter_fp, forced_photometries)
+
     forced_photometries = (
         pickle.loads(forced_photometries) if forced_photometries else []
     )
