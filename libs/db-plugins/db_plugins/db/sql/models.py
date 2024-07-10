f967ec7d38a5376e94ed185bb5897148e3fd52d7
Alex Alvarez Toledo <otajla@hotmail.com>
2024-07-10 16:55:27 -0400

    moved detertor name to taxonomy

--- libs/db-plugins/db_plugins/db/sql/models.py
+++ libs/db-plugins/db_plugins/db/sql/models.py
@@ -119,7 +119,7 @@ class Score(Base):
 
 class ScoreDistribution(Base):
     __tablename__ = "score_distribution"
-    detector_name = Column(String, ForeignKey(Score.detector_name), primary_key=True)
+    detector_name = Column(String, ForeignKey(Taxonomy.classifier_name), primary_key=True)
     distribution_version = Column(String, primary_key=True)
     creation_date = Column(DateTime)
     category_name = Column(String)
