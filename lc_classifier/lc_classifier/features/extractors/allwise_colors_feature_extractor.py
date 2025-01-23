ba0b6e94c760067bb1b44b69a3b73abd410e3a97
Ale M <amma.19@gmail.com>
2025-01-23 16:09:03 -0300

    Fixed treatment of nans in features

--- lc_classifier/lc_classifier/features/extractors/allwise_colors_feature_extractor.py
+++ lc_classifier/lc_classifier/features/extractors/allwise_colors_feature_extractor.py
@@ -54,6 +54,7 @@ class AllwiseColorsFeatureExtractor(FeatureExtractor):
 
         features_df["sid"] = sid
         features_df["version"] = self.version
+        features_df["value"] = features_df["value"].astype(np.float64)
 
         all_features = [astro_object.features, features_df]
         astro_object.features = pd.concat(
