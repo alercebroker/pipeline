55daea3e408e9ba84400306f9ad847005b814c93
bastiancito <bastian.gamboa@ug.uchile.cl>
2025-02-19 15:01:31 -0300

    stamp_classifier_2025_step

---
+++ stamp_classifier_2025_step/scripts/run_step.py
@@ -0,0 +1,36 @@
+import os
+import sys
+import logging
+from apf.core.settings import config_from_yaml_file
+from apf.metrics.prometheus import DefaultPrometheusMetrics
+
+SCRIPT_PATH = os.path.dirname(os.path.abspath(__file__))
+PACKAGE_PATH = os.path.abspath(os.path.join(SCRIPT_PATH, ".."))
+sys.path.append(PACKAGE_PATH)
+
+level = logging.INFO
+
+logging.basicConfig(
+    level=level,
+    format="%(asctime)s %(levelname)s %(name)s.%(funcName)s: %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S",
+)
+
+
+from stamp_classifier_2025_step.core.step import MultiScaleStampClassifier
+
+
+step_config = config_from_yaml_file("/home/bastiancito/Desktop/config.yaml")
+prometheus_metrics = DefaultPrometheusMetrics()
+
+if step_config["FEATURE_FLAGS"]["PROMETHEUS"]:
+    from prometheus_client import start_http_server
+    from apf.metrics.prometheus import PrometheusMetrics
+
+    prometheus_metrics = PrometheusMetrics()
+    start_http_server(8000)
+
+step = MultiScaleStampClassifier(
+    config=step_config, level=level, prometheus_metrics=prometheus_metrics
+)
+step.start()
