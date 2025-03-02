b2d15570cebb86c35402fff6bb2cc7d95055f7d1
Alex Alvarez Toledo <otajla@hotmail.com>
2025-03-02 11:57:35 -0300

    docker image working

--- stamp_classifier_2025_step/scripts/run_step.py
+++ stamp_classifier_2025_step/scripts/run_step.py
@@ -20,7 +20,7 @@ logging.basicConfig(
 from stamp_classifier_2025_step.step import MultiScaleStampClassifier
 
 
-step_config = config_from_yaml_file(os.path.join(SCRIPT_PATH, "config.yaml"))
+step_config = config_from_yaml_file(os.path.join(SCRIPT_PATH, "/config/config.yaml"))
 prometheus_metrics = DefaultPrometheusMetrics()
 
 if step_config["FEATURE_FLAGS"]["PROMETHEUS"]:
