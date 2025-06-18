ed34b1236396d9f9b0d5c93deb21f0d9caa74880
hectorxh <hectorplj@gmail.com>
2025-06-18 16:48:33 -0400

    test(ingestion): improve generator for lsst

--- ingestion_step/tests/data/generator_lsst.py
+++ ingestion_step/tests/data/generator_lsst.py
@@ -1,17 +1,20 @@
 from pathlib import Path
 from random import choice, randint
-from typing import Any, Callable, Iterable, cast
+from typing import Callable, Iterable, cast
 
 from fastavro.schema import load_schema as _load_schema
 from fastavro.types import Schema
-from fastavro.utils import generate_many
+from fastavro.utils import generate_many, generate_one
 
 from ingestion_step.core.types import Message
 
 # Overwrite type as it is wrongly infered as `Unknown`
 load_schema = cast(Callable[[str], Schema], _load_schema)
 
-base_schema_path = Path("../schemas/surveys/lsst")
+base_schema_path = Path("../schemas/surveys/lsst/")
+alert_schema = load_schema(str(base_schema_path / "v7_4_alert.avsc"))
+diaObject_schema = load_schema(str(base_schema_path / "v7_4_diaObject.avsc"))
+ssObject_schema = load_schema(str(base_schema_path / "v7_4_ssObject.avsc"))
 
 
 def generate_bands(alert: Message) -> Message:
@@ -26,30 +29,40 @@ def generate_bands(alert: Message) -> Message:
     return alert
 
 
-def generate_object_ids(alert: Message) -> Message:
-    obj = "ss" if alert["diaObject"] is None else "dia"
+def generate_objects(alert: Message) -> Message:
+    obj = choice(["dia", "ss"])
+
     if obj == "dia":
-        alert["diaSource"]["diaObjectId"] = randint(0, 2**63 - 1)
-        alert["diaSource"]["ssObjectId"] = None
-        for prv_source in alert["prvDiaSources"] or []:
-            prv_source["diaObjectId"] = randint(0, 2**63 - 1)
-            prv_source["ssObjectId"] = None
+        diaObjectId = randint(0, 2**63 - 1)
+        ssObjectId = None
+        alert["diaObject"] = generate_one(diaObject_schema)
+        alert["diaObject"]["diaObjectId"] = diaObjectId
+        alert["ssObject"] = None
     else:
-        alert["diaSource"]["diaObjectId"] = None
-        alert["diaSource"]["ssObjectId"] = randint(0, 2**63 - 1)
-        for prv_source in alert["prvDiaSources"] or []:
-            prv_source["diaObjectId"] = None
-            prv_source["ssObjectId"] = randint(0, 2**63 - 1)
+        diaObjectId = None
+        ssObjectId = randint(0, 2**63 - 1)
+        alert["diaObject"] = None
+        alert["ssObject"] = generate_one(ssObject_schema)
+        alert["ssObject"]["ssObjectId"] = ssObjectId
+
+    alert["diaSource"]["diaObjectId"] = diaObjectId
+    alert["diaSource"]["ssObjectId"] = ssObjectId
+    for prv_source in alert["prvDiaSources"] or []:
+        prv_source["diaObjectId"] = diaObjectId
+        prv_source["ssObjectId"] = ssObjectId
+
+    if obj == "ss":
+        alert["prvDiaForcedSources"] = None
+    for forced_source in alert["prvDiaForcedSources"] or []:
+        forced_source["diaObjectId"] = diaObjectId
 
     return alert
 
 
-def generate_alerts(n: int = 30) -> Iterable[dict[str, Any]]:
-    schema_path = str(base_schema_path.joinpath("v7_4_alert.avsc"))
-    schema = load_schema(schema_path)
-    alerts = generate_many(schema, n)
+def generate_alerts(n: int = 30) -> Iterable[Message]:
+    alerts = generate_many(alert_schema, n)
 
-    alerts = map(generate_object_ids, alerts)
+    alerts = map(generate_objects, alerts)
     alerts = map(generate_bands, alerts)
 
     return alerts
