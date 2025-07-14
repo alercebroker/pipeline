4b952e8d440b57d07ef21de2b3a37613214f7450
VexHafid <43154044+kaymedinam@users.noreply.github.com>
2025-07-14 11:21:28 -0400

    Update pre_execute.py
    Added default value to errors when raErr/decErr are null

--- magstats_mutistream_step/magstats_step/core/surveys/LSST/pre_execute.py
+++ magstats_mutistream_step/magstats_step/core/surveys/LSST/pre_execute.py
@@ -1,20 +1,28 @@
 from typing import List, Dict, Tuple
 
+# Combine previous sources with sources
+# Change name of coordinates errors to a standard ra_error and dec_error for all surveys 
+# Add a default value to ra_error and dec_error for LSST of 0.01 to replace when raErr and decErr are null
+
 def extract_lsst_data(msg: Dict) -> Tuple[List[Dict], List[Dict]]:
     """Extract LSST format: source + previous_sources + forced_sources = Detections (used in the statistics calculator)
     Add ra_error as a field from raErr and decErr in the original fields names"""    
     
-    # Combine previous sources with sourcees
+    def add_default_errors(source, default_value=0.01):
+        """Add error fields with default values if missing."""
+        source["ra_error"] = source.get("raErr") or default_value
+        source["dec_error"] = source.get("decErr") or default_value
+    
+    # Process sources
     sources = msg.get("sources", [])
     for source in sources:
-        source["ra_error"] = source["raErr"]
-        source["dec_error"] = source["decErr"]
-    
-    # Process previous sources and add to main sources
+        add_default_errors(source)
+
+    # Process and extend with previous sources
     prev_sources = msg.get("previous_sources", [])
     for source in prev_sources:
-        source["ra_error"] = source["raErr"]
-        source["dec_error"] = source["decErr"]
+        add_default_errors(source)
+    
     sources.extend(prev_sources)
     
     # Process forced sources
