f4f6c17c744850c44fe6129dd66ea7cdb7c2a722
hectorxh <hectorplj@gmail.com>
2025-09-10 12:05:37 -0300

    feat: add custom key partitioning for lsst reflector

--- reflector_step/scripts/run_step.py
+++ reflector_step/scripts/run_step.py
@@ -1,10 +1,10 @@
+import logging
 import os
 import sys
 
-import logging
 from apf.core.settings import config_from_yaml_file
-from reflector_step.step import CustomMirrormaker
 
+from reflector_step.step import CustomMirrormaker
 
 SCRIPT_PATH = os.path.dirname(os.path.abspath(__file__))
 PACKAGE_PATH = os.path.abspath(os.path.join(SCRIPT_PATH, ".."))
@@ -23,11 +23,18 @@ step_config = settings["STEP_CONFIG"]
 
 keep_original_timestamp = step_config.pop("keep_original_timestamp")
 use_message_topic = step_config.pop("use_message_topic")
+producer_key = step_config.pop("producer_key")
+survey = str(step_config.pop("survey")).lower()
+
+if survey not in ["ztf", "lsst"]:
+    raise Exception("In config field `survey` must be one of ['ztf', 'lsst']")
 
 step = CustomMirrormaker(
     config=step_config,
     keep_original_timestamp=keep_original_timestamp,
+    survey=survey,
     use_message_topic=use_message_topic,
+    producer_key=producer_key,
 )
 
 step.start()
