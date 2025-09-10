141d829f5394451756929cb44cb874f83b02ef82
hectorxh <hectorplj@gmail.com>
2025-09-10 14:01:45 -0300

    fix(ingestion): remove duplicated detections before insertion

--- ingestion_step/tests/integration/test_process_alerts_lsst.py
+++ ingestion_step/tests/integration/test_process_alerts_lsst.py
@@ -42,12 +42,27 @@ def test_process_out_of_order(psql_db: PsqlDatabase):
 
     n_alerts = row[0]
 
-    assert n_alerts == 50
+    assert n_alerts == len(lsst_alerts)
 
 
 @pytest.mark.usefixtures("psql_db")
 def test_process_duplicated(psql_db: PsqlDatabase, lsst_alerts: list[Message]):
+    psql_db.drop_db()
+    psql_db.create_db()
+
     lsst_alerts_duplicated = lsst_alerts + lsst_alerts
 
     parsed_data = LsstStrategy.parse(lsst_alerts_duplicated)
     LsstStrategy.insert_into_db(psql_db, parsed_data)
+
+    with psql_db.session() as session:
+        assert isinstance(session, Session)
+        res = session.execute(
+            text("SELECT count(*) FROM lsst_detection WHERE has_stamp = true;")
+        )
+        row = res.fetchone()
+        assert row is not None and len(row) == 1
+
+    n_alerts = row[0]
+
+    assert n_alerts == len(lsst_alerts)
